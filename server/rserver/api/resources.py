fc89697efe24d01dab27117ae1c0544ffc11ab7e
Gabriel Vigliensoni <gabriel@vigliensoni.com>
2011-09-07 10:01:16 -0400

    api.resources.py deleted
    Signed-off-by: Gabriel Vigliensoni <gabriel@vigliensoni.com>

--- server/rserver/api/resources.py
+++
@@ -1,7 +0,0 @@
-from tastypie.resources import ModelResource
-from rserver.gserver.models import MyModel
-
-class MyModelResource(ModelResource):
-	class Meta:
-		queryset = MyModel.objects.all()
-		allowed_methods = ['get']
