a7352d77ed6e1a32d678bf1b25f1bcce7f865140
dellsystem <ilostwaldo@gmail.com>
2012-05-22 16:45:43 -0400

    Add fake list of projects to dashboard view
    Not yet styled

--- projects/views.py
+++ projects/views.py
@@ -2,4 +2,13 @@
 from django.shortcuts import render
 
 def dashboard(request):
-    return render(request, 'projects/dashboard.html')
+    # Fake list of projects. Replace this once the model has been defined
+    projects = [
+        {'name': 'My project', 'description': 'My awesome project', 'slug': 'my-project'},
+        {'name': 'Your project', 'description': 'Your terrible project', 'slug': 'your-project'},
+    ]
+
+    data = {
+        'projects': projects,
+    }
+    return render(request, 'projects/dashboard.html', data)
