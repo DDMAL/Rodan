d03a6ea7c01195f10988d75f6e522f483327cfbf
dellsystem <ilostwaldo@gmail.com>
2012-05-22 17:15:18 -0400

    Use context processor to display sidebar content
    Since the sidebar should only ever need to show projects (I think)

--- projects/views.py
+++ projects/views.py
@@ -2,13 +2,5 @@
 from django.shortcuts import render
 
 def dashboard(request):
-    # Fake list of projects. Replace this once the model has been defined
-    projects = [
-        {'name': 'My project', 'description': 'My awesome project', 'slug': 'my-project'},
-        {'name': 'Your project', 'description': 'Your terrible project', 'slug': 'your-project'},
-    ]
-
-    data = {
-        'projects': projects,
-    }
+    data = {}
     return render(request, 'projects/dashboard.html', data)
