e6fbcd4353f83ba0d8adde088f68a3fcd1c3e0dd
dellsystem <ilostwaldo@gmail.com>
2012-05-23 17:16:54 -0400

    Add more fake projects to context processor

--- projects/context_processors.py
+++ projects/context_processors.py
@@ -2,7 +2,7 @@ def list_projects(request):
     # Fake list of projects for now
     projects = []
 
-    for i in xrange(1, 21):
+    for i in xrange(1, 41):
         this_project = {
             'name': 'Project %d' % i,
             'desc': 'Some random description',
