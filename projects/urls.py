4ba426e0dfcbf8c7b60a49984c8cbb0fc81aad81
dellsystem <ilostwaldo@gmail.com>
2012-05-23 17:18:28 -0400

    Remove project create and status views

--- projects/urls.py
+++ projects/urls.py
@@ -2,8 +2,6 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^dashboard', 'projects.views.dashboard'),
-    url(r'^create', 'projects.views.create'),
     url(r'^settings', 'projects.views.settings'),
-    url(r'^status', 'projects.views.status'),
     url(r'^view/(?P<project_id>\d+)', 'projects.views.view'),
 )
--- projects/views.py
+++ projects/views.py
@@ -5,17 +5,9 @@ def dashboard(request):
     data = {}
     return render(request, 'projects/dashboard.html', data)
 
-def create(request):
-    data = {}
-    return render(request, 'projects/create.html', data)
-
 def settings(request):
     data = {}
     return render(request, 'projects/settings.html', data)
-
-def status(request):
-    data = {}
-    return render(request, 'projects/status.html', data)
 
 def view(request, project_id):
     # Fake project for now (until the model is set up)
