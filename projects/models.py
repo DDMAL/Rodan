262255e1e04162d27a846a9fb2b80c547e1c1887
dellsystem <ilostwaldo@gmail.com>
2012-05-25 14:26:29 -0400

    Add unicode method to more models
    This commit also converts tabs to spaces.

--- projects/models.py
+++ projects/models.py
@@ -4,48 +4,52 @@ from django.contrib.auth.models import User
 
 #this model is to extend the default user behaviour if we need additional information such as avatar
 class RodanUser(models.Model):
-	user = models.OneToOneField(User)
-	affiliation = models.CharField(max_length=100)
+    user = models.OneToOneField(User)
+    affiliation = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return self.user.username
 
 class Project(models.Model):
-	name = models.CharField(max_length=50)
-	description = models.CharField(max_length=250)
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+    rodan_users = models.ManyToManyField(RodanUser)
 
-	rodan_users = models.ManyToManyField(RodanUser)
+    def __unicode__(self):
+        return self.name
 
 class Page(models.Model):
-	PIXELTYPE_CHOICES = (
-		(0, "RGB"),
-		(1, "grey_scale")
-	)
+    PIXELTYPE_CHOICES = (
+        (0, "RGB"),
+        (1, "grey_scale")
+    )
 
-	image_name = models.CharField(max_length=50)
-	path_to_image = models.CharField(max_length=200) #full path + file name? or just directory location?
-	pixel_type = models.IntegerField(choices=PIXELTYPE_CHOICES)
-	width = models.IntegerField()
-	height = models.IntegerField()
-	size_in_kB = models.IntegerField()
+    image_name = models.CharField(max_length=50)
+    path_to_image = models.CharField(max_length=200) #full path + file name? or just directory location?
+    pixel_type = models.IntegerField(choices=PIXELTYPE_CHOICES)
+    width = models.IntegerField()
+    height = models.IntegerField()
+    size_in_kB = models.IntegerField()
 
-	project = models.ForeignKey(Project)
+    project = models.ForeignKey(Project)
 
-	def __unicode__(self):
-		return "Page %s" % (self.path_to_image + self.image_name)
+    def __unicode__(self):
+        return "Page %s" % (self.path_to_image + self.image_name)
 
-	def get_num_pixels(self):
-		return self.width * self.height
+    def get_num_pixels(self):
+        return self.width * self.height
 
-	def get_size_in_mB(self):
-		return (size_in_kB / 1024)
+    def get_size_in_mB(self):
+        return (size_in_kB / 1024)
 
-	def get_absolute_url(self):
-		return '/projects/page_view/%d' % self.id
-		
+    def get_absolute_url(self):
+        return '/projects/page_view/%d' % self.id
 
 class Workflow(models.Model):
-	name = models.CharField(max_length=50)
-	description = models.CharField(max_length=250)
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
 
-	project = models.ForeignKey(Project)
+    project = models.ForeignKey(Project)
 
-	def __unicode__(self):
-		return "Workflow name: %s" % self.name
+    def __unicode__(self):
+        return "Workflow name: %s" % self.name
