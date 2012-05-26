dbc3d0f55082de0da45550140fc228e8466be200
dellsystem <ilostwaldo@gmail.com>
2012-05-26 15:24:45 -0400

    Fix whitespace in highcharts JS

--- rodan/static/js/rodan.js
+++ rodan/static/js/rodan.js
@@ -1,143 +1,78 @@
 $(document).ready(function () {
-
-    var graph = new Highcharts.Chart({
-
+    if ($('#main-line-graph').length) {
+        var graph = new Highcharts.Chart({
             chart: {
                 backgroundColor: null,
                 height: 300,
-
                 renderTo: 'main-line-graph',
-
                 type: 'spline'
-
             },
             credits: {
                 enabled: false,
             },
-
             title: {
-
                 text: ''
-
             },
-
             xAxis: {
-
-                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-
-                    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-
+                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
             },
-
             yAxis: {
-
                 title: {
-
                     text: 'Pages processed per day'
-
                 },
-
                 labels: {
-
                     formatter: function() {
-
                         return this.value
-
                     }
-
                 }
-
             },
-
             tooltip: {
-
                 crosshairs: true,
-
                 shared: true
-
             },
-
             plotOptions: {
-
                 spline: {
-
                     marker: {
-
                         radius: 4,
-
                         lineColor: '#666666',
-
                         lineWidth: 1
-
                     }
-
                 }
-
             },
-
             series: [{
-
                 name: 'Binarisation',
-
                 marker: {
-
                     symbol: 'square'
-
                 },
-
                 data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, {
-
                     y: 26.5,
-
                     marker: {
-
                         //symbol: 'url(http://www.highcharts.com/demo/gfx/sun.png)'
-
                     }
-
                 }, 23.3, 18.3, 13.9, 9.6]
-
-    
-
             }, {
-
                 name: 'Segmentation',
-
                 marker: {
-
                     symbol: 'diamond'
-
                 },
-
                 data: [{
-
                     y: 3.9,
-
                     marker: {
-
                         //symbol: 'url(http://www.highcharts.com/demo/gfx/snow.png)'
-
                     }
-
                 }, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-
             }]
+        });
+    }
 
-        }); 
-
-    var chart = new Highcharts.Chart({
-
+    if ($('#main-pie-chart').length) {
+        var chart = new Highcharts.Chart({
             chart: {
-
                 renderTo: 'main-pie-chart',
-
                 backgroundColor: null,
-
                 plotBorderWidth: null,
                 height: 200,
-
                 plotShadow: false
-
             },
             credits: {
                 enabled: false,
@@ -146,78 +81,43 @@ $(document).ready(function () {
                 text: ''
             },
             tooltip: {
-
                 formatter: function() {
-
                     return '<b>'+ this.point.name +'</b>: '+ this.percentage +' %';
-
                 }
-
             },
-
             plotOptions: {
-
                 pie: {
-
                     allowPointSelect: true,
-
                     cursor: 'pointer',
-
                     dataLabels: {
-
                         enabled: false,
-
                         color: '#000000',
-
                         connectorColor: '#000000',
-
                         formatter: function() {
-
                             return '<b>'+ this.point.name +'</b>: '+ this.percentage +' %';
-
                         }
-
                     }
-
                 }
-
             },
-
             series: [{
-
                 type: 'pie',
-
                 name: 'Browser share',
-
                 data: [
-
                     ['Firefox',   45.0],
-
                     ['IE',       26.8],
-
                     {
-
                         name: 'Chrome',
-
                         y: 12.8,
-
                         sliced: true,
-
                         selected: true
-
                     },
-
                     ['Safari',    8.5],
-
                     ['Opera',     6.2],
-
                     ['Others',   0.7]
-
                 ]
-
             }]
-
         });
+    }
 
 /*
     (function ($) {
