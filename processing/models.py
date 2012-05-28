34d0449461edd144eaf39171260a3d34ca936003
Anton Khelou <anton_midi@hotmail.com>
2012-05-28 19:03:07 -0400

    Change processing models

--- processing/models.py
+++ processing/models.py
@@ -1,74 +1,35 @@
 import os
 from django.db import models
-from projects.models import Workflow, Page
+from projects.models import Workflow, Page, Job, RodanUser
 
 import gamera.core as gam
 from gamera.plugins import threshold
-
 
 
 class Result(models.Model):
     start_time = models.DateTimeField(auto_now_add=True)
     end_time = models.DateTimeField(null=True)
 
-    work_flow = models.ForeignKey(Workflow)
+    job = models.ForeignKey(Job)
     page = models.ForeignKey(Page)
+    rodan_user = models.ForeignKey(RodanUser)
 
-class Rotate(models.Model):
-    rotation_value = models.IntegerField()
-
-    result = models.OneToOneField(Result)
-
-    def __unicode__(self):
-        return "Rotate Result w/ rotation_value=%s" % self.rotation_value
-
-'''
-class Crop(Job):
+    class Meta:
+        unique_together = ("job","page")
 
 
-class Segmentation(Job):
+class ResultFile(models.Model):
+    file_name = models.CharField(max_length=50)
+    path_to_file = models.CharField(max_length=200)
+
+    result = models.ForeignKey(Result)
 
 
-class SegCorrection(Job):
-    def do_stuff(self):
-        return seg_correct_view
-'''
-#inherits the default behaviour and attributes from job, and adds additional information specific to this type of relationship
-class Binarise(models.Model):
-    #TO DO: find possible parameters for a ize job
-    #perhaps extend this as well for different types of binarisation jobs??
-    threshold_value = models.IntegerField()
+class ActionParameter(models.Model):
+    key = models.CharField(max_length=50)
+    value = models.CharField(max_length=50)
 
-    result = models.OneToOneField(Result)
-
-    def __unicode__(self):
-        return "binarise Result w/ threshold_value=%s" % self.threshold_value
-
-    def binarise_image(self):
-        bin_page = self.result.page
-        path_to_img = bin_page.path_to_image.encode('ascii','ignore') #not used for now
-        image_name = bin_page.image_name.encode('ascii','ignore')
-
-        file_name,file_extension = os.path.splitext(image_name)
-        gam.init_gamera()
-        simple_thresh_obj = threshold.threshold()
-
-        if not os.path.exists("images"):
-            os.makedirs("images")
-        
-        output_img = simple_thresh_obj(gam.load_image("images/" + image_name),self.threshold_value)
-
-        if not os.path.exists("resultimages"):
-            os.makedirs("resultimages")
-
-        output_path =  "resultimages/" + file_name + "_binarise_simplethresh_" + str(self.threshold_value) + file_extension
-        gam.save_image(output_img, output_path)
-
-        return output_path
-
-
-
-
+    result = models.ForeignKey(Result)
 
 
 
