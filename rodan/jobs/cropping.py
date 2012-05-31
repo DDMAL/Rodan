a6d94a256cb98c039cb22e6565e548ccc66cdd36
dellsystem <ilostwaldo@gmail.com>
2012-05-31 16:14:35 -0400

    Add skeleton for a cropping job
    Just so I can have more jobs to play with etc

---
+++ rodan/jobs/cropping.py
@@ -0,0 +1,10 @@
+from rodan.models import jobs
+
+class Crop(jobs.JobBase):
+    input_type = jobs.JobType.IMAGE
+    output_type = input_type
+    description = 'Crop an image.'
+    show_during_wf_create = True
+
+    def on_post(self, **kwargs):
+        pass
