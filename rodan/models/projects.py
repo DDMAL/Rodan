becbf1356a40a89f6b5d61c8f9b5e31c9501121b
Anton Khelou <anton_midi@hotmail.com>
2012-05-31 15:58:12 -0400

    Add celery simple_binarise task
    create an equivalent of simple_binarise as a celery task.
    
    removed unnecessary imports that caused circular imports.
    
    changed some settings in settings.py to get celery to communicate properly with django.

--- rodan/models/projects.py
+++ rodan/models/projects.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
-from rodan.jobs import jobs
 
 class RodanUser(models.Model):
     class Meta:
