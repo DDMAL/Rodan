8b6ec246c8746cb03c636de94809e8e9c262ef02
Anton Khelou <anton_midi@hotmail.com>
2012-06-01 16:10:17 -0400

    Add new tasks and jobs
    added:
    djvu 		->binarisation
    despeckle 	->morphology
    rank filter	->filtering
    staff find	->staff finding

--- rodan/jobs/binarisation.py
+++ rodan/jobs/binarisation.py
@@ -1,7 +1,7 @@
 from rodan.models.jobs import JobType, JobBase
 from tasks import tasks 
 
-class Binarise(JobBase):
+class SimpleThresholdBinarise(JobBase):
     input_type = JobType.IMAGE
     output_type = input_type
     description = 'Convert a greyscale image to black and white.'
@@ -12,5 +12,22 @@ class Binarise(JobBase):
     '''
     def on_post(self, **kwargs):
         result_id = kwargs['result_id']
-        tv = kwargs['threshold_value']
+        tv = kwargs['threshold']
         tasks.simple_binarise.delay(result_id,tv)
+
+class DJVUBinarise(JobBase):
+    input_type = JobType.IMAGE
+    output_type = input_type
+    description = 'Convert a RGB image to black and white.'
+    show_during_wf_create = True
+
+    '''
+    Requires a valid result_id and a threshold value
+    '''
+    def on_post(self, **kwargs):
+        result_id = kwargs['result_id']
+        smoothness = kwargs['smoothness']
+        max_block_size = kwargs['max_block_size']
+        min_block_size = kwargs['min_block_size']
+        block_factor = kwargs['block_factor']
+        tasks.djvu_binarise.delay(result_id,smoothness,max_block_size,min_block_size,block_factor)
