5259aa98d20b5791010a077aaecd1256eb888c4f
Anton Khelou <anton_midi@hotmail.com>
2012-06-06 13:03:45 -0400

    Fix little JobType bug
    undefined.. after i changed job types yesterday

--- rodan/jobs/classification.py
+++ rodan/jobs/classification.py
@@ -4,7 +4,7 @@ from tasks import tasks
 class Classifier(JobBase):
     name = 'Gamera classifier'
     slug = 'classifier'
-    input_type = JobType.IMAGE
+    input_type = JobType.IMAGE_ONEBIT
     output_type = JobType.XML
     description = 'Performs classification on a binarized staff-less image and outputs an xml file'
     show_during_wf_create = True
--- rodan/jobs/cropping.py
+++ rodan/jobs/cropping.py
@@ -1,7 +1,7 @@
 from rodan.models import jobs
 
 class Crop(jobs.JobBase):
-    input_type = jobs.JobType.IMAGE
+    input_type = jobs.JobType.IMAGE_RGB
     output_type = input_type
     description = 'Crop an image.'
     show_during_wf_create = True
