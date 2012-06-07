cb9f908f0fb7de6303eef9a49b742c2e74a0e472
Alastair Porter <alastair.porter@mail.mcgill.ca>
2012-06-07 15:28:53 -0400

    Test save resultfile method

--- rodan/tests/results.py
+++ rodan/tests/results.py
@@ -1,15 +1,18 @@
 
 from django.utils import unittest
-from rodan.models.results import Result, Parameter, ResultFile
+
+from rodan.models.jobs import JobType
 from rodan.models.projects import JobItem, RodanUser, Page
+from rodan.models.results import Result, Parameter, ResultFile
 
 class TestResult(unittest.TestCase):
-    def setUp(self):
-        self.ji = JobItem.objects.get(pk=1)
-        self.user = RodanUser.objects.get(pk=1)
-        self.page = Page.objects.get(pk=1)
-        self.result = Result(job_item=self.ji, user=self.user, page=self.page)
-        self.result.save()
+    @classmethod
+    def setUpClass(cls):
+        cls.ji = JobItem.objects.get(pk=1)
+        cls.user = RodanUser.objects.get(pk=1)
+        cls.page = Page.objects.get(pk=1)
+        cls.result = Result(job_item=cls.ji, user=cls.user, page=cls.page)
+        cls.result.save()
 
     def testSaveParams(self):
         params = {"param1": "v1",
@@ -24,3 +27,16 @@ class TestResult(unittest.TestCase):
         self.assertTrue(savedParams[0].key in ["param1", "param2"])
         self.assertTrue(savedParams[0].value in ["v1", "v2"])
 
+    def testCreateFile(self):
+        self.result.create_file("testfilename")
+
+        self.assertEqual(1, len(self.result.resultfile_set.all()))
+        files = self.result.resultfile_set.all()
+        self.assertEqual("testfilename", files[0].filename)
+        # Default
+        self.assertEqual(JobType.IMAGE, files[0].result_type)
+
+        self.result.create_file("anotherfile", JobType.MEI)
+        self.assertEqual(2, len(self.result.resultfile_set.all()))
+        files = self.result.resultfile_set.all()
+        self.assertEqual(JobType.MEI, files[1].result_type)
