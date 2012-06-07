69e655c42dc8e24f77faee3e3985dbe8cc206836
Anton Khelou <anton_midi@hotmail.com>
2012-06-07 17:15:51 -0400

    WIP: simple_binarise() is working with new system
    currently fixing up all tasks to work with new system, only simple_binarise() is implemented and working

--- rodan/jobs/tasks/tasks.py
+++ rodan/jobs/tasks/tasks.py
@@ -2,7 +2,7 @@
 import json
 import os
 
-#from gamera.core import *
+import gamera.core
 from gamera import gamera_xml
 from gamera.classify import BoundingBoxGroupingFunction
 from gamera.plugins import threshold, transformation
@@ -12,20 +12,8 @@ from gamera.gameracore import Point
 
 from celery.task import task
 from django.conf import settings
-from django.utils import timezone
 from rodan.models.results import Result, Parameter, ResultFile
 from rodan.models.jobs import JobType
-
-
-def __setup_task(result_id):
-    global result
-    result = Result.objects.get(pk=result_id)
-
-    page = result.page
-    image_name = page.filename.encode('ascii','ignore')
-
-    init_gamera()
-    return image_name
 
 def __create_polygon_json_dict(poly_list):
     '''
@@ -45,47 +33,6 @@ def __create_polygon_json_dict(poly_list):
         poly_json_list.append(point_list_one)
 
     return poly_json_list
-
-
-# def __fix_poly_point_list(poly_list, staffspace_height):
-#     debug_string = ''
-#     for poly in poly_list:#loop over polygons
-#         debug_string += "\nPOLYGON\n"
-#         #following condition checks if there are the same amount of points on all 4 staff lines
-#         if len(poly[0].vertices) == len(poly[1].vertices) and \
-#         len(poly[0].vertices) == len(poly[2].vertices) and \
-#         len(poly[0].vertices) == len(poly[3].vertices):
-#             debug_string += "polygon is fine.\n"
-#             continue
-#         else:
-#             debug_string += "polygon needs fixing..\n"
-#             for j in xrange(0,len(poly)):#loop over all 4 staff lines
-#                 debug_string += "outer loop on staff %s\n" % j
-#                 for k in xrange(0,len(poly[j].vertices)):#loop over points of staff
-#                     debug_string += "point %s\n" % k
-#                     for l in xrange(0,len(poly)):#loop over all 4 staff lines
-#                         debug_string += "inner loop on staff %s\n" % l
-#                         if l == j:# optimization to not loop through the same staff line as outer loop
-#                             continue
-
-#                         if(k < len(poly[l].vertices)):
-#                             y_pix_diff = poly[j].vertices[k].x - poly[l].vertices[k].x
-#                         else:
-#                             y_pix_diff = -10000
-
-#                         debug_string += "y_pix_diff = %s\n" % y_pix_diff
-#                         if(y_pix_diff < 5 and y_pix_diff > -5): #if the y coordinate pixel difference within acceptable deviation
-#                             debug_string += "matching point found within deviation range\n"
-#                             continue
-#                         else:
-#                             debug_string += "NO matching point found within deviation range\n"
-#                             #missing a point on that staff
-#                             staffspace_multiplier = (l - j) #represents the number of staff lines apart from one another
-#                             poly[l].vertices.insert(k, Point(poly[j].vertices[k].x, poly[j].vertices[k].y + (staffspace_multiplier * staffspace_height)))
-#                             debug_string += "current staff %s points: %s\n" % (l, poly[l].vertices)
-
-#     return poly_list
-
 
 def __fix_poly_point_list(poly_list, staffspace_height):
     for poly in poly_list:#loop over polygons
@@ -129,10 +76,7 @@ def __print_poly_list(poly_list):
 
 #####BINARISE######
 @task(name="binarisation.simple_binarise")
-def simple_binarise(result_id, threshold=0):
-    result = Result.objects.get(pk=result_id)
-    page = result.page.get_latest_file(JobType.IMAGE)
-     
+def simple_binarise(result_id, **kwargs):
     """
     The below has been commented out because it doesn't work
     The above is for debugging
@@ -141,25 +85,27 @@ def simple_binarise(result_id, threshold=0):
     maybe add a method to Page to make it easier to do that?
     ====================
     """
-    image_name = __setup_task(result_id)
-    output_img = load_image("images/" + image_name).threshold(threshold_value)
+    gamera.core.init_gamera()
 
-    file_name,file_extension = os.path.splitext(image_name)
-    output_file_name =file_name + "_binarize_simplethresh_" + str(threshold_value) + file_extension
+    result = Result.objects.get(pk=result_id)
+    page_file_name = result.page.get_latest_file(JobType.IMAGE)   
 
-    save_image(output_img,"resultimages/" + output_file_name)
-    params = {"threshold_value": threshold_value}
-    result.save_parameters(**params)
-    result.create_file(output_file_name)
+    output_img = gamera.core.load_image(page_file_name).threshold(kwargs['threshold'])
 
-    file = open(os.path.join(settings.MEDIA_ROOT, 'lol'), 'wt')
-    file.write("lol")
-    filepath = Result.objects.get(pk=result_id).page.get_latest_file(JobType.IMAGE)
-    file.write(filepath)
-    return filepath
+    full_output_path = result.page.get_filename_for_job(result.job_item.job)
+    output_dir = os.path.dirname(full_output_path)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    gamera.core.save_image(output_img, full_output_path)
+
+    result.save_parameters(**kwargs)
+    result.create_file(full_output_path, JobType.IMAGE_ONEBIT)
+    result.update_end_total_time()
+
 
 @task(name="binarisation.djvu_threshold")
-def djvu_binarise(result_id, smoothness=0.2, max_block_size=512, min_block_size=64, block_factor=2):
+def djvu_binarise(result_id, **kwargs):
     """
         *smoothness*
           The amount of effect that parent blocks have on their children
@@ -177,27 +123,26 @@ def djvu_binarise(result_id, smoothness=0.2, max_block_size=512, min_block_size=
           For instance, a *block_factor* of 2 results in 4 children per
           parent.
     """
+    gamera.core.init_gamera()
+
     result = Result.objects.get(pk=result_id)
-    image_name = __setup_task(result_id)
-    output_img = load_image("images/" + image_name).djvu_threshold(smoothness,max_block_size,min_block_size,block_factor)
+    page_file_name = result.page.get_latest_file(JobType.IMAGE)  
+
+    output_img = gamera.core.load_image(page_file_name).djvu_threshold(kwargs['smoothness'],kwargs['max_block_size'],kwargs['min_block_size'],kwargs['block_factor'])
 
     file_name,file_extension = os.path.splitext(image_name)
     output_file_name = file_name + "_binarize_djvu_smo" + str(smoothness) + "max" + str(max_block_size) + "min" + str(min_block_size) + "fac" + str(block_factor) + file_extension
 
-    save_image(output_img,"resultimages/" + output_file_name)
+    gamera.core.save_image(output_img,"resultimages/" + output_file_name)
 
-    params = {"smoothness": smoothness,
-              "max_block_size": max_block_size,
-              "min_block_size": min_block_size,
-              "block_factor": block_factor
-             }
-    result.save_parameters(**params)
-    result.create_file(output_file_name)
+    result.save_parameters(**kwargs)
+    result.create_file(output_file_name, JobType.IMAGE_ONEBIT)
+    result.total_timestamp()
 
 #####FILTERS(ONLY RANK?)#####
 
 @task(name="filters.rank")
-def rank_filter(result_id, rank_val, k, border_treatment):
+def rank_filter(result_id, **kwargs):
     """
       *rank_val* (1, 2, ..., *k* * *k*)
         The rank of the windows pixels to select for the center. (k*k+1)/2 is
@@ -210,26 +155,26 @@ def rank_filter(result_id, rank_val, k, border_treatment):
         When 0 ('padwhite'), window pixels outside the image are set to white.
         When 1 ('reflect'), reflecting boundary conditions are used.
     """
+    gamera.core.init_gamera()
+
     result = Result.objects.get(pk=result_id)
-    image_name = __setup_task(result_id)
-    output_img = load_image("images/" + image_name).rank(rank_val,k,border_treatment)
+    page_file_name = result.page.get_latest_file(JobType.IMAGE) 
+
+    output_img = gamera.core.load_image(page_file_name).rank(kwargs['rank_val'],kwargs['k'],kwargs['border_treatment'])
 
     file_name,file_extension = os.path.splitext(image_name)
     output_file_name = file_name + "_rankfilter_rkv" + str(rank_val) + "k" + str(k) + "bt" + str(border_treatment) + file_extension
 
-    save_image(output_img,"resultimages/" + output_file_name)
+    gamera.core.save_image(output_img,"resultimages/" + output_file_name)
 
-    params = {"rank_val": rank_val,
-              "k": k,
-              "border_treatment": border_treatment
-             }
-    result.save_parameters(**params)
-    result.create_file(output_file_name)
+    result.save_parameters(**kwargs)
+    result.create_file(output_file_name, JobType.IMAGE_ONEBIT) #need to specify output type in this case its the same as input but we cannot send JobType.IMAGE --> tuple
+    result.total_timestamp()
 
 
 #####DESPECKLE#####
 @task(name="morphology.despeckle")
-def despeckle(result_id, despeckle_value=100, **params):
+def despeckle(result_id, **kwargs):
     """
       Removes connected components that are smaller than the given size.
 
@@ -246,23 +191,26 @@ def despeckle(result_id, despeckle_value=100, **params):
       *size* == 1 is a special case and runs much faster, since it does not
       require recursion.
     """
-    image_name = __setup_task(result_id)
+    gamera.core.init_gamera()
 
-    output_img = load_image("images/%s" % image_name)
-    output_img.despeckle(despeckle_value)
+    result = Result.objects.get(pk=result_id)
+    page_file_name = result.page.get_latest_file(JobType.IMAGE_ONEBIT) 
+
+    output_img = gamera.core.load_image(page_file_name)
+    output_img.despeckle(kwargs['despeckle_value'])
 
     file_name,file_extension = os.path.splitext(image_name)
     output_file_name = "%s_despeckle_%s%s" % (filename, str(despeckle_value) + file_extension)
 
-    save_image(output_img,"resultimages/" + output_file_name)
+    gamera.core.save_image(output_img,"resultimages/" + output_file_name)
 
-    result.save_parameters(**params)
-    result.create_file(output_file_name)
+    result.save_parameters(**kwargs)
+    result.create_file(output_file_name, JobType.IMAGE_ONEBIT)
+    result.total_timestamp()
 
 #####STAFF FINDING#####
-
 @task(name="staff_find.miyao")
-def find_staves(result_id, num_lines=0, scanlines=20, blackness=0.8, tolerance=-1):
+def find_staves(result_id, **kwargs):
     """
       *num_lines*:
         Number of lines within one staff. When zero, the number is automatically
@@ -280,44 +228,42 @@ def find_staves(result_id, num_lines=0, scanlines=20, blackness=0.8, tolerance=-
         from staffline_height. When negative, this value is set to
         *max([2, staffline_height / 4])*.
     """
-    image_name = __setup_task(result_id)
+    gamera.core.init_gamera()
+
+    result = Result.objects.get(pk=result_id)
+    page_file_name = result.page.get_latest_file(JobType.IMAGE) 
 
     #both 0's can be parameterized, first one is staffline_height and second is staffspace_height, both default 0
-    staff_finder = musicstaves.StaffFinder_miyao(load_image("images/" + image_name),0,0)
-    staff_finder.find_staves(num_lines,scanlines,blackness,tolerance)
+    staff_finder = musicstaves.StaffFinder_miyao(gamera.core.load_image(page_file_name),0,0)
+    staff_finder.find_staves(kwargs['num_lines'], kwargs['scanlines'], kwargs['blackness'], kwargs['tolerance'])
     poly_list = staff_finder.get_polygon()
-    with open("resultimages/test/before.txt","w") as f:
-        f.write(__print_poly_list(poly_list))
 
     poly_list = __fix_poly_point_list(poly_list,staff_finder.staffspace_height)
-    with open("resultimages/test/after.txt","w") as f:
-        f.write(__print_poly_list(poly_list))
 
-    # poly_json_list = __create_polygon_json_dict(poly_list)
 
-    # encoded = json.dumps(poly_json_list)
+    poly_json_list = __create_polygon_json_dict(poly_list)
 
-    # output_file_name ="resultimages/json/" + image_name + "_stdata.json"
-    # with open(output_file_name,"w") as f:
-    #     f.write(encoded)
+    encoded = json.dumps(poly_json_list)
 
-    params = {"smoothness": smoothness,
-              "max_block_size": max_block_size,
-              "min_block_size": min_block_size,
-              "block_factor": block_factor
-             }
-    result.save_parameters(**params)
-    result.create_file(output_file_name)
-    __save_results(output_file_name,num_lines=num_lines,scanlines=scanlines,blackness=blackness,tolerance=tolerance)
+    output_file_name ="resultimages/json/" + image_name + "_stdata.json"
+    with open(output_file_name,"w") as f:
+        f.write(encoded)
+
+    result.save_parameters(**kwargs)
+    result.create_file(output_file_name, JobType.JSON)
+    result.total_timestamp()
 
 ######CLASSIFICATION######
 @task(name="classifier")
-def classifier(result_id):
-    image_name = __setup_task(result_id)
+def classifier(result_id,**kwargs):
+    gamera.core.init_gamera()
+
+    result = Result.objects.get(pk=result_id)
+    page_file_name = result.page.get_latest_file(JobType.IMAGE_ONEBIT) 
 
     cknn = kNNNonInteractive("optimized_classifier_31Jan.xml",'all',True,1) #will be replaced by a new classifier that will be created soon
 
-    ccs = load_image("images/" + image_name).cc_analysis()
+    ccs = gamera.core.load_image(page_file_name).cc_analysis()
     func = BoundingBoxGroupingFunction(4)
 
     cs_image = cknn.group_and_update_list_automatic(ccs,grouping_function=func,max_parts_per_group=4,max_graph_size=16)
@@ -328,33 +274,25 @@ def classifier(result_id):
     myxml.write_filename("resultimages/xml/" + output_file_name)
 
 
-    params = {"smoothness": smoothness,
-              "max_block_size": max_block_size,
-              "min_block_size": min_block_size,
-              "block_factor": block_factor
-             }
-    result.save_parameters(**params)
-    result.create_file(output_file_name)
-    __save_results(output_file_name)
-
+    result.save_parameters(**kwargs)
+    result.create_file(output_file_name,JobType.XML)
+    result.total_timestamp()
 
 #####TRANSFORMATION#####
 @task(name="rotate")
-def rotate(result_id,angle):
-    image_name = __setup_task(result_id)
+def rotate(result_id,**kwargs):
+    gamera.core.init_gamera()
 
-    output_img = load_image("images/" + image_name).rotate(angle)
+    result = Result.objects.get(pk=result_id)
+    page_file_name = result.page.get_latest_file(JobType.IMAGE) 
+
+    output_img = gamera.core.load_image(page_file_name).rotate(kwargs['angle'])
 
     file_name,file_extension = os.path.splitext(image_name)
     output_file_name = file_name + "_rotate_" + str(angle) + file_extension
 
-    output_img.save_image("resultimages/" + output_file_name)
+    output_img.gamera.core.save_image("resultimages/" + output_file_name)
 
-    params = {"smoothness": smoothness,
-              "max_block_size": max_block_size,
-              "min_block_size": min_block_size,
-              "block_factor": block_factor
-             }
-    result.save_parameters(**params)
-    result.create_file(output_file_name)
-    __save_results(output_file_name,angle=angle)
+    result.save_parameters(**kwargs)
+    result.create_file(output_file_name, JobType.IMAGE_ONEBIT) #same problem as for rank filter, need more specific output type
+    result.total_timestamp()
