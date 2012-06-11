688ea69f4d908906cbb873e75bc808ba6cb40007
Brian Stern <brian.stern@mail.mcgill.ca>
2012-06-11 16:35:21 -0400

    Added basic auto-thumbnailing

--- rodan/jobs/utility.py
+++ rodan/jobs/utility.py
@@ -1,7 +1,16 @@
 import os
+import gamera.core
+from rodan.models import Result, Page
 
 
 def create_result_output_dirs(full_output_path):
     output_dir = os.path.dirname(full_output_path)
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
+
+def create_thumbnails(output_img, page):
+    page.scale_value = 100. / max(output_img.ncols, output_img.nrows)
+    scale_img_s = output_img.scale(page.scale_value, 0)
+    scale_img_l = output_img.scale(page.scale_value * 10, 0)
+    scale_img_s.save_PNG(page.get_path_to_image('small'))
+    scale_img_l.save_PNG(page.get_path_to_image('large'))
---
+++ rodan/tests/tasks.py
@@ -0,0 +1,15 @@
+from django.utils import unittest
+from rodan.models.projects import Page
+from rodan.jobs import utility
+import gamera.core
+
+
+class TestTask(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        gamera.core.init_gamera()
+    
+    def testGenerateThumbnails(self):
+        page = Page.objects.get(pk=2)
+        output_img = gamera.core.load_image(page.get_filename_for_job("rodan.jobs.binarisation.SimpleThresholdBinarise"))
+        utility.create_thumbnails(output_img, page)
