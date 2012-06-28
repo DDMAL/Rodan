53de4b8ef0fd454285d13857e70aed77f4c68557
Gregory Burlet <gregory.burlet@mail.mcgill.ca>
2012-06-28 17:51:22 -0400

    Neon Django API
    for modifying the underlying MEI document. Filename information isn't
    in yet ... I don't know where to get this.

--- rodan/jobs/neon.py
+++ rodan/jobs/neon.py
@@ -6,7 +6,23 @@ from django.conf.urls import patterns, url
 from rodan.models.jobs import JobType, JobBase
 
 neon_urls = patterns('rodan.views.neon',
-    url(r'^edit', 'edit'),
+    url(r'^edit/insert/neume', 'insert_neume'),
+    url(r'^edit/move/neume', 'move_neume'),
+    url(r'^edit/delete/neume', 'delete_neume'),
+    url(r'^edit/neumify', 'neumify'),
+    url(r'^edit/ungroup', 'ungroup'),
+    url(r'^edit/insert/division', 'insert_division'),
+    url(r'^edit/move/division', 'move_division'),
+    url(r'^edit/delete/division', 'delete_division'),
+    url(r'^edit/insert/dot', 'insert_dot'),
+    url(r'^edit/delete/dot', 'delete_dot'),
+    url(r'^edit/insert/clef', 'insert_clef'),
+    url(r'^edit/move/clef', 'move_clef'),
+    url(r'^edit/update/clef/shape', 'update_clef_shape'),
+    url(r'^edit/delete/clef', 'delete_clef'),
+    url(r'^edit/insert/custos', 'insert_custos'),
+    url(r'^edit/move/custos', 'move_custos'),
+    url(r'^edit/delete/custos', 'delete_custos')
 )
 
 @utils.rodan_task(inputs=None)
