cbd536a6e952de839ffd5db51a1abaf940481ac8
dellsystem <ilostwaldo@gmail.com>
2012-06-29 19:33:56 -0400

    Move SOLR_URL setting to settings.py

--- rodan/jobs/solr_indexing.py
+++ rodan/jobs/solr_indexing.py
@@ -1,4 +1,5 @@
 import gamera.core
+from django.conf import settings
 
 import utils
 import solr_resources.MEI2Solr
@@ -9,7 +10,7 @@ gamera.core.init_gamera()
 
 @utils.rodan_task(inputs='mei')
 def index_solr(mei_filepath, **kwargs):
-    solr_resources.MEI2Solr.processMeiFile(mei_filepath, kwargs['solr_server'],\
+    solr_resources.MEI2Solr.processMeiFile(mei_filepath, settings.SOLR_URL,\
         kwargs['longest_gram'], kwargs['shortest_gram'])
 
     return {
@@ -24,7 +25,6 @@ class SolrIndexing(JobBase):
     description = 'Indexes .mei data into SOLR'
     show_during_wf_create = True
     parameters = {
-        'solr_server': "http://localhost:8080",  # needs to be changed
         'shortest_gram': 2,
         'longest_gram': 9,
     }
