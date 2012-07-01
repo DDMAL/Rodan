50e1282d1f552760765d232ac7d7c9cbb2210d91
dellsystem <ilostwaldo@gmail.com>
2012-07-01 14:01:22 -0400

    Add link to perform a task on a specific page
    Closes #73

--- rodan/views/pages.py
+++ rodan/views/pages.py
@@ -9,6 +9,7 @@ from rodan.models.results import Result
 from rodan.utils import rodan_view
 from rodan.forms.workflows import WorkflowForm
 from rodan.models.jobs import JobType
+from rodan.views.projects import task as task_view
 
 
 @rodan_view(Page)
@@ -32,7 +33,10 @@ def view(request, page):
 
         steps.append(step)
 
+    user = request.user.get_profile() if request.user.is_authenticated() else None
+
     data = {
+        'next_available_job': page.get_next_job(user=user),
         'medium_thumbnail': page.get_thumb_url(size=settings.MEDIUM_THUMBNAIL),
         'page': page,
         'steps': steps,
@@ -46,8 +50,7 @@ def view(request, page):
 # Called when submiting the form on the task page
 def process(request, page, job):
     if request.method != 'POST':
-        # Temp - should redirect to the task page
-        return redirect('/')
+        return task_view(request, job_slug=job.slug, page_id=page.id)
     else:
         job_object = job.get_object()
         kwargs = {}
