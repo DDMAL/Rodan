bc91ce0d072afb7a8381da791203aafab349117a
Anton Khelou <anton_midi@hotmail.com>
2012-07-05 13:23:42 -0400

    Fix crash issue on workflow creation step
    it would crash because the save overrides what was created by the workflow, making the workflow creation step crash(if you get to the workflow creation before the task completes) as soon as the create_thumbnails_task() would call save()

--- rodan/jobs/misc_tasks.py
+++ rodan/jobs/misc_tasks.py
@@ -12,4 +12,3 @@ def create_thumbnails_task(page_id, image_path, thumbnail_sizes):
         thumb_path = page.get_thumb_path(size=thumbnail_size)
         utils.create_thumbnail(image_path, thumb_path, thumbnail_size)
     page.is_ready = True
-    page.save()
