d40e3794c0fedc371c3c56e5d918d19d14fb1db6
dellsystem <ilostwaldo@gmail.com>
2012-07-09 20:39:48 -0400

    Add page.save() to create_thumbnails_task
    @antonkhelou This needs to be here, otherwise the change to is_ready
    simply isn't saved. I'm not sure why it would crash during the workflow
    creation step (bc91ce0d072afb7a8381da791203aafab349117a) but it seems
    that the simple fix is to prevent workflow creation until all the
    thumnails have been created.

--- rodan/jobs/misc_tasks.py
+++ rodan/jobs/misc_tasks.py
@@ -12,3 +12,4 @@ def create_thumbnails_task(page_id, image_path, thumbnail_sizes):
         thumb_path = page.get_thumb_path(size=thumbnail_size)
         utils.create_thumbnail(image_path, thumb_path, thumbnail_size)
     page.is_ready = True
+    page.save()
