c62b3cfeb297f032b3e24ea1efe8c4ef8004936e
Gregory Burlet <gregory.burlet@mail.mcgill.ca>
2012-09-25 14:01:12 -0400

    Combine MEI job
    Integrated the barline finder mei combination script into RODAN.
    Currently it doesn't look like the sequence of mei paths sent to the
    combination script is ordered according to the ordering of the uploaded
    pages in the project. This needs to be done.

---
+++ rodan/jobs/mei_resources/meicombine.py
@@ -0,0 +1,106 @@
+import argparse
+import datetime
+import os
+
+from pymei import XmlImport, XmlExport, MeiElement
+
+class MeiCombiner:
+    '''
+    Combines mei files created by the barline finding algorithm.
+    '''
+
+    def __init__(self, input_mei_paths):
+        '''
+        PARAMETERS
+        ----------
+        input_mei_paths {list}: list of mei paths to combine
+        '''
+
+        self._input_mei_paths = input_mei_paths
+        if len(self._input_mei_paths):
+            self._meidoc = XmlImport.documentFromFile(self._input_mei_paths[0])
+        else:
+            self._meidoc = None
+
+    def combine(self):
+        if self._meidoc and len(self._input_mei_paths) > 1:
+            base_facsimile = self._meidoc.getElementsByName('facsimile')[0]
+            base_section = self._meidoc.getElementsByName('section')[0]
+            for f in self._input_mei_paths[1:]:
+                mei = XmlImport.documentFromFile(f)
+
+                # combine surface
+                surface = mei.getElementsByName('surface')
+                if len(surface):
+                    # have to remove the child from the old document in memory
+                    # or else pymei segfaults ...
+                    surface[0].getParent().removeChild(surface[0])
+                    base_facsimile.addChild(surface[0])
+
+                # combine measures
+                pb = MeiElement('pb')
+                base_section.addChild(pb)
+
+                # get last measure number
+                measures = base_section.getChildrenByName('measure')
+                last_measure_n = int(measures[-1].getAttribute('n').value)
+
+                new_section = mei.getElementsByName('section')[0]
+                music_elements = new_section.getChildren()
+
+                for e in music_elements:
+                    if e.getName() == 'measure':
+                        last_measure_n += 1
+                        e.addAttribute('n', str(last_measure_n))
+
+                    base_section.addChild(e)
+
+                # remove all musical elements from the old document or else pymei segfaults
+                new_section.getParent().deleteAllChildren()
+
+            self._add_revision()
+
+    def _add_revision(self):
+        # add a revision
+        today = datetime.date.today().isoformat()
+        change = MeiElement('change')
+
+        # get last change number
+        changes = self._meidoc.getElementsByName('change')
+        if len(changes):
+            last_change = int(changes[-1].getAttribute('n').value)
+
+        change.addAttribute('n', str(last_change+1))
+        resp_stmt = MeiElement('respStmt')
+        corp_name = MeiElement('corpName')
+        corp_name.setValue('Distributed Digital Music Archives and Libraries Lab (DDMAL)')
+        change_desc = MeiElement('changeDesc')
+        ref = MeiElement('ref')
+        p = MeiElement('p')
+        application = self._meidoc.getElementsByName('application')
+        app_name = 'RODAN/barlineFinder'
+        if len(application):
+            ref.addAttribute('target', '#'+application[0].getId())
+            ref.setValue(app_name)
+            ref.setTail('.')
+            p.addChild(ref)
+
+        p.setValue('Combining individual page MEIs using ')
+        date = MeiElement('date')
+        date.setValue(today)
+
+        revision_descs = self._meidoc.getElementsByName('revisionDesc')
+        if len(revision_descs):
+            revision_descs[0].addChild(change)
+            change.addChild(resp_stmt)
+            resp_stmt.addChild(corp_name)
+            change.addChild(change_desc)
+            change_desc.addChild(p)
+            change.addChild(date)
+
+    def write_mei(self, output_path):
+        if self._meidoc:
+            XmlExport.meiDocumentToFile(self._meidoc, output_path)
+
+    def get_mei(self):
+        return self._meidoc
--- rodan/jobs/write_mei.py
+++ rodan/jobs/write_mei.py
@@ -3,31 +3,31 @@ from rodan.models.jobs import JobType, JobBase
 from rodan.models import Page
 from pymei import XmlImport
 
+from mei_resources.meicombine import MeiCombiner
 
 @utils.rodan_multi_task(inputs='mei')
 def combine_mei(mei_path, **kwargs):
     target_page_ids = kwargs['target_page_ids']
 
-    mei_docs = []
-
-    # get the mei of the current page
-    mei_docs.append(XmlImport.documentFromFile(str(mei_path)))
+    # the first element of the list is the page that's sending the job
+    # and the rest of the list are all other pages waiting to get merged
+    mei_paths = [str(mei_path)]
 
     # get the meis of all other pages
     for page_id in target_page_ids:
         page = Page.objects.get(pk=page_id)
         page_mei_path = page.get_mei_path()
-        mei_docs.append(XmlImport.documentFromFile(str(page_mei_path)))
+        mei_paths.append(str(page_mei_path))
 
-    #use mei_docs to merge into one... the first elem of the list is the page thats sending the job
-    # and the rest of the list are all other pages waiting to get merged
-    all_mei = mei_docs[0]
+    # the list of paths going into the combiner should be an ordered
+    # sequence corresponding to the sequence of uploaded pages
+    mc = MeiCombiner(mei_paths)
+    mc.combine()
+    combined_mei = mc.get_mei()
 
     return {
-        'mei': all_mei,
-        'page_ids': target_page_ids
+        'mei': combined_mei
     }
-
 
 class CombineMei(JobBase):
     name = 'Combine all MEI files'
@@ -37,5 +37,6 @@ class CombineMei(JobBase):
     description = 'Make a single MEI file out of all images'
     show_during_wf_create = True
     is_automatic = True
+    outputs_image = False
     all_pages = True
     task = combine_mei
