3c99db0dd7ca1baacd91e798e5596ac1b7153de1
Andrew Hankinson <andrew.hankinson@gmail.com>
2012-11-17 12:45:49 -0500

    Fix up user_barline_input module
    Also cleaned up a potentially hazardous nested if statement.

--- rodan/jobs/user_barline_input.py
+++ rodan/jobs/user_barline_input.py
@@ -1,15 +1,14 @@
-import utils
-import gamera
-import json
-
+from rodan.jobs.utils import rodan_task
 from rodan.models.jobs import JobType, JobBase
 
-@utils.rodan_task(inputs='tiff')
+
+@rodan_task(inputs='tiff')
 def barline_input(image_filepath, **kwargs):
     print kwargs
     return {
         'txt': kwargs['sequence']
     }
+
 
 class BarlineInput(JobBase):
     input_type = JobType.BORDER_REMOVE_IMAGE
@@ -23,15 +22,22 @@ class BarlineInput(JobBase):
         'sequence': ''
     }
     task = barline_input
-    
+
     def get_context(self, page):
         prev_structure = None
         prev_page = page.get_previous_page()
-        if prev_page:
-            prev_txt_path = prev_page.get_latest_file_path('txt')
-            if prev_txt_path:
-                structure = open(prev_txt_path)
-                prev_structure = structure.read()
+        if not prev_page:
+            return {}
+
+        prev_txt_path = prev_page.get_latest_file_path('txt')
+
+        if not prev_txt_path:
+            return {}
+
+        structure = open(prev_txt_path)
+        prev_structure = structure.read()
+        structure.close()
+
         return {
             'previous_structure': prev_structure
         }
