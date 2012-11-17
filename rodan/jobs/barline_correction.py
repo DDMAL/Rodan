0ad9dffd936cfd421b65234300c04941c45870b3
Andrew Hankinson <andrew.hankinson@gmail.com>
2012-11-17 12:25:54 -0500

    Fix up barline_correction module
    Also closed a file handler that was not being closed.

--- rodan/jobs/barline_correction.py
+++ rodan/jobs/barline_correction.py
@@ -1,16 +1,15 @@
-import utils
-
 import os
-
 from django.conf import settings
-
+from rodan.jobs.utils import rodan_task, create_dirs
 from rodan.models.jobs import JobType, JobBase
 from rodan.models.projects import Job
 
-@utils.rodan_task(inputs=[])
+
+@rodan_task(inputs=[])
 def barline_correction(**kwargs):
     return {
     }
+
 
 class BarlineCorrection(JobBase):
     name = 'Barline Correction'
@@ -29,9 +28,11 @@ class BarlineCorrection(JobBase):
         input_mei_path = page.get_latest_file_path('mei')
         j = Job.objects.get(pk=self.slug)
         output_path = page.get_job_path(j, 'mei')
-        utils.create_dirs(output_path)
+        create_dirs(output_path)
         if not os.path.exists(output_path):
-            open(output_path, 'w').write(open(input_mei_path).read())
+            f = open(output_path, 'w')
+            f.write(open(input_mei_path).read())
+            f.close()
 
         mei_url = settings.MEDIA_URL + page._get_job_path(j, 'mei')
         return {
