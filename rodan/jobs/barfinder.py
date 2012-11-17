69d30ae03af4cd6bc657a552329447fb42d7fe7f
Andrew Hankinson <andrew.hankinson@gmail.com>
2012-11-17 12:23:27 -0500

    Code cleanup for bar finder module

--- rodan/jobs/barfinder.py
+++ rodan/jobs/barfinder.py
@@ -1,11 +1,14 @@
-import utils
-import gamera
-
+from rodan.jobs.utils import rodan_task
 from rodan.models.jobs import JobType, JobBase
+
 from barfinder_resources.barfinder import BarlineFinder
 from barfinder_resources.meicreate import BarlineDataConverter
 
-@utils.rodan_task(inputs=('tiff','txt'))
+import gamera.core
+gamera.core.init_gamera()
+
+
+@rodan_task(inputs=('tiff', 'txt'))
 def barfinder(image_filepath, sg_hint_filepath, **kwargs):
     input_image = gamera.core.load_image(image_filepath)
     image_width = input_image.width
@@ -19,10 +22,9 @@ def barfinder(image_filepath, sg_hint_filepath, **kwargs):
     # get the staff group hint inputted by the user
     with open(sg_hint_filepath, 'r') as sg_file:
         sg_hint = sg_file.read()
-        
     bar_finder = BarlineFinder()
     staff_bb, bar_bb = bar_finder.process_file(input_image, sg_hint, image_dpi)
-    
+
     bar_converter = BarlineDataConverter(staff_bb, bar_bb)
     bar_converter.bardata_to_mei(sg_hint, image_filepath, image_width, image_height, image_dpi)
     mei_file = bar_converter.get_wrapped_mei()
@@ -30,6 +32,7 @@ def barfinder(image_filepath, sg_hint_filepath, **kwargs):
     return {
         'mei': mei_file
     }
+
 
 class BarFinder(JobBase):
     slug = 'bar-finder'
