622a31395418d40346ef5e99ed845fd25ccd03c9
Andrew Hankinson <andrew.hankinson@gmail.com>
2012-11-17 02:17:15 -0500

    Formatting for context processors

--- rodan/context_processors.py
+++ rodan/context_processors.py
@@ -1,11 +1,12 @@
 from django.core.urlresolvers import reverse
-
 from rodan.models.projects import Project
+
 
 def list_projects(request):
     return {
-        'projects': Project.objects.order_by('-id').all()
+        'project_list': Project.objects.order_by('-id').all()
     }
+
 
 def login_url(request):
     return {
