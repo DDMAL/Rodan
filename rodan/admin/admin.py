c11ccc55b5aa465969c1682e8189f02d9d1601e0
Andrew Hankinson <andrew.hankinson@gmail.com>
2012-11-18 22:04:30 -0500

    Update admin interface

--- rodan/admin/admin.py
+++ rodan/admin/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
 from rodan.models.project import Project
 from rodan.models.page import Page
 from rodan.models.workflow import Workflow
@@ -6,6 +9,7 @@ from rodan.models.jobitem import JobItem
 from rodan.models.job import Job
 from rodan.models.parameter import Parameter
 from rodan.models.result import Result
+from rodan.models.rodanuser import RodanUser
 
 
 class JobItemInline(admin.StackedInline):
@@ -16,6 +20,20 @@ class JobItemInline(admin.StackedInline):
 class WorkflowAdmin(admin.ModelAdmin):
     inlines = [JobItemInline]
 
+
+class UserProfileInline(admin.StackedInline):
+    model = RodanUser
+    can_delete = False
+    verbose_name_plural = 'profile'
+
+
+class RodanUserAdmin(UserAdmin):
+    inlines = (UserProfileInline, )
+
+admin.site.unregister(User)
+admin.site.register(User, RodanUserAdmin)
+
+admin.site.register(RodanUser)
 admin.site.register(Project)
 admin.site.register(Page)
 admin.site.register(Workflow, WorkflowAdmin)
