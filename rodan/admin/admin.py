a1540eb9dcef7ddcff2e90a4b8d0be7333b68d70
Andrew Hankinson <andrew.hankinson@gmail.com>
2012-12-10 00:05:17 -0500

    Updating admin site models

--- rodan/admin/admin.py
+++ rodan/admin/admin.py
@@ -5,39 +5,37 @@ from django.contrib.auth.models import User
 from rodan.models.project import Project
 from rodan.models.page import Page
 from rodan.models.workflow import Workflow
-from rodan.models.jobitem import JobItem
+from rodan.models.workflowjob import WorkflowJob
 from rodan.models.job import Job
-from rodan.models.parameter import Parameter
 from rodan.models.result import Result
-from rodan.models.rodanuser import RodanUser
+# from rodan.models.rodanuser import RodanUser
 
 
-class JobItemInline(admin.StackedInline):
-    model = JobItem
+class WorkflowJobInline(admin.StackedInline):
+    model = WorkflowJob
     extra = 5
 
 
 class WorkflowAdmin(admin.ModelAdmin):
-    inlines = [JobItemInline]
+    inlines = [WorkflowJobInline]
 
 
-class UserProfileInline(admin.StackedInline):
-    model = RodanUser
-    can_delete = False
-    verbose_name_plural = 'profile'
+# class UserProfileInline(admin.StackedInline):
+#     model = RodanUser
+#     can_delete = False
+#     verbose_name_plural = 'profile'
 
 
-class RodanUserAdmin(UserAdmin):
-    inlines = (UserProfileInline, )
+# class RodanUserAdmin(UserAdmin):
+#     inlines = (UserProfileInline, )
 
-admin.site.unregister(User)
-admin.site.register(User, RodanUserAdmin)
+# admin.site.unregister(User)
+# admin.site.register(User, RodanUserAdmin)
 
-admin.site.register(RodanUser)
+# admin.site.register(RodanUser)
 admin.site.register(Project)
 admin.site.register(Page)
 admin.site.register(Workflow, WorkflowAdmin)
 admin.site.register(Job)
-admin.site.register(JobItem)
-admin.site.register(Parameter)
+admin.site.register(WorkflowJob)
 admin.site.register(Result)
