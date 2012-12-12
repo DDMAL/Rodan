f9b2aca79ced81c841a9acbe3878958f2acf7ab6
Andrew Hankinson <andrew.hankinson@gmail.com>
2012-12-11 23:01:33 -0500

    Update workflow model

--- rodan/models/workflow.py
+++ rodan/models/workflow.py
@@ -3,8 +3,9 @@ from django.db import models
 
 class Workflow(models.Model):
     name = models.CharField(max_length=255)
+    project = models.ForeignKey('rodan.Project', related_name="workflows")
     jobs = models.ManyToManyField('rodan.Job', through='WorkflowJob', null=True, blank=True)
-    pages = models.ManyToManyField('rodan.Page')
+    pages = models.ManyToManyField('rodan.Page', related_name="workflows")
     description = models.TextField()
     has_started = models.BooleanField(default=False)
 
