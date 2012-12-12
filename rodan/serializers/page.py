310422c7e00e68e9744bd0b97147c3f8a12a1a1d
Andrew Hankinson <andrew.hankinson@gmail.com>
2012-12-11 23:03:02 -0500

    page and project serizlizers

--- rodan/serializers/page.py
+++ rodan/serializers/page.py
@@ -4,7 +4,21 @@ from rest_framework import serializers
 
 class PageSerializer(serializers.HyperlinkedModelSerializer):
     project = serializers.HyperlinkedRelatedField(view_name="project-detail")
+    page_image = serializers.FileField()
+    page_order = serializers.IntegerField()
 
     class Meta:
         model = Page
+        read_only_fields = ('created', 'updated')
         fields = ("url", "project", "page_image", "page_order", "created", "updated")
+
+
+class PageModelSerializer(serializers.ModelSerializer):
+    # project = serializers.HyperlinkedRelatedField(view_name="project-detail")
+    page_image = serializers.FileField()
+    page_order = serializers.IntegerField()
+
+    class Meta:
+        model = Page
+        read_only_fields = ('created', 'updated')
+        fields = ("page_image", "page_order", "created", "updated")
--- rodan/serializers/project.py
+++ rodan/serializers/project.py
@@ -4,8 +4,9 @@ from rest_framework import serializers
 
 class ProjectSerializer(serializers.HyperlinkedModelSerializer):
     creator = serializers.HyperlinkedRelatedField(view_name="user-detail")
-    # pages = serializers.HyperlinkedRelatedField(view_name="page-detail")
+    pages = serializers.ManyHyperlinkedRelatedField(view_name="page-detail")
+    workflows = serializers.ManyHyperlinkedRelatedField(view_name="workflow-detail")
 
     class Meta:
         model = Project
-        fields = ("url", "name", "description", "creator")
+        fields = ("url", "name", "description", "creator", "pages", "workflows")
