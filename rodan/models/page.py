6f45fa87d86d76b173a1ca66381179f0f3b1fa93
Andrew Hankinson <andrew.hankinson@gmail.com>
2012-12-11 23:01:17 -0500

    update page models

--- rodan/models/page.py
+++ rodan/models/page.py
@@ -1,8 +1,9 @@
 from django.db import models
+from rodan.models.project import Project
 
 
 class Page(models.Model):
-    project = models.ForeignKey('rodan.Project')
+    project = models.ForeignKey(Project, related_name="pages")
     page_image = models.FileField(upload_to="images", null=True)
     page_order = models.IntegerField(null=True)
 
