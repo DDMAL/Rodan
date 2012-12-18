5924d91fb53c352ead5fd2daf4310f6c7fe5c0da
Andrew Hankinson <andrew.hankinson@gmail.com>
2012-12-18 15:50:59 -0500

    Return page objects in the serialized output of projects.

--- rodan/serializers/project.py
+++ rodan/serializers/project.py
@@ -1,10 +1,11 @@
 from rodan.models.project import Project
 from rest_framework import serializers
+from rodan.serializers.page import PageSerializer
 
 
 class ProjectSerializer(serializers.HyperlinkedModelSerializer):
     creator = serializers.HyperlinkedRelatedField(view_name="user-detail")
-    pages = serializers.ManyHyperlinkedRelatedField(view_name="page-detail")
+    pages = PageSerializer()
     workflows = serializers.ManyHyperlinkedRelatedField(view_name="workflow-detail")
 
     class Meta:
