1349d91d054ea08d62a99cf382a00d00b5a435e3
Andrew Hankinson <andrew.hankinson@gmail.com>
2012-12-31 17:51:59 -0500

    Changes to the job model

--- rodan/models/job.py
+++ rodan/models/job.py
@@ -1,16 +1,19 @@
 from django.db import models
+from django_extensions.db.fields import json
 
 
 class Job(models.Model):
     name = models.CharField(max_length=255)
-    slug = models.CharField(max_length=255)
-    module = models.CharField(max_length=255)
-    enabled = models.CharField(max_length=255)
+    author = models.CharField(max_length=255, blank=True, null=True)
+    category = models.CharField(max_length=255, blank=True, null=True)
+
+    input_types = json.JSONField()
+    output_types = json.JSONField()
+    arguments = json.JSONField(blank=True, null=True)
+
+    is_enabled = models.BooleanField()
     is_automatic = models.BooleanField()
     is_required = models.BooleanField()
-
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         app_label = 'rodan'
