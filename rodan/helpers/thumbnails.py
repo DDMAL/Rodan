5febd924eb9ede1dfbc33df1a95c60de9f4c777a
Andrew Hankinson <andrew.hankinson@gmail.com>
2013-01-19 17:33:38 -0500

    Fix thumbnail generation

--- rodan/helpers/thumbnails.py
+++ rodan/helpers/thumbnails.py
@@ -1,5 +1,4 @@
 import os
-from django.conf import settings
 from celery import task
 
 import PIL.Image
@@ -11,9 +10,8 @@ def create_thumbnail(image_path, thumb_path, thumbnail_size):
     image = PIL.Image.open(image_path).convert('RGB')
     width, height = image.size
 
-    if thumbnail_size != settings.ORIGINAL_SIZE:
-        dimensions = (thumbnail_size, int(width / float(thumbnail_size) * height))
-        image.thumbnail(dimensions, PIL.Image.ANTIALIAS)
+    dimensions = (thumbnail_size, int(width / float(thumbnail_size) * height))
+    image.thumbnail(dimensions, PIL.Image.ANTIALIAS)
 
     if not os.path.exists(os.path.dirname(thumb_path)):
         os.makedirs(os.path.dirname(thumb_path))
