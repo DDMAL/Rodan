63b29de43ef8dce4fa723d489e4c9efc9387841f
Andrew Hankinson <andrew.hankinson@gmail.com>
2013-01-19 17:32:58 -0500

    update the job model to use UUIDs for the pk

--- rodan/models/job.py
+++ rodan/models/job.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django_extensions.db.fields import json
+from uuidfield import UUIDField
 
 
 class Job(models.Model):
+    uuid = UUIDField(primary_key=True, auto=True)
     name = models.CharField(max_length=255)
     author = models.CharField(max_length=255, blank=True, null=True)
     category = models.CharField(max_length=255, blank=True, null=True)
