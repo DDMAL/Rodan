1a1611895cd89c9a9d7113f3bd6795c24c6ec581
Andrew Hankinson <andrew.hankinson@gmail.com>
2013-01-19 17:51:25 -0500

    Updating the way results are stored in the database

--- rodan/models/result.py
+++ rodan/models/result.py
@@ -1,9 +1,25 @@
+import os
 from django.db import models
+from rodan.models.workflowjob import WorkflowJob
+from rodan.models.page import Page
+from uuidfield import UUIDField
 
 
 class Result(models.Model):
-    workflow_job = models.ForeignKey('rodan.WorkflowJob')
-    page = models.ForeignKey('rodan.Page')
+
+    @property
+    def upload_path(self):
+        """ We will need to know the path where the results are being stored"""
+        return os.path.join("projects", str(self.page.project.pk), "pages", str(self.page.pk), "results", str(self.uuid))
+
+    def upload_fn(self, filename):
+        path = self.upload_path
+        return os.path.join(path, filename)
+
+    uuid = UUIDField(primary_key=True, auto=True)
+    workflow_job = models.ForeignKey(WorkflowJob)
+    page = models.ForeignKey(Page)
+    result = models.FileField(upload_to=upload_fn, null=True)
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
