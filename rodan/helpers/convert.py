e122c3da7d6bcbf285e2a6dbac4761d94979200d
Andrew Hankinson <andrew.hankinson@gmail.com>
2013-01-20 15:33:47 -0500

    Store the compat png file on the page model

--- rodan/helpers/convert.py
+++ rodan/helpers/convert.py
@@ -1,12 +1,23 @@
 import os
+import tempfile
+import shutil
 from celery import task
+from django.core.files import File
 import PIL.Image
 import PIL.ImageFile
 
 
 @task(name="rodan.helpers.convert.ensure_compatible", ignore_result=True)
 def ensure_compatible(page_object):
+    filename = "compat_image.png"
+
     image = PIL.Image.open(page_object.page_image.path).convert('RGB')
-    compatible_image_path = os.path.join(page_object.image_path, "compat_image.png")
-    image.save(compatible_image_path)
+    tmpdir = tempfile.mkdtemp()
+    image.save(os.path.join(tmpdir, filename))
+
+    compatible_image_path = os.path.join(page_object.image_path, filename)
+    f = open(os.path.join(tmpdir, filename), 'rb')
+    page_object.compat_page_image.save(compatible_image_path, File(f))
+
+    shutil.rmtree(tmpdir)
     return page_object
