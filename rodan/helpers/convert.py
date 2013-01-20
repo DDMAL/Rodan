7d2c99126784ad0ff3d83d7af41e5a9c57555552
Andrew Hankinson <andrew.hankinson@gmail.com>
2013-01-20 13:26:42 -0500

    create an ensure compatible task to automatically convert the page.

--- rodan/helpers/convert.py
+++ rodan/helpers/convert.py
@@ -1,19 +1,12 @@
 import os
 from celery import task
-
 import PIL.Image
 import PIL.ImageFile
 
 
 @task(name="rodan.helpers.convert.ensure_compatible", ignore_result=True)
-def ensure_compatible(image_path):
-    image = PIL.Image.open(image_path).convert('RGB')
-    width, height = image.size
-
-    # takes every image uploaded to Rodan and converts them to
-    # lossless PNG, ensuring that they are always compatible for
-    # future processing.
-
-    ## do conversion
-
-    return True
+def ensure_compatible(page_object):
+    image = PIL.Image.open(page_object.page_image.path).convert('RGB')
+    compatible_image_path = os.path.join(page_object.image_path, "compat_image.png")
+    image.save(compatible_image_path)
+    return page_object
