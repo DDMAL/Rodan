d8fdfaa990158cda741098d2cab1d96e36ee4757
Andrew Hankinson <andrew.hankinson@gmail.com>
2013-01-22 17:41:10 -0500

    Adding result serializer

---
+++ rodan/serializers/result.py
@@ -0,0 +1,14 @@
+from rodan.models.result import Result
+from rest_framework import serializers
+from rodan.serializers.page import PageSerializer
+from rodan.serializers.workflowjob import WorkflowJobSerializer
+
+
+class ResultSerializer(serializers.HyperlinkedModelSerializer):
+    page = PageSerializer()
+    workflow_job = WorkflowJobSerializer()
+    result = serializers.FileField()
+
+    class Meta:
+        model = Result
+        fields = ("url", "page", "workflow_job", "result", "created", "updated")
