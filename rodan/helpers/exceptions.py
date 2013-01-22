12cdbb371604f511e3efcc308199fa710dcc9c45
Andrew Hankinson <andrew.hankinson@gmail.com>
2013-01-22 17:42:18 -0500

    Add exceptions

---
+++ rodan/helpers/exceptions.py
@@ -0,0 +1,13 @@
+
+
+class RodanError(Exception):
+    """ BagIt Errors """
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return repr(self.message)
+
+
+class RodanJobError(RodanError):
+    pass
