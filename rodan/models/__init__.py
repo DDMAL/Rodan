9d065fc9610f2565a8af72501188c728d74e97cb
Andrew Hankinson <andrew.hankinson@gmail.com>
2013-01-22 17:41:21 -0500

    remove older imports

--- rodan/models/__init__.py
+++ rodan/models/__init__.py
@@ -2,7 +2,5 @@ from rodan.models.project import Project
 from rodan.models.job import Job
 from rodan.models.workflowjob import WorkflowJob
 from rodan.models.result import Result
-from rodan.models.resultfile import ResultFile
-from rodan.models.resulttask import ResultTask
 from rodan.models.page import Page
 from rodan.models.workflow import Workflow
