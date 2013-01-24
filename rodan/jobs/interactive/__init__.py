b7c5a987e82b4c5ece9446016a3b31739f7241ba
Anton Khelou <anton_midi@hotmail.com>
2013-01-24 14:57:48 -0500

    Add functionality for registering interactive tasks

---
+++ rodan/jobs/interactive/__init__.py
@@ -0,0 +1,1 @@
+print "Loading rodan interactive jobs"
---
+++ rodan/jobs/interactive/helpers.py
@@ -0,0 +1,68 @@
+import os
+import uuid
+import tempfile
+import shutil
+from django.core.files import File
+from celery import Task
+from celery import registry
+from rodan.models.job import Job
+from rodan.jobs import utils
+from rodan.models.result import Result
+import gamera.core
+
+
+class RodanInteractiveTask(Task):
+    max_retries = None
+    module_fn = None
+    module_settings = []
+    workflowjob_obj = None
+
+    # this flag will get set to `False` when the task has
+    # received human input and is ready to be run; otherwise
+    # it will re-queue itself indefinitely, waiting for more input.
+    needs_input = True
+
+    def run(self, job_data, *args, **kwargs):
+        if self.needs_input:
+            self.retry(job_data)
+        else:
+            # do stuff
+            pass
+
+    def retry(self, job_data, *args, **kwargs):
+        # do something like this
+        super(RodanInteractiveTask, self).retry(job_data, *args, **kwargs)
+
+
+def create_interactive_job_from_gamera_function(gamera_fn):
+    previously_loaded_modules = Job.objects.values_list('name', flat=True)
+
+    module_task = RodanInteractiveTask()
+    module_task.name = str(gamera_fn)
+    module_task.module_fn = gamera_fn
+    registry.tasks.register(module_task)
+
+    # skip the job creation if we've already
+    # stored a reference to this job in the database
+    if str(gamera_fn) in previously_loaded_modules:
+        return
+
+    input_types = utils.convert_input_type(gamera_fn.self_type)
+
+    output_types = None
+    if gamera_fn.return_type:
+        output_types = utils.convert_output_type(gamera_fn.return_type)
+
+    arguments = utils.convert_arg_list(gamera_fn.args.list)
+
+    j = Job(
+        name=str(gamera_fn),
+        author=gamera_fn.author,
+        description=gamera_fn.escape_docstring().replace("\\n", "\n").replace('\\"', '"'),
+        input_types=input_types,
+        output_types=output_types,
+        arguments=arguments,
+        enabled=True,
+        category=gamera_fn.module.category
+    )
+    j.save()
