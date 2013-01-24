c1f5d95b151cbf9bbb08aeb2189ae57d1bedf6fd
Andrew Hankinson <andrew.hankinson@gmail.com>
2013-01-24 15:05:47 -0500

    fixing settings

--- rodan/helpers/workflow.py
+++ rodan/helpers/workflow.py
@@ -19,17 +19,6 @@ def run_workflow(workflow_id):
         # construct the workflow by creating a task chain for each image.
         # To kick it off we will define a first "Result", which will simply
         # be a copy of the original image saved as a result.
-
-        # create a task chain from the workflow jobs for this image.
-        task_chain = []
-        for wjob in workflow_jobs:
-            rodan_task = registry.tasks[str(wjob.job.name)]
-            if wjob.job_settings:
-                rodan_task.module_settings = wjob.job_settings
-
-            rodan_task.workflowjob_obj = wjob
-            task_chain.append(rodan_task)
-
         # now we kick it off by passing this result to the first job in the chain
         f = open(os.path.join(page.image_path, "compat_file.png"), 'rb')
         r = Result(
@@ -39,14 +28,19 @@ def run_workflow(workflow_id):
         )
         r.save()
         r.result.save(os.path.join(page.image_path, "compat_file.png"), File(f))
+
         job_data = {
             'previous_result': r
         }
 
-        first_task = task_chain[0]
-        execute_tasks = [fn.s() for fn in task_chain[1:]]
+        res = None
+        for num, wjob in enumerate(workflow_jobs):
+            rodan_task = registry.tasks[str(wjob.job.name)]
+            if num == 0:
+                res = rodan_task.s(job_data, wjob.job_settings)
+                continue
+            else:
+                res.link(rodan_task.s(wjob.job_settings))
 
-        res = first_task.s(job_data)
-        for t in execute_tasks:
-            res.link(t)
+        # once we've done a task chain, let's kick it off.
         res.apply_async()
