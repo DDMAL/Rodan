2d3d2498e290e0d67ce041f4eb3fa51b8a98c7ab
Anton Khelou <a.khelou@gmail.com>
2013-01-24 19:28:23 -0500

    Add interactive crop job code skeleton

--- rodan/jobs/interactive/__init__.py
+++ rodan/jobs/interactive/__init__.py
@@ -1,3 +1,6 @@
 print "Loading rodan interactive jobs"
 from rodan.jobs.interactive.despeckle import despeckle
 despeckle.load_interactive_job()
+
+from rodan.jobs.interactive.crop import crop
+crop.load_interactive_job()
