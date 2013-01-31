d2d868f4bfce982a08c00547ffe04ba614ce83c8
Anton Khelou <a.khelou@gmail.com>
2013-01-31 17:55:20 -0500

    Modify despeckle to make use of rdn_despeckle
    rdn_despeckle is a wrapped despeckle function which can be found in rodan_plugins toolkit:
    
    https://github.com/DDMAL/rodan_plugins

--- rodan/jobs/interactive/despeckle/despeckle.py
+++ rodan/jobs/interactive/despeckle/despeckle.py
@@ -1,6 +1,6 @@
 from rodan.jobs.interactive.helpers import create_interactive_job_from_gamera_function
-from gamera.plugins.morphology import despeckle
+from gamera.toolkits.rodan_plugins.plugins.rdn_despeckle import rdn_despeckle
 
 
 def load_interactive_job():
-    create_interactive_job_from_gamera_function(despeckle)
+    create_interactive_job_from_gamera_function(rdn_despeckle)
