f06c0539c3763c3d8835ab4e9e001eb02844d545
Andrew Hankinson <andrew.hankinson@gmail.com>
2013-02-07 18:58:14 -0500

    Update workflow serializers

--- rodan/serializers/workflow.py
+++ rodan/serializers/workflow.py
@@ -5,12 +5,15 @@ from rodan.serializers.page import PageSerializer
 
 class WorkflowSerializer(serializers.HyperlinkedModelSerializer):
     project = serializers.HyperlinkedRelatedField(view_name="project-detail")
-    pages = PageSerializer()
+    # pages = PageSerializer()
+    pages = serializers.ManyHyperlinkedRelatedField(view_name="page-detail", null=True)
+    jobs = serializers.ManyHyperlinkedRelatedField(view_name="job-detail", null=True)
 
     class Meta:
         model = Workflow
         read_only_fields = ('created', 'updated')
         fields = ("url",
+                  "name",
                   "project",
                   "jobs",
                   "pages",
