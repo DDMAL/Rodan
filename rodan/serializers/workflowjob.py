aa62237cb8a3ce5f580aa0e58da3696e8aa3934c
Andrew Hankinson <andrew.hankinson@gmail.com>
2013-02-13 20:00:27 -0500

    Support more fields in workflowjob.py serializer

--- rodan/serializers/workflowjob.py
+++ rodan/serializers/workflowjob.py
@@ -5,10 +5,23 @@ from rest_framework import serializers
 class WorkflowJobSerializer(serializers.HyperlinkedModelSerializer):
     workflow = serializers.HyperlinkedRelatedField(view_name="workflow-detail")
     job = serializers.HyperlinkedRelatedField(view_name="job-detail")
+    sequence = serializers.IntegerField(required=False)
+    job_settings = serializers.CharField(required=False)
 
     name = serializers.Field(source="name")
+    input_pixel_types = serializers.Field(source="input_pixel_types")
+    output_pixel_types = serializers.Field(source="output_pixel_types")
 
     class Meta:
         model = WorkflowJob
         read_only_fields = ('created', 'updated')
-        fields = ("url", "workflow", "name", "job", "sequence", "job_settings", "created", "updated")
+        fields = ("url",
+                "workflow",
+                "input_pixel_types",
+                "output_pixel_types",
+                "name",
+                "job",
+                "sequence",
+                "job_settings",
+                "created",
+                "updated")
