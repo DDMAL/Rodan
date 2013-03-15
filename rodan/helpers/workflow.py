36463b3300c59a08a35e7b1da21301eb685ce735
Anton Khelou <a.khelou@gmail.com>
2013-03-15 14:38:53 -0400

    Change workflow and task execution code
    -No "copy task" result creation at the beginning of every workflow to initiate the task execution process
    --Now uses WorkflowJob uuid as the job_data driving the task execution.
    --The task execution now works with current WorkflowJob rather than previous Result

--- rodan/helpers/workflow.py
+++ rodan/helpers/workflow.py
@@ -16,20 +16,9 @@ def run_workflow(workflow_id):
 
     for page in pages:
         workflow_jobs = WorkflowJob.objects.filter(workflow__uuid=workflow_id, page=page).order_by('sequence')
-        # construct the workflow by creating a task chain for each image.
-        # To kick it off we will define a first "Result", which will simply
-        # be a copy of the original image saved as a result.
-        # now we kick it off by passing this result to the first job in the chain
-        f = open(os.path.join(page.image_path, "compat_file.png"), 'rb')
-        r = Result(
-            workflow_job=workflow_jobs[0],
-            task_name="rodan.initial.copy_task"
-        )
-        r.save()
-        r.result.save(os.path.join(page.image_path, "compat_file.png"), File(f))
 
         job_data = {
-            'previous_result': r
+            'current_wf_job_uuid': workflow_jobs[0].uuid,
         }
 
         job_chain = []
