2ee47eed2835fbe955d5c0b31161aa708a6639c3
Anton Khelou <a.khelou@gmail.com>
2013-03-15 16:42:27 -0400

    Add last_run field to Workflow model

--- rodan/models/workflow.py
+++ rodan/models/workflow.py
@@ -15,6 +15,8 @@ class Workflow(models.Model):
     description = models.TextField(blank=True, null=True)
     has_started = models.BooleanField(default=False)
 
+    last_run = models.IntegerField(default=0)
+
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
