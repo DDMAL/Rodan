6b543e7d7cee68acaa31f2b580f93e7c7e086851
Andrew Hankinson <andrew.hankinson@gmail.com>
2013-03-21 16:05:30 -0400

    Override the patch method to support adding pages to the workflow model

--- rodan/views/main.py
+++ rodan/views/main.py
@@ -1,8 +1,10 @@
 import os
 import json
+import urlparse
 from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.views.decorators.csrf import ensure_csrf_cookie
+from django.core.urlresolvers import resolve
 
 from rest_framework import generics
 from rest_framework import permissions
@@ -87,6 +89,39 @@ class WorkflowDetail(generics.RetrieveUpdateDestroyAPIView):
     model = Workflow
     permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
     serializer_class = WorkflowSerializer
+
+    def patch(self, request, pk, *args, **kwargs):
+        print "Patch called"
+        kwargs['partial'] = True
+
+        workflow = Workflow.objects.get(pk=pk)
+        if not workflow:
+            return Response({}, status=status.HTTP_404_NOT_FOUND)
+
+        pages = request.DATA.get('pages')
+        if pages:
+            for page in pages:
+                value = urlparse.urlparse(page['url']).path
+                # resolve the URL we're passed to a page object
+                try:
+                    p = resolve(value)
+                except:
+                    return Response({'error': 'Could not resolve path to page object'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+                # check if the page already exists on this workflow. If so, we skip it.
+                relationship_exists = workflow.pages.filter(pk=p.kwargs.get('pk')).exists()
+                if relationship_exists:
+                    continue
+
+                # now use the pk to grab the Page object from the database
+                page_obj = Page.objects.get(pk=p.kwargs.get('pk'))
+                # finally, add this page to the workflow
+                if not page_obj:
+                    return Response({'error': 'Page Object was not found'}, status=status.HTTP_404_NOT_FOUND)
+
+                workflow.pages.add(page_obj)
+
+        return self.update(request, *args, **kwargs)
 
 
 class WorkflowJobList(generics.ListCreateAPIView):
