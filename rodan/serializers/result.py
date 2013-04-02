da3356df5d04cf74e3f8cc5f9968f00be2f7415a
Andrew Hankinson <andrew.hankinson@gmail.com>
2013-04-02 15:03:40 -0400

    Formatting

--- rodan/serializers/result.py
+++ rodan/serializers/result.py
@@ -1,14 +1,12 @@
 from rodan.models.result import Result
 from rest_framework import serializers
-from rodan.serializers.page import PageSerializer
 from rodan.serializers.workflowjob import WorkflowJobSerializer
 
 
 class ResultSerializer(serializers.HyperlinkedModelSerializer):
-    page = PageSerializer()
     workflow_job = WorkflowJobSerializer()
     result = serializers.FileField(allow_empty_file=True)
 
     class Meta:
         model = Result
-        fields = ("url", "page", "workflow_job", "task_name", "result", "created", "updated")
+        fields = ("url", "result", "workflow_job", "task_name", "result", "created", "updated")
--- rodan/serializers/workflow.py
+++ rodan/serializers/workflow.py
@@ -2,18 +2,13 @@ from rodan.models.workflow import Workflow
 from rest_framework import serializers
 
 from rodan.serializers.workflowjob import WorkflowJobSerializer
-from rodan.serializers.job import JobSerializer
 from rodan.serializers.page import PageSerializer
 
 
 class WorkflowSerializer(serializers.HyperlinkedModelSerializer):
     project = serializers.HyperlinkedRelatedField(view_name="project-detail")
-    # pages = serializers.ManyHyperlinkedRelatedField(view_name="page-detail", null=True)
     pages = PageSerializer()
-    # jobs = JobSerializer()
-
     wjobs = WorkflowJobSerializer()
-
     uuid = serializers.Field(source='uuid')
 
     class Meta:
