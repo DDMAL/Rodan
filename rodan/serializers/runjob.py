ebe802ae48470cdc6fe16371ed6362932b8fe0ea
Andrew Hankinson <andrew.hankinson@gmail.com>
2013-04-14 01:33:39 -0400

    Updating runjob serializer

--- rodan/serializers/runjob.py
+++ rodan/serializers/runjob.py
@@ -8,6 +8,10 @@ class ResultRunJobSerializer(serializers.HyperlinkedModelSerializer):
         We define a simple serializer here to avoid a circular import
         with the 'normal' ResultSerializer.
     """
+    small_thumb_url = serializers.Field(source="small_thumb_url")
+    medium_thumb_url = serializers.Field(source="medium_thumb_url")
+    result = serializers.Field(source="image_url")
+
     class Meta:
         model = Result
 
@@ -17,11 +21,13 @@ class RunJobSerializer(serializers.HyperlinkedModelSerializer):
     job_settings = serializers.CharField()
     # result = ResultSerializer()
     result = ResultRunJobSerializer()
+    job_name = serializers.Field(source="job_name")
 
     class Meta:
         model = RunJob
         read_only_fields = ("created", "updated")
         fields = ('url',
+                  'job_name',
                   'workflow_run',
                   'workflow_job',
                   'sequence',
