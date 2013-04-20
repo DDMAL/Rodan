4c0a5541e50b00ace7124a20d96fe98ea0408c0b
Andrew Hankinson <andrew.hankinson@gmail.com>
2013-04-20 18:21:28 -0400

    Create an auth token when a user is created

--- rodan/views/user.py
+++ rodan/views/user.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework.authtoken.models import Token
 
 from rodan.serializers.user import UserSerializer, UserListSerializer
 
@@ -18,3 +22,7 @@ class UserDetail(generics.RetrieveAPIView):
     model = User
     permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
     serializer_class = UserSerializer
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
