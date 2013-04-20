37336b9e58e3c31ead0d99107aa0a73bb22aebe7
Andrew Hankinson <andrew.hankinson@gmail.com>
2013-04-20 19:12:43 -0400

    Prevent run job creation and modification

--- rodan/views/runjob.py
+++ rodan/views/runjob.py
@@ -5,14 +5,14 @@ from rodan.models.runjob import RunJob
 from rodan.serializers.runjob import RunJobSerializer
 
 
-class RunJobList(generics.ListCreateAPIView):
+class RunJobList(generics.ListAPIView):
     model = RunJob
     permission_classes = (permissions.IsAuthenticated, )
     serializer_class = RunJobSerializer
     paginate_by = None
 
 
-class RunJobDetail(generics.RetrieveUpdateDestroyAPIView):
+class RunJobDetail(generics.RetrieveUpdateAPIView):
     model = RunJob
     permission_classes = (permissions.IsAuthenticated, )
     serializer_class = RunJobSerializer
