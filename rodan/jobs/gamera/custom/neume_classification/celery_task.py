9dd6026e7fc1dbd2de746f4b6818cb1180cd5f1c
Deepanjan Roy <deepanjan.roy@mail.mcgill.ca>
2013-06-18 17:23:44 -0400

    New: Automatic Classification Job
    Now fully implemented and functional.

--- rodan/jobs/gamera/custom/neume_classification/celery_task.py
+++ rodan/jobs/gamera/custom/neume_classification/celery_task.py
@@ -1,21 +1,23 @@
-from celery import Task
-from rodan.models.runjob import RunJob
-from rodan.models.runjob import RunJobStatus
-from rodan.models.result import Result
-from rodan.helpers.thumbnails import create_thumbnails
-from gamera.core import init_gamera, load_image
-from django.core.files import File
-from rodan.jobs.util import taskutil
+import os
+import shutil
+import tarfile
+
 import gamera.core
 import gamera.gamera_xml
 import gamera.classify
 import gamera.knn
-from gamera import classify
+from gamera.core import init_gamera, load_image
+from celery import Task
+from django.core.files import File
 
-import os
-import shutil
 from rodan.models.classifier import Classifier
 from rodan.models.pageglyphs import PageGlyphs
+from rodan.models.runjob import RunJob
+from rodan.models.runjob import RunJobStatus
+from rodan.models.result import Result
+from rodan.jobs.util import taskutil
+from rodan.helpers.thumbnails import create_thumbnails
+from rodan.settings import PACKAGE
 
 
 class ManualClassificationTask(Task):
@@ -93,56 +95,80 @@ class ManualClassificationTask(Task):
         runjob.save()
 
 
-#class NonInteractiveClassificationTask(Task):
-#    max_retries = None
-#    name = 'gamera.custom.neume_classification.automatic_classification'
-#    settings = [{'default':1,'has_default':True,'rng':(1,1048576),'name':'num_k','type':'int'},
-#                {'default':4,'has_default':True,'rng':(-1048576,1048576),'name':'max_parts_per_group','type':'int'},
-#                {'default':16,'has_default':True,'rng':(-1048576,1048576),'name':'max_graph_size','type':'int'},
-#                {'default':-1,'has_default':True,'rng':(-1048576,1048576),'name':'tolerance','type':'int'},
-#                {'default': None,'has_default':True,'name':'polygon_outer_points','type':'json'},
-#                {'default': 0,'has_default':True,'rng':(-1048576,1048576),'name':'image_width','type':'int'}]
-#
-#    def process_image(task_image, settings):
-#        # Code borrowed from the old rodan classification job.
-#        
-#        # Is there a way to do a dropdown menu or a checkbox menu in the client instrea of using features='all'?
-#        cknn = gamera.knn.kNNNonInteractive(settings.CLASSIFIER_XML, features='all', perform_splits=True, num_k=settings['num_k'])
-#        func = gamera.classify.BoundingBoxGroupingFunction(2)
-#        ccs = task_image.cc_analysis()
-##
-#        cs_image = cknn.group_and_update_list_automatic(ccs,
-#                                                        grouping_function=func,
-#                                                        max_parts_per_group=4,
-#                                                        max_graph_size=16)
-##
-#        cknn.generate_features_on_glyphs(cs_image)
-#        return gamera.gamera_xml.WriteXMLFile(glyphs=cs_image, with_features=True)
-##
-##
-#    def run(self, result_id, runjob_id, *args, **kwargs):
-#        runjob = RunJob.objects.get(pk=runjob_id)
-#        taskutil.set_running(runjob)
-#        page_url = taskutil.get_page_url(runjob, result_id)
-#        settings = taskutil.get_settings(runjob)
-##
-#        init_gamera()
-#        task_image = load_image(page_url)
-#        result_xml = self.process_image(task_image, settings)
-##
-##
-##
-#        result = taskutil.save_result_as_png(result_image, runjob)
-#        return str(result.uuid)
-##
-#    def on_success(self, retval, task_id, args, kwargs):
-#        result = Result.objects.get(pk=retval)
-#        result.run_job.status = RunJobStatus.HAS_FINISHED
-#        result.run_job.save()
-##
-#    def on_failure(self, *args, **kwargs):
-#        runjob = RunJob.objects.get(pk=args[2][1])  # index into args to fetch the failed runjob instance
-#        runjob.status = RunJobStatus.FAILED
-#        runjob.save()
-##
-#
+class AutoClassificationTask(Task):
+    max_retries = None
+    name = 'gamera.custom.neume_classification.automatic_classification'
+    settings = [{'default':1,'has_default':True,'rng':(1,1048576),'name':'num_k','type':'int'},
+                {'default':4,'has_default':True,'rng':(-1048576,1048576),'name':'max_parts_per_group','type':'int'},
+                {'default':16,'has_default':True,'rng':(-1048576,1048576),'name':'max_graph_size','type':'int'},
+                {'default': None,'has_default':False,'name':'classifier','type':'uuid'},
+                {'default': None,'has_default':False,'name':'pageglyphs','type':'uuid'}]
+
+    def process_image(self, task_image, settings, classifier_model):
+        # Code borrowed from the old rodan classification job.
+
+       # Is there a way to do a dropdown menu or a checkbox menu in the client instrea of using features='all'?
+        cknn = gamera.knn.kNNNonInteractive(classifier_model.file_path,
+                                            features='all', perform_splits=True,
+                                            num_k=settings['num_k'])
+        func = gamera.classify.BoundingBoxGroupingFunction(2)
+        ccs = task_image.cc_analysis()
+
+        grouped_glyphs = cknn.group_and_update_list_automatic(ccs,
+                                                              grouping_function=func,
+                                                              max_parts_per_group=4,
+                                                              max_graph_size=16)
+
+        cknn.generate_features_on_glyphs(grouped_glyphs)
+        return grouped_glyphs
+
+    def save_result(self, image, glyphs, classifier, runjob):
+        temp_image_path = taskutil.create_temp_path(ext='png')
+        image.save_image(temp_image_path)
+
+        temp_xml_path = taskutil.create_temp_path(ext='xml')
+        gamera.gamera_xml.glyphs_to_xml(temp_xml_path, glyphs, with_features=True)
+        rdn_pageglyph = PageGlyphs(classifier=classifier)
+        rdn_pageglyph.save()
+
+        with open(temp_xml_path) as f:
+            rdn_pageglyph.pageglyphs_file.save('page_glyphs.xml', File(f))
+
+        temp_tar_path = taskutil.create_temp_path(ext='tar')
+        with tarfile.open(temp_tar_path, 'w') as tar:
+            tar.add(temp_image_path, arcname='image.png')
+            tar.add(temp_xml_path, arcname='glyphs.xml')
+
+        result = taskutil.init_result(runjob)
+        taskutil.save_result(result, temp_tar_path)
+        result.result_type = PACKAGE
+        result.save()
+
+        shutil.rmtree(os.path.dirname(temp_xml_path))
+        shutil.rmtree(os.path.dirname(temp_image_path))
+
+        return result
+
+    def run(self, result_id, runjob_id, *args, **kwargs):
+        runjob = RunJob.objects.get(pk=runjob_id)
+        taskutil.set_running(runjob)
+        page_url = taskutil.get_page_url(runjob, result_id)
+        settings = taskutil.get_settings(runjob)
+        classifier = Classifier.objects.get(pk=settings['classifier'])
+
+        init_gamera()
+        task_image = load_image(page_url)
+        result_glyphs = self.process_image(task_image, settings, classifier)
+        glyphs_model = save_glyphs(result_glyphs, classifier)
+        result = self.save_result(task_image, result_glyphs, classifier, runjob)
+        return str(result.uuid)
+
+    def on_success(self, retval, task_id, args, kwargs):
+        result = Result.objects.get(pk=retval)
+        result.run_job.status = RunJobStatus.HAS_FINISHED
+        result.run_job.save()
+
+    def on_failure(self, *args, **kwargs):
+        runjob = RunJob.objects.get(pk=args[2][1])  # index into args to fetch the failed runjob instance
+        runjob.status = RunJobStatus.FAILED
+        runjob.save()
--- rodan/jobs/gamera/custom/neume_classification/module_loader.py
+++ rodan/jobs/gamera/custom/neume_classification/module_loader.py
@@ -1,17 +1,17 @@
 from rodan.models.job import Job
-from rodan.jobs.gamera.custom.neume_classification.celery_task import ManualClassificationTask
-from rodan.settings import ONEBIT
+from rodan.jobs.gamera.custom.neume_classification.celery_task import ManualClassificationTask, AutoClassificationTask
+from rodan.settings import ONEBIT, PACKAGE
 
 
-def load_manual_classifier():
+def load_manual_classification():
     task_class = ManualClassificationTask
     
     if not Job.objects.filter(job_name=task_class.name).exists():
         j = Job(job_name=task_class.name,
                 author="Deepanjan Roy",
-                description="Classifies the neumes detected in the page.",
+                description="Classifies the neumes detected in the page using the classifier interface.",
                 input_types={"default": None, "has_default": False, "list_of": False, "pixel_types": (ONEBIT,), "name": "input"},
-                output_types={"default": None, "has_default": False, "list_of": False, "pixel_types": (ONEBIT,), "name": "output"},
+                output_types={"default": None, "has_default": False, "list_of": False, "pixel_types": (PACKAGE,), "name": "output"},
                 settings=task_class.settings,
                 enabled=True,
                 category="Classification",
@@ -20,6 +20,24 @@ def load_manual_classifier():
 
         j.save()
 
+def load_automatic_classification():
+    task_class = AutoClassificationTask
+    
+    if not Job.objects.filter(job_name=task_class.name).exists():
+        j = Job(job_name=task_class.name,
+                author="Deepanjan Roy",
+                description="Automatically classifies the neumes detected in the page using the classifier provided.",
+                input_types={"default": None, "has_default": False, "list_of": False, "pixel_types": (ONEBIT,), "name": "input"},
+                output_types={"default": None, "has_default": False, "list_of": False, "pixel_types": (PACKAGE,), "name": "output"},
+                settings=task_class.settings,
+                enabled=True,
+                category="Classification",
+                interactive=False
+                )
+
+        j.save()
+
 
 def load_module():
-    load_manual_classifier()
+    load_manual_classification()
+    load_automatic_classification()
