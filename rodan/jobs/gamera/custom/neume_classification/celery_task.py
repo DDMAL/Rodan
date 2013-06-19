95103af7134bb05376914ecd2991d9f4980db989
Deepanjan Roy <deepanjan.roy@mail.mcgill.ca>
2013-06-19 10:19:14 -0400

    Refactor and Cleanup: Classification Jobs.
    Reduced the amount of duplicate code. Removed unused imports. Used proper spacing.

--- rodan/jobs/gamera/custom/neume_classification/celery_task.py
+++ rodan/jobs/gamera/custom/neume_classification/celery_task.py
@@ -16,51 +16,31 @@ from rodan.models.runjob import RunJob
 from rodan.models.runjob import RunJobStatus
 from rodan.models.result import Result
 from rodan.jobs.util import taskutil
-from rodan.helpers.thumbnails import create_thumbnails
 from rodan.settings import PACKAGE
 
 
-class ManualClassificationTask(Task):
-    max_retries = None
-    name = 'gamera.custom.neume_classification.manual_classification'
-    settings = [{'default': None, 'has_default': False, 'name': 'classifier', 'type': 'uuid'},
-                {'default': None, 'has_default': False, 'name': 'pageglyphs', 'type': 'uuid'},
-                {'default': 1, 'has_default': True, 'rng': (1, 1048576), 'name': 'num_k', 'type': 'int'},
-                {'default': 4, 'has_default': True, 'rng': (-1048576, 1048576), 'name': 'max_parts_per_group', 'type': 'int'},
-                {'default':  16,'has_default': True, 'rng': (-1048576, 1048576), 'name': 'max_graph_size', 'ty pe': 'int'},
-                {'default': 2, 'has_default': True, 'rng': (-1048576, 1048576), 'name': 'max_grouping_distance', 'type': 'int'}]
+class ClassificationTaskBase(Task):
 
-    def preconfigure_settings(self, page_url, settings):
-        init_gamera()
-        task_image = load_image(page_url)
-        classifier_model = Classifier.objects.get(pk=settings['classifier'])
-        rdn_pageglyph = PageGlyphs(classifier=classifier_model)
-        rdn_pageglyph.save()
+    def get_classifier(self, uuid):
+        try:
+            return Classifier.objects.get(pk=uuid)
+        except Classifier.DoesNotExist:
+            if uuid is None:
+                print "Looks like you forgot to provide a classifier uuid."
+            else:
+                print "Classifier with the given uuid not found."
+            print "This task will now fail with cryptic error messages."
 
-        temp_xml_path = taskutil.create_temp_path(ext='xml')
-        gamera.gamera_xml.glyphs_to_xml(temp_xml_path,
-                                        task_image.cc_analysis(),
-                                        with_features=True)
-        with open(temp_xml_path) as f:
-            rdn_pageglyph.pageglyphs_file.save('page_glyphs.xml', File(f))
-
-        tdir = os.path.dirname(temp_xml_path)
-        shutil.rmtree(tdir)
-
-        return {'pageglyphs': u"{0}".format(rdn_pageglyph.uuid)}
-
-    def save_result(self, page_url, glyphs, runjob):
-
+    def save_result(self, page_url, glyphs_model, runjob):
         temp_tar_path = taskutil.create_temp_path(ext='tar')
         with tarfile.open(temp_tar_path, 'w') as tar:
             tar.add(page_url, arcname='image.png')
-            tar.add(glyphs.file_path, arcname='glyphs.xml')
+            tar.add(glyphs_model.file_path, arcname='glyphs.xml')
 
         result = taskutil.init_result(runjob)
         taskutil.save_result(result, temp_tar_path)
         result.result_type = PACKAGE
         result.save()
-
         return result
 
     def run(self, result_id, runjob_id, *args, **kwargs):
@@ -87,9 +67,11 @@ class ManualClassificationTask(Task):
             taskutil.set_running(runjob)
             page_url = taskutil.get_page_url(runjob, result_id)
             settings = taskutil.get_settings(runjob)
-            result_glyphs = PageGlyphs.objects.get(pk=settings['pageglyphs'])
 
-            result = self.save_result(page_url, result_glyphs, runjob)
+            init_gamera()
+            task_image = load_image(page_url)
+            pageglyphs = self.process_image(task_image, settings)
+            result = self.save_result(page_url, pageglyphs, runjob)
             return str(result.uuid)
 
     def on_success(self, retval, task_id, args, kwargs):
@@ -103,78 +85,72 @@ class ManualClassificationTask(Task):
         runjob.save()
 
 
-class AutoClassificationTask(Task):
+class ManualClassificationTask(ClassificationTaskBase):
+    max_retries = None
+    name = 'gamera.custom.neume_classification.manual_classification'
+    settings = [{'default': None, 'has_default': False, 'name': 'classifier', 'type': 'uuid'},
+                {'default': None, 'has_default': False, 'name': 'pageglyphs', 'type': 'uuid'},
+                {'default': 1, 'has_default': True, 'rng': (1, 1048576), 'name': 'num_k', 'type': 'int'},
+                {'default': 4, 'has_default': True, 'rng': (-1048576, 1048576), 'name': 'max_parts_per_group', 'type': 'int'},
+                {'default':  16, 'has_default': True, 'rng': (-1048576, 1048576), 'name': 'max_graph_size', 'ty pe': 'int'},
+                {'default': 2, 'has_default': True, 'rng': (-1048576, 1048576), 'name': 'max_grouping_distance', 'type': 'int'}]
+
+    def preconfigure_settings(self, page_url, settings):
+        init_gamera()
+        task_image = load_image(page_url)
+        classifier_model = self.get_classifier(settings['classifier'])
+        rdn_pageglyph = PageGlyphs(classifier=classifier_model)
+        rdn_pageglyph.save()
+
+        temp_xml_path = taskutil.create_temp_path(ext='xml')
+        gamera.gamera_xml.glyphs_to_xml(temp_xml_path,
+                                        task_image.cc_analysis(),
+                                        with_features=True)
+        with open(temp_xml_path) as f:
+            rdn_pageglyph.pageglyphs_file.save('page_glyphs.xml', File(f))
+
+        shutil.rmtree(os.path.dirname(temp_xml_path))
+
+        return {'pageglyphs': u"{0}".format(rdn_pageglyph.uuid)}
+
+    def process_image(self, task_image, settings):
+        return PageGlyphs.objects.get(pk=settings['pageglyphs'])
+
+
+class AutoClassificationTask(ClassificationTaskBase):
     max_retries = None
     name = 'gamera.custom.neume_classification.automatic_classification'
-    settings = [{'default':1,'has_default':True,'rng':(1,1048576),'name':'num_k','type':'int'},
-                {'default':4,'has_default':True,'rng':(-1048576,1048576),'name':'max_parts_per_group','type':'int'},
-                {'default':16,'has_default':True,'rng':(-1048576,1048576),'name':'max_graph_size','type':'int'},
-                {'default': None,'has_default':False,'name':'classifier','type':'uuid'},
-                {'default': None,'has_default':False,'name':'pageglyphs','type':'uuid'}]
+    settings = [{'default': 1, 'has_default': True, 'rng': (1, 1048576), 'name': 'num_k', 'type': 'int'},
+                {'default': 4, 'has_default': True, 'rng': (-1048576, 1048576), 'name': 'max_parts_per_group', 'type': 'int'},
+                {'default': 16, 'has_default': True, 'rng': (-1048576, 1048576), 'name': 'max_graph_size', 'type': 'int'},
+                {'default':  None, 'has_default': False, 'name': 'classifier', 'type': 'uuid'},
+                {'default': None, 'has_default': False, 'name': 'pageglyphs', 'type': 'uuid'}]
 
-    def process_image(self, task_image, settings, classifier_model):
+    def save_glyphs(self, glyphs, classifier):
+        pageglyphs_model = PageGlyphs(classifier=classifier)
+        pageglyphs_model.save()
+        temp_xml_path = taskutil.create_temp_path(ext='xml')
+        gamera.gamera_xml.glyphs_to_xml(temp_xml_path, glyphs, with_features=True)
+
+        with open(temp_xml_path) as f:
+            pageglyphs_model.pageglyphs_file.save('page_glyphs.xml', File(f))
+        shutil.rmtree(os.path.dirname(temp_xml_path))
+
+        return pageglyphs_model
+
+    def process_image(self, task_image, settings):
+        classifier_model = self.get_classifier(settings['classifier'])
        # Is there a way to do a dropdown menu or a checkbox menu in the client instrea of using features='all'?
         cknn = gamera.knn.kNNNonInteractive(classifier_model.file_path,
                                             features='all', perform_splits=True,
                                             num_k=settings['num_k'])
         func = gamera.classify.BoundingBoxGroupingFunction(2)
         ccs = task_image.cc_analysis()
-
         grouped_glyphs = cknn.group_and_update_list_automatic(ccs,
                                                               grouping_function=func,
                                                               max_parts_per_group=4,
                                                               max_graph_size=16)
-
         cknn.generate_features_on_glyphs(grouped_glyphs)
-        return grouped_glyphs
+        glyph_model = self.save_glyphs(grouped_glyphs, classifier_model)
 
-    def save_result(self, image, glyphs, classifier, runjob):
-        temp_image_path = taskutil.create_temp_path(ext='png')
-        image.save_image(temp_image_path)
-
-        temp_xml_path = taskutil.create_temp_path(ext='xml')
-        gamera.gamera_xml.glyphs_to_xml(temp_xml_path, glyphs, with_features=True)
-        rdn_pageglyph = PageGlyphs(classifier=classifier)
-        rdn_pageglyph.save()
-
-        with open(temp_xml_path) as f:
-            rdn_pageglyph.pageglyphs_file.save('page_glyphs.xml', File(f))
-
-        temp_tar_path = taskutil.create_temp_path(ext='tar')
-        with tarfile.open(temp_tar_path, 'w') as tar:
-            tar.add(temp_image_path, arcname='image.png')
-            tar.add(temp_xml_path, arcname='glyphs.xml')
-
-        result = taskutil.init_result(runjob)
-        taskutil.save_result(result, temp_tar_path)
-        result.result_type = PACKAGE
-        result.save()
-
-        shutil.rmtree(os.path.dirname(temp_xml_path))
-        shutil.rmtree(os.path.dirname(temp_image_path))
-
-        return result
-
-    def run(self, result_id, runjob_id, *args, **kwargs):
-        runjob = RunJob.objects.get(pk=runjob_id)
-        taskutil.set_running(runjob)
-        page_url = taskutil.get_page_url(runjob, result_id)
-        settings = taskutil.get_settings(runjob)
-        classifier = Classifier.objects.get(pk=settings['classifier'])
-
-        init_gamera()
-        task_image = load_image(page_url)
-        result_glyphs = self.process_image(task_image, settings, classifier)
-        glyphs_model = save_glyphs(result_glyphs, classifier)
-        result = self.save_result(task_image, result_glyphs, classifier, runjob)
-        return str(result.uuid)
-
-    def on_success(self, retval, task_id, args, kwargs):
-        result = Result.objects.get(pk=retval)
-        result.run_job.status = RunJobStatus.HAS_FINISHED
-        result.run_job.save()
-
-    def on_failure(self, *args, **kwargs):
-        runjob = RunJob.objects.get(pk=args[2][1])  # index into args to fetch the failed runjob instance
-        runjob.status = RunJobStatus.FAILED
-        runjob.save()
+        return glyph_model
