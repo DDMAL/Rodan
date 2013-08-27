c11b29926339747b6b0ab3ee85af915427fb4d03
Deepanjan Roy <deepanjan.roy@mail.mcgill.ca>
2013-08-26 23:55:56 -0400

    New: query parameters for classifiersetting.
    As you can see, I'm conflicted about whether to take in a url or a uuid as query params.

--- rodan/views/classifiersetting.py
+++ rodan/views/classifiersetting.py
@@ -1,12 +1,32 @@
 from rest_framework import generics, permissions
+from rodan.models.classifier import Classifier
 from rodan.models.classifiersetting import ClassifierSetting
 from rodan.serializers.classifiersetting import ClassifierSettingSerializer
+from rodan.helpers.dbmanagement import resolve_object_from_url
 
 class ClassifierSettingList(generics.ListCreateAPIView):
     model = ClassifierSetting
     permission_classes = (permissions.IsAuthenticated,)
     serializer_class = ClassifierSettingSerializer
     paginate_by = None
+
+    def get_queryset(self):
+        queryset = ClassifierSetting.objects.all()
+        project = self.request.QUERY_PARAMS.get('project', None)
+
+        if project:
+            queryset = queryset.filter(project__uuid=project)
+
+        producer_url = self.request.QUERY_PARAMS.get('producer', None)
+        if producer_url:
+            try:
+                classifier_instance = resolve_object_from_url(Classifier, producer_url)
+            except Classifier.DoesNotExist:
+                return queryset
+
+            queryset = queryset.filter(producer=classifier_instance)
+
+        return queryset
 
     def post(self, request, *args, **kwargs):
         response = super(ClassifierSetting, self).post(request, *args, **kwargs)
