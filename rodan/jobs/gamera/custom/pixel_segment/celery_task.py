a6ef558fd0c9b24703ba68e7b667c1ff012ee6c6
Ryan Bannon <ryan.bannon@gmail.com>
2014-04-30 11:31:26 -0400

    Fix: Lyric extraction correction now returning pure black.

--- rodan/jobs/gamera/custom/pixel_segment/celery_task.py
+++ rodan/jobs/gamera/custom/pixel_segment/celery_task.py
@@ -29,47 +29,80 @@ class PixelSegmentTask(GameraCustomTask):
             geometries = []
 
         # Make copy of original
-        imageOriginalCopy = task_image.to_pil()
+        imageOriginal = task_image.to_pil()
 
         # For each of the geometries provided, we have to apply the pixel colour to non-white pixels.
         for geometry in geometries:
+            imageOriginal = self._apply_geometry(imageOriginal, geometry)
 
-            # we don't use the alpha component.
-            newColour = (geometry['colour'][0], geometry['colour'][1], geometry['colour'][2])
-
-            # Get scaling factor.
-            workingWidth = geometry['workingWidth']
-            originalWidth = geometry['originalWidth']
-            scale = float(originalWidth) / workingWidth
-
-            # For the polygon, create crop box.
-            upperLeftX = sys.maxint
-            upperLeftY = sys.maxint
-            lowerRightX = 0;
-            lowerRightY = 0;
-            for point in geometry['points']:
-                point['x'] = int(point['x'] * scale)
-                point['y'] = int(point['y'] * scale)
-                upperLeftX = point['x'] if point['x'] < upperLeftX else upperLeftX
-                upperLeftY = point['y'] if point['y'] < upperLeftY else upperLeftY
-                lowerRightX = point['x'] if point['x'] > lowerRightX else lowerRightX
-                lowerRightY = point['y'] if point['y'] > lowerRightY else lowerRightY
-            box = (upperLeftX, upperLeftY, lowerRightX, lowerRightY)
-
-            # Crop out the image and get data.
-            imageCrop = imageOriginalCopy.crop(box)
-            imageCrop.load()
-            imagePixelData = list(imageCrop.getdata())
-
-            # Go through pixels.
-            for i in range(len(imagePixelData)):
-                if imagePixelData[i] != (255, 255, 255):
-                    imagePixelData[i] = newColour
-
-            # Apply.
-            imageCrop.putdata(imagePixelData)
-            imageOriginalCopy.paste(imageCrop, (upperLeftX, upperLeftY, lowerRightX, lowerRightY))
+        # Convert red to white and black to green.
+        colour_swap = {(255, 0, 0): (255, 255, 255), (0, 255, 0): (0, 0, 0)}
+        box = (0, 0, imageOriginal.size[0], imageOriginal.size[1])
+        imageOriginal = self._colour_swap_pixels(imageOriginal, box, colour_swap)
 
         # Convert back to gamera image and return.
-        finalImage = from_pil(imageOriginalCopy)
+        finalImage = from_pil(imageOriginal)
         return finalImage
+
+    # Given image and geometry (with colour and working vs original dimensions), apply it to the image.
+    def _apply_geometry(self, image, geometry):
+
+        # we don't use the alpha component.
+        newColour = (geometry['colour'][0], geometry['colour'][1], geometry['colour'][2])
+
+        # Get scaling factor.
+        workingWidth = geometry['workingWidth']
+        originalWidth = geometry['originalWidth']
+        scale = float(originalWidth) / workingWidth
+
+        # For the polygon, create crop box.
+        upperLeftX = sys.maxint
+        upperLeftY = sys.maxint
+        lowerRightX = 0;
+        lowerRightY = 0;
+        for point in geometry['points']:
+            point['x'] = int(point['x'] * scale)
+            point['y'] = int(point['y'] * scale)
+            upperLeftX = point['x'] if point['x'] < upperLeftX else upperLeftX
+            upperLeftY = point['y'] if point['y'] < upperLeftY else upperLeftY
+            lowerRightX = point['x'] if point['x'] > lowerRightX else lowerRightX
+            lowerRightY = point['y'] if point['y'] > lowerRightY else lowerRightY
+        box = (upperLeftX, upperLeftY, lowerRightX, lowerRightY)
+
+        # Change pixels and return.
+        image = self._colour_pixels(image, box, newColour)
+        return image
+
+    # Colours all pixels in the image defined by the box the given colour (except white pixels).
+    def _colour_pixels(self, image, box, colour):
+
+        # Crop out the image and get data.
+        imageCrop = image.crop(box)
+        imageCrop.load()
+        imagePixelData = list(imageCrop.getdata())
+
+        # Go through pixels.
+        for i in range(len(imagePixelData)):
+            if imagePixelData[i] != (255, 255, 255):
+                imagePixelData[i] = colour
+
+        # Apply and return.
+        imageCrop.putdata(imagePixelData)
+        image.paste(imageCrop, box)
+        return image
+
+    # Colours all pixels defined in "swap" keys with their "swap" values for the box dimensions.
+    # See '_colour_pixels'...very similar.
+    def _colour_swap_pixels(self, image, box, colour_swap):
+        imageCrop = image.crop(box)
+        imageCrop.load()
+        imagePixelData = list(imageCrop.getdata())
+        for i in range(len(imagePixelData)):
+            if (imagePixelData[i] in colour_swap):
+                imagePixelData[i] = colour_swap[imagePixelData[i]]
+        imageCrop.putdata(imagePixelData)
+        image.paste(imageCrop, box)
+        return image
+
+
+
