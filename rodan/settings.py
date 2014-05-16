fd9226af8509492252f0d61ccb47989f79d5c7a9
Andrew Hankinson <andrew.hankinson@gmail.com>
2014-05-16 15:20:32 -0400

    Fixed: Updated settings file for Django 1.7
    New settings include test runner

--- rodan/settings.py
+++ rodan/settings.py
@@ -1,13 +1,7 @@
 from settings_production import *
 
 TEMPLATE_DEBUG = DEBUG
-
-ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
-)
-
-MANAGERS = ADMINS
-
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
