29d0ba1a7de7a4e482e9c58da312c09112765e57
Andrew Hankinson <andrew.hankinson@gmail.com>
2014-05-19 14:03:22 -0400

    Tests: Update project view to use Rest Framework Testing
    This commit moves to the Django Rest Framework testing system. It introduces APITestCase as the superclass of the test cases, and uses the status module rather than the raw numeric codes for response codes.

--- rodan/test/views/test_project.py
+++ rodan/test/views/test_project.py
@@ -1,8 +1,8 @@
 import os
-from django.test import TestCase
-from django.core.files.uploadedfile import SimpleUploadedFile
-from rest_framework.test import APIClient
 from django.conf import settings
+from django.core.files.uploadedfile import SimpleUploadedFile
+from rest_framework.test import APITestCase
+from rest_framework import status
 
 from rodan.models.page import Page
 from rodan.models.project import Project
@@ -10,12 +10,13 @@ from rodan.models.project import Project
 # Refer to http://www.django-rest-framework.org/api-guide/testing
 
 
-class ProjectViewTestCase(TestCase):
+class ProjectViewTestCase(APITestCase):
     fixtures = ["1_users", "2_initial_data"]
 
     def setUp(self):
         self.media_root = os.path.join(settings.PROJECT_DIR, 'uploads/')
-        self.client = APIClient()
+
+        # self.client is automatically instantiated in APITestCase
         self.client.login(username="ahankins", password="hahaha")
         self.project_dir = os.path.join(self.media_root, "projects/9e8e928b4ec24a09b6113f1b0af1ea53/pages/2f63f986449349769d7a313e0fc6edb3/")
 
@@ -35,11 +36,11 @@ class ProjectViewTestCase(TestCase):
 
     def test_get_list(self):
         response = self.client.get("/projects/")
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_get_detail(self):
         response = self.client.get("/project/9e8e928b4ec24a09b6113f1b0af1ea53/")
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_post(self):
         proj_obj = {
@@ -48,7 +49,7 @@ class ProjectViewTestCase(TestCase):
             "name": "Another Test Project",
         }
         response = self.client.post("/projects/", proj_obj, format='json')
-        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_put(self):
         pass
@@ -56,10 +57,10 @@ class ProjectViewTestCase(TestCase):
     def test_patch(self):
         proj_mod = {"name": "Changing the title"}
         response = self.client.patch("/project/9e8e928b4ec24a09b6113f1b0af1ea53/", proj_mod, format='json')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['name'], "Changing the title")
 
     def test_delete(self):
         response = self.client.delete("/project/9e8e928b4ec24a09b6113f1b0af1ea53/")
-        self.assertEqual(response.status_code, 204)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertFalse(Project.objects.filter(uuid="9e8e928b4ec24a09b6113f1b0af1ea53").exists())
