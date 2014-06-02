7d4b0961a81de801416a057f7a26d8811555633f
rberkow <ruth.berkow@mail.mcgill.ca>
2014-06-02 13:50:52 -0400

    Tests: more workflowrun view tests

--- rodan/test/views/test_workflowrun.py
+++ rodan/test/views/test_workflowrun.py
@@ -3,9 +3,12 @@ from django.conf import settings
 from django.core.files.uploadedfile import SimpleUploadedFile
 from rest_framework.test import APITestCase
 from rest_framework import status
+from django.contrib.auth.models import User
 
 from rodan.models.page import Page
-
+from rodan.models.workflowrun import WorkflowRun
+from rodan.models.workflow import Workflow
+from rodan.models.runjob import RunJobStatus
 
 
 class WorkflowRunViewTest(APITestCase):
@@ -27,7 +30,13 @@ class WorkflowRunViewTest(APITestCase):
         }
 
         response = self.client.post("/workflowruns/", workflowrun_obj, format='json')
-        print(response)
+        wfr = {
+            'creator': User.objects.get(pk=1),
+            'workflow': Workflow.objects.get(pk='df78a1aa79554abcb5f1b0ac7bba2bad')
+        }
+        workflow_run = WorkflowRun(**wfr)
+        workflow_run.save()
+        self.assertEqual(workflow_run.workflow, wfr['workflow'])
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_post_no_workflow_ID(self):
@@ -65,7 +74,7 @@ class WorkflowRunViewTest(APITestCase):
 
     def test_post_no_pages(self):
         workflowrun_obj = {
-            'creator': 'http://localhost:8000/user/1',
+            'creator': 'http://localhost:8000/user/1/',
             'workflow': 'http://localhost:8000/workflow/ff78a1aa79554abcb5f1b0ac7bba2bad/',
         }
 
@@ -73,3 +82,42 @@ class WorkflowRunViewTest(APITestCase):
         anticipated_message = {"message": "No pages were assigned to workflow ID {0}".format(workflowrun_obj['workflow'])}
         self.assertEqual(response.data, anticipated_message)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_get_detail(self):
+        response = self.client.get("/workflowrun/eb4b3661be2a44908c4c932b0783bb3e/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_list(self):
+        response = self.client.get("/workflowruns/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_detail_by_page(self):
+        response = self.client.get('/workflowrun/eb4b3661be2a44908c4c932b0783bb3e/?by_page=true')
+        # self.assertEqual(response)
+
+    def test_patch(self):
+        workflowrun_update = {'run': WorkflowRun.objects.get(pk="eb4b3661be2a44908c4c932b0783bb3e").run+1}
+        response = self.client.patch("/workflowrun/eb4b3661be2a44908c4c932b0783bb3e/", workflowrun_update, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_patch_not_found(self):
+        workflowrun_update = {'run': 5}
+        response = self.client.patch("/workflowrun/12345/", workflowrun_update, format='json')
+        anticipated_message = {'message': 'Workflow_run not found'}
+        self.assertEqual(anticipated_message, response.data)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_patch_already_cancelled(self):
+        workflowrun_update = {'cancelled': False}
+        response = self.client.patch("/workflowrun/4b1a0d13b2cd48a5a99324d7308ca27a/", workflowrun_update, format='json')
+        anticipated_message = {"message": "Workflowrun cannot be uncancelled."}
+        self.assertEqual(anticipated_message, response.data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_patch_newly_cancelled(self):
+        workflowrun_update = {'cancelled': True}
+        self.client.patch("/workflowrun/eb4b3661be2a44908c4c932b0783bb3e/", workflowrun_update, format='json')
+        workflowrun_obj = WorkflowRun.objects.get(pk='eb4b3661be2a44908c4c932b0783bb3e')
+        expected_status = RunJobStatus.CANCELLED or RunJobStatus.HAS_FINISHED or RunJobStatus.FAILED
+        for rj in workflowrun_obj.run_jobs.all():
+            self.assertEqual(rj.status, expected_status)
