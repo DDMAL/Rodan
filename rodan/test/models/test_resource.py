d40db0fded4a837c56b3c0267f45daaf3017c564
rberkow <ruth.berkow@mail.mcgill.ca>
2014-06-16 16:20:32 -0400

    Tests: update tests for resource & workflow models
    and adding test for new 'valid' field on workflow model

--- rodan/test/models/test_resource.py
+++ rodan/test/models/test_resource.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 from rodan.models.resource import Resource
 from rodan.models.project import Project
 from rodan.models.output import Output
+from rodan.models.inputport import InputPort
+from rodan.models.resourceassignment import ResourceAssignment
+from rodan.models.workflow import Workflow
 
 
 class ResourceTestCase(TestCase):
@@ -12,23 +15,31 @@ class ResourceTestCase(TestCase):
         self.test_user = User.objects.get(username="ahankins")
         self.test_project = Project.objects.get(uuid="9e8e928b4ec24a09b6113f1b0af1ea53")
         self.test_output = Output.objects.get(uuid="04ae88f664664d3eaa68406c7c2f1211")
+        self.test_inputport = InputPort.objects.get(uuid="e38b11918691486cbd99627e5baec0a1")
+        self.test_workflow = Workflow.objects.get(uuid="df78a1aa79554abcb5f1b0ac7bba2bad")
+
+        self.test_resource_data = {
+            "project": self.test_project,
+            "creator": self.test_user,
+            "name": "test resource",
+        }
 
     def test_save_original_file(self):
-        resource = Resource(project=self.test_project, creator=self.test_user, name="test resource")
+        resource = Resource(**self.test_resource_data)
         resource.save()
 
         retr_resource = Resource.objects.get(name="test resource")
         self.assertEqual(retr_resource.name, resource.name)
 
     def test_save_runjob_result(self):
-        resource = Resource(project=self.test_project, origin=self.test_output, name="test resource")
+        resource = Resource(**self.test_resource_data)
         resource.save()
 
         retr_resource = Resource.objects.get(name="test resource")
         self.assertEqual(retr_resource.name, resource.name)
 
     def test_delete(self):
-        resource = Resource(project=self.test_project, creator=self.test_user, name="test resource")
+        resource = Resource(**self.test_resource_data)
         resource.save()
 
         retr_resource = Resource.objects.get(name="test resource")
--- rodan/test/models/test_workflow.py
+++ rodan/test/models/test_workflow.py
@@ -11,15 +11,21 @@ class WorkflowTestCase(TestCase):
         self.test_user = User.objects.get(username="ahankins")
         self.test_project = Project.objects.get(uuid="9e8e928b4ec24a09b6113f1b0af1ea53")
 
+        self.test_workflow_data = {
+            "name": "test workflow",
+            "project": self.test_project,
+            "creator": self.test_user
+        }
+
     def test_save(self):
-        workflow = Workflow.objects.create(name="test workflow", project=self.test_project, creator=self.test_user)
+        workflow = Workflow(**self.test_workflow_data)
         workflow.save()
 
         retr_workflow = Workflow.objects.get(name="test workflow")
         self.assertEqual(retr_workflow.name, workflow.name)
 
     def test_delete(self):
-        workflow = Workflow.objects.create(name="test workflow", project=self.test_project, creator=self.test_user)
+        workflow = Workflow(**self.test_workflow_data)
         workflow.save()
 
         retr_workflow = Workflow.objects.get(name="test workflow")
@@ -27,3 +33,10 @@ class WorkflowTestCase(TestCase):
 
         retr_workflow2 = Workflow.objects.filter(name="test workflow")
         self.assertFalse(retr_workflow2.exists())
+
+    def test_should_not_be_valid(self):
+        workflow = Workflow(**self.test_workflow_data)
+        workflow.save()
+
+        retr_workflow = Workflow.objects.get(name="test workflow")
+        self.assertFalse(retr_workflow.valid)
