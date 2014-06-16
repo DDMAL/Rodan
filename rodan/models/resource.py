68ca13b1186226d71ca53db8a3a8c730062d6f13
rberkow <ruth.berkow@mail.mcgill.ca>
2014-06-16 16:27:30 -0400

    Fixed: added 'workflow' field to Resource model

--- rodan/models/resource.py
+++ rodan/models/resource.py
@@ -52,6 +52,7 @@ class Resource(models.Model):
     resource_file = models.FileField(upload_to=upload_path, null=True, max_length=255)
     compat_resource_file = models.FileField(upload_to=compat_path, null=True, blank=True, max_length=255)
     resource_type = models.CharField(max_length=20, choices=RESOURCE_TYPE_CHOICES)
+    workflow = models.ForeignKey('rodan.Workflow', null=True, blank=True)
 
     creator = models.ForeignKey(User, related_name="resources", null=True, blank=True)
     origin = models.ForeignKey('rodan.Output', related_name="+", null=True, blank=True)
