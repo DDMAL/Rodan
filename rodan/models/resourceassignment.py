32cf1898878b2360e0268f5cbaf7fc1ffd5816d5
rberkow <ruth.berkow@mail.mcgill.ca>
2014-06-30 17:01:21 -0400

    New: workflowjob field on ResourceAssignment model

--- rodan/models/resourceassignment.py
+++ rodan/models/resourceassignment.py
@@ -10,3 +10,8 @@ class ResourceAssignment(models.Model):
     input_port = models.ForeignKey('rodan.InputPort')
     resources = models.ManyToManyField('rodan.Resource', related_name='resource_assignments')
     workflow = models.ForeignKey('rodan.Workflow')
+    workflow_job = models.ForeignKey('rodan.WorkflowJob', null=True, blank=True)
+
+    def save(self):
+        self.workflow_job = self.input_port.workflow_job
+        super(ResourceAssignment, self).save()
