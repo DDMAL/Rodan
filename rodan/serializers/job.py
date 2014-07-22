f1494bff1391eeff5085aca96a58b605d54421e0
rberkow <ruth.berkow@mail.mcgill.ca>
2014-07-22 14:35:58 -0400

    Fixed: "source" should have been "view_name"

--- rodan/serializers/job.py
+++ rodan/serializers/job.py
@@ -5,8 +5,8 @@ from rodan.models.job import Job
 class JobSerializer(serializers.HyperlinkedModelSerializer):
     settings = serializers.CharField(required=False)  # this actually sends it as JSON
 
-    input_port_types = serializers.HyperlinkedRelatedField(source="input_port_types")
-    output_port_types = serializers.HyperlinkedRelatedField(source="output_port_types")
+    input_port_types = serializers.HyperlinkedRelatedField(view_name="input_port_types")
+    output_port_types = serializers.HyperlinkedRelatedField(view_name="output_port_types")
 
     class Meta:
         model = Job
