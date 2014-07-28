d394316be13decd1f1668e10901136c506f07434
rberkow <ruth.berkow@mail.mcgill.ca>
2014-07-28 15:40:52 -0400

    Fixed: Changed resource_type field to CharField
    This is so that the automatic resource type detection using Python's mimetypes library will work

--- rodan/models/outputporttype.py
+++ rodan/models/outputporttype.py
@@ -9,7 +9,7 @@ class OutputPortType(models.Model):
     uuid = UUIDField(primary_key=True, auto=True)
     job = models.ForeignKey('rodan.Job', related_name='outputporttype', null=True, blank=True)
     name = models.CharField(max_length=255, null=True, blank=True)
-    resource_type = models.IntegerField()
+    resource_type = models.CharField(max_length=20)
     minimum = models.IntegerField()
     maximum = models.IntegerField()
 
