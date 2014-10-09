424028c2a19852374ac7cf70161a00b0271ac08d
rberkow <ruth.berkow@mail.mcgill.ca>
2014-10-09 16:42:27 -0400

    Fixed #325: create image file object in test setUp method

--- rodan/test/jobs/test_GameraTask.py
+++ rodan/test/jobs/test_GameraTask.py
@@ -1,8 +1,8 @@
 from PIL import Image
 from rest_framework.test import APITestCase
-from django.core.files.uploadedfile import SimpleUploadedFile
 from rodan.jobs.gamera.celery_task import GameraTask
 from rodan.models.resource import Resource
+from StringIO import StringIO
 from model_mommy import mommy
 
 
@@ -11,29 +11,41 @@ class GameraTaskTestCase(APITestCase):
 
     def setUp(self):
         self.client.login(username="ahankins", password="hahaha")
+        file_obj = StringIO()
+        image = Image.new("RGBA", size=(50, 50), color=(256, 0, 0))
+        image.save(file_obj, 'png')
+        file_obj.name = 'page1.png'
+        file_obj.seek(0)
         self.test_project = mommy.make('rodan.Project')
         resource_obj = {
             'project': "http://localhost:8000/project/{0}/".format(self.test_project.uuid),
             'files': [
-                SimpleUploadedFile('page1.png', 'n/t')
+                file_obj
             ],
         }
-        retr_res = self.client.post("/resources/", resource_obj, format='multipart').data['resources']
-        self.test_resource = Resource.objects.get(pk=retr_res[0]['uuid'])
+        retr_res = self.client.post("/resources/", resource_obj, format='multipart')
+        self.test_resource = Resource.objects.get(pk=retr_res.data['resources'][0]['uuid'])
         self.test_output = mommy.make('rodan.Output',
                                       resource=self.test_resource)
+        self.test_input = mommy.make('rodan.Input',
+                                     resource=self.test_resource)
+        self.test_resource.origin = self.test_output
+        self.test_resource.save()
         self.test_workflowjob = mommy.make('rodan.WorkflowJob')
         self.test_workflowrun = mommy.make('rodan.WorkflowRun',
                                            workflow=self.test_workflowjob.workflow)
         self.test_runjob = mommy.make('rodan.RunJob',
                                       workflow_job=self.test_workflowjob,
                                       workflow_run=self.test_workflowrun)
+        self.test_output.run_job = self.test_runjob
+        self.test_output.save()
+        self.test_input.run_job = self.test_runjob
+        self.test_input.save()
 
     def test_to_greyscale_no_previous_result(self):
         task = GameraTask()
         task.name = "gamera.plugins.image_conversion.to_greyscale"
-        #def run_task(self, output_id, runjob_id, *args, **kwargs)
-        result = task.run_task(None, self.test_runjob.uuid)
+        result = task.run_task(output_id=None, runjob_id=self.test_runjob.uuid)
 
         result_image = Resource.objects.get(pk="{0}".format(result))
         im = Image.open(result_image.resource_file.path)
@@ -42,7 +54,7 @@ class GameraTaskTestCase(APITestCase):
     def test_to_greyscale(self):
         task = GameraTask()
         task.name = "gamera.plugins.image_conversion.to_greyscale"
-        result = task.run_task(self.test_output.uuid, self.test_runjob.uuid)
+        result = task.run_task(output_id=self.test_output.uuid, runjob_id=self.test_runjob.uuid)
         result_image = Resource.objects.get(pk="{0}".format(result))
         im = Image.open(result_image.resource_file.path)
         self.assertEqual(im.mode, 'L')
