8ea2b94edee991c166e9504226db18f657f06a16
Ling-Xiao Yang <ling-xiao.yang@mail.mcgill.ca>
2014-11-06 13:30:13 -0500

    Fixed: ResourceAssignment view - remove unnecessary POST parameters

--- rodan/views/resourceassignment.py
+++ rodan/views/resourceassignment.py
@@ -19,8 +19,6 @@ class ResourceAssignmentList(generics.ListCreateAPIView):
 
     #### Parameters
     - input_port -- POST-only, URL to an InputPort object
-    - workflow -- POST-only, URL to an Workflow object [TODO] unnecessary??
-    - workflow_job -- POST-only, URL to an WorkflowJob object [TODO] unnecessary??
     - [TODO]: Resources?
     """
     model = ResourceAssignment
@@ -37,26 +35,6 @@ class ResourceAssignmentList(generics.ListCreateAPIView):
             return Response({'message': "Please specify an input port"}, status=status.HTTP_400_BAD_REQUEST)
         except InputPort.DoesNotExist:
             return Response({'message': "No input port with specified uuid exists"}, status=status.HTTP_400_BAD_REQUEST)
-
-        workflow = request.DATA.get('workflow', None)
-        try:
-            resolve_to_object(workflow, Workflow)
-        except Resolver404:
-            return Response({'message': "Couldn't resolve Workflow object"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        except AttributeError:
-            return Response({'message': "Please sepcify a workflow"}, status=status.HTTP_400_BAD_REQUEST)
-        except Workflow.DoesNotExist:
-            return Response({'message': "No workflow with specified uuid exists"}, status=status.HTTP_400_BAD_REQUEST)
-
-        workflow_job = request.DATA.get('workflow_job', None)
-        try:
-            resolve_to_object(workflow_job, WorkflowJob)
-        except Resolver404:
-            return Response({'message': "Could't resolve WorkflowJob object"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        except AttributeError:
-            return Response({'message': "Please specify a workflowjob"}, status=status.HTTP_400_BAD_REQUEST)
-        except WorkflowJob.DoesNotExist:
-            return Response({'message': "No workflowjob with specified uuid exists"}, status.HTTP_400_BAD_REQUEST)
 
         return self.create(request, *args, **kwargs)
 
