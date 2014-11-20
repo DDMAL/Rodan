73e2220a0738640e688ec7149be1743159b82569
Ling-Xiao Yang <ling-xiao.yang@mail.mcgill.ca>
2014-11-20 14:11:22 -0500

    Maintenance: directory structure

--- rodan/jobs/gamera/__init__.py
+++ rodan/jobs/gamera/__init__.py
@@ -15,14 +15,17 @@ module_loader('gamera.toolkits.lyric_extraction.plugins.lyricline', load_gamera_
 module_loader('gamera.toolkits.lyric_extraction.plugins.lyric_extractor', load_gamera_module)
 module_loader('gamera.toolkits.musicstaves.plugins.musicstaves_plugins', load_gamera_module)
 module_loader('gamera.toolkits.staffline_removal.plugins.staff_removal', load_gamera_module)
+module_loader('gamera.toolkits.rodan_plugins.plugins.rdn_despeckle', load_gamera_module)
 
+
+module_loader('rodan.jobs.gamera.interactive_tasks.pixel_segment')
+module_loader('rodan.jobs.gamera.interactive_tasks.poly_mask')
 module_loader('rodan.jobs.gamera.interactive_tasks.rdn_rotate')
 module_loader('rodan.jobs.gamera.interactive_tasks.rdn_despeckle')
-module_loader('gamera.toolkits.rodan_plugins.plugins.rdn_despeckle', load_gamera_module)
 module_loader('rodan.jobs.gamera.interactive_tasks.rdn_crop')
-module_loader('rodan.jobs.gamera.custom.border_removal')
-module_loader('rodan.jobs.gamera.custom.pixel_segment')
-module_loader('rodan.jobs.gamera.custom.pitch_finding.pitch_finding')
-module_loader('rodan.jobs.gamera.custom.poly_mask')
-module_loader('rodan.jobs.gamera.custom.segmentation.tasks')
-module_loader('rodan.jobs.gamera.custom.staff_removal')
+module_loader('rodan.jobs.gamera.interactive_tasks.segmentation.tasks')
+
+
+module_loader('rodan.jobs.gamera.auto_tasks.border_removal')
+module_loader('rodan.jobs.gamera.auto_tasks.pitch_finding.pitch_finding')
+module_loader('rodan.jobs.gamera.auto_tasks.staff_removal')
