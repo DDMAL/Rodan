43dd094f45002dd83a41ce5f635f305b7835f17e
rberkow <ruth.berkow@mail.mcgill.ca>
2014-11-28 10:37:20 -0500

    Working: migration of to_jpeg2000 job

--- rodan/jobs/conversion/__init__.py
+++ rodan/jobs/conversion/__init__.py
@@ -2,3 +2,4 @@ import logging
 logger = logging.getLogger('rodan')
 
 from rodan.jobs.conversion.to_png import to_png
+from rodan.jobs.conversion.to_jpeg2000 import to_jpeg2000
--- rodan/jobs/diva/__init__.py
+++ rodan/jobs/diva/__init__.py
@@ -1,8 +1,8 @@
 import logging
 logger = logging.getLogger('rodan')
 
-try:
-    from rodan.jobs.diva import to_jpeg2000
-    to_jpeg2000.load_module()
-except ImportError as e:
-    logger.warning("No Diva JPEG2000 converter Installed. Skipping. {0}".format(e))
+# try:
+#     from rodan.jobs.diva import to_jpeg2000
+#     to_jpeg2000.load_module()
+# except ImportError as e:
+#     logger.warning("No Diva JPEG2000 converter Installed. Skipping. {0}".format(e))
