65fe5046fcfae97e00d601ffc3471b41ce9111b5
Ling-Xiao Yang <ling-xiao.yang@mail.mcgill.ca>
2014-11-28 17:06:02 -0500

    Fixed: Resource serializer -- absolute url for resources and thumbnails

--- rodan/serializers/resource.py
+++ rodan/serializers/resource.py
@@ -2,14 +2,27 @@ from rodan.models.resource import Resource
 from rest_framework import serializers
 from rodan.serializers.user import UserListSerializer
 
+class AbsoluteURLField(serializers.Field):
+    def to_native(self, relative_url):
+        """
+        http://www.django-rest-framework.org/api-guide/fields/
+        """
+        if relative_url is not None:
+            request = self.context['request']
+            return request.build_absolute_uri(relative_url)
+        else:
+            return super(AbsoluteURLField, self).to_native(relative_url)
+
 
 class ResourceSerializer(serializers.HyperlinkedModelSerializer):
     project = serializers.HyperlinkedRelatedField(view_name="project-detail")
     creator = UserListSerializer(read_only=True)
-    uuid = serializers.Field(source='uuid')
-    resource_file = serializers.Field(source='resource_url')
-    compat_resource_file = serializers.Field(source='compat_file_url')
+    resource_file = AbsoluteURLField(source='resource_url')
+    compat_resource_file = AbsoluteURLField(source='compat_file_url')
     resource_type = serializers.HyperlinkedRelatedField(view_name="resourcetype-detail")
+    small_thumb = AbsoluteURLField(source='small_thumb_url')
+    medium_thumb = AbsoluteURLField(source='medium_thumb_url')
+    large_thumb = AbsoluteURLField(source='large_thumb_url')
 
     class Meta:
         model = Resource
@@ -27,7 +40,10 @@ class ResourceSerializer(serializers.HyperlinkedModelSerializer):
                   "creator",
                   "origin",
                   "created",
-                  "updated")
+                  "updated",
+                  "small_thumb",
+                  "medium_thumb",
+                  "large_thumb")
 
 
 class OutputResourceSerializer(serializers.HyperlinkedModelSerializer):
