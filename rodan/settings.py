5801ec15252f03c41d5f59360a03ede343106bd3
Ling-Xiao Yang <ling-xiao.yang@mail.mcgill.ca>
2015-02-09 14:37:37 -0500

    Maintenance: isolate vendors

--- rodan/settings.py
+++ rodan/settings.py
@@ -80,7 +80,6 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'rodan.urls'
 
 TEMPLATE_DIRS = (
-    PROJECT_PATH + '/jobs/',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
