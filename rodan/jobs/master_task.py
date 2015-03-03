d1687b30a5b47d17f018e0000c16c7db64147001
Ling-Xiao Yang <ling-xiao.yang@mail.mcgill.ca>
2015-03-03 14:31:42 -0500

    Fixed: improve parallel processing of workflowruns
    Originally, master task finds all runable runjobs, runs them in a chord, and calls itself back after the chord is finished. Then the execution time always depends on the slowest runjob.
    
    This commit fixes this issue by calling master task after finishing every runjob. However, it needs a lock on the runjob, to avoid multiple master tasks assigning the same runjob. The lock field stores the thread ID of master task when the runjob is locked. And this field is not designed to be provided to the client.

--- rodan/jobs/master_task.py
+++ rodan/jobs/master_task.py
@@ -1,13 +1,16 @@
-from celery import registry, task, chord
+from celery import registry, task
 from rodan.models import RunJob, Resource, WorkflowRun
 from rodan.constants import task_status
 from django.db.models import Q
+import thread
 
 # Read more on Django queries: https://docs.djangoproject.com/en/dev/topics/db/queries/
 
 @task(name='rodan.core.master_task')
 def master_task(workflow_run_id):
     """
+    Core task for Rodan job assignments.
+
     Code here are run asynchronously in Celery thread.
 
     To prevent re-creating a deleted object, any write to database should use
@@ -20,37 +23,33 @@ def master_task(workflow_run_id):
     + `obj.save()`
     + `obj.file_field.save(..., save=True)`
     """
-    # find runable runjobs
-    runable_runjobs_query = RunJob.objects.filter(
+    thread_id = str(thread.get_ident())
+
+    # find and lock runable runjobs
+    locked_runjobs_count = RunJob.objects.filter(
         Q(workflow_run__uuid=workflow_run_id)
         & Q(status=task_status.SCHEDULED)
-        & (~Q(inputs__resource__compat_resource_file__exact='')   # no ANY input with compat_resource_file==''
+        & (~Q(inputs__resource__compat_resource_file__exact='')   # not having ANY input with compat_resource_file==''
            | Q(inputs__isnull=True))      # OR no input
-    )
+        & Q(lock__isnull=True)    # not locked by other concurrent master tasks
+    ).update(lock=thread_id)
 
-    runable_runjobs_repeated = runable_runjobs_query.values('uuid', 'job_name')  # CAUTION: underlying database performs an INNER JOIN operation, which could return repeated lines due to reverse foreign key query "inputs".
-    runable_runjobs = []
-    uuid_set = set()
-    for rj_value in runable_runjobs_repeated:
-        if rj_value['uuid'] not in uuid_set:
-            runable_runjobs.append(rj_value)
-            uuid_set.add(rj_value['uuid'])
-
-    if len(runable_runjobs) == 0:
+    if locked_runjobs_count == 0:
         if not RunJob.objects.filter(Q(workflow_run__uuid=workflow_run_id) & ~Q(status=task_status.FINISHED)).exists():
             # WorkflowRun has finished!
             WorkflowRun.objects.filter(uuid=workflow_run_id).update(status=task_status.FINISHED)
             return "wfRun FINISHED"
         else:
-            return "wfRun WAITING FOR INPUT"
+            return "wfRun NO RUNABLE RUNJOBS NOW"
     else:
-        task_group = []
-        runable_runjobs_query.update(status=task_status.PROCESSING)  # in test, tasks are executed synchronously, therefore update the status before dispatching the task
+        runable_runjobs_query = RunJob.objects.filter(lock=thread_id)
+        runable_runjobs = list(runable_runjobs_query.values('uuid', 'job_name'))  # immediate evaluation
+        runable_runjobs_query.update(status=task_status.PROCESSING, lock=None)  # unlock now because the task status has been changed
+
         for rj_value in runable_runjobs:
             task = registry.tasks[str(rj_value['job_name'])]
             runjob_id = str(rj_value['uuid'])
-            task_group.append(task.si(runjob_id))
+            async_task = (task.si(runjob_id) | master_task.si(workflow_run_id)).apply_async()
+            RunJob.objects.filter(uuid=runjob_id).update(celery_task_id=async_task.task_id)
 
-        async_task = chord(task_group)(master_task.si(workflow_run_id))
-        runable_runjobs_query.update(celery_task_id=async_task.task_id)
         return "wfRun PROCESSING"
--- rodan/models/runjob.py
+++ rodan/models/runjob.py
@@ -38,7 +38,12 @@ class RunJob(models.Model):
     - `error_details` -- details of error when the `RunJob` fails.
     - `created`
     - `updated`
-    - `interactive_timings` -- a JSON list that tracks the start and end of every manual phase of the job.
+    - `interactive_timings` -- a JSON list that tracks the start and end of every manual
+      phase of the job.
+
+    - `lock` -- (internal use) stores the thread identifier of one of Celery workers, or
+      None. For a worker thread to lock the RunJobs and avoid competition. (see
+      `rodan.jobs.master_task`)
 
     **Properties**
 
@@ -75,6 +80,8 @@ class RunJob(models.Model):
     updated = models.DateTimeField(auto_now=True)
 
     interactive_timings = JSONField(default=[]) # track when a person starts and submits the job
+
+    lock = models.CharField(max_length=50, blank=True, null=True)
 
     def __unicode__(self):
         return u"<RunJob {0} {1}{2}>".format(str(self.uuid), self.workflow_job.job.job_name, ' (interactive)' if self.workflow_job.job.interactive else '')
