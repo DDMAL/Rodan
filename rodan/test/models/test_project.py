26c66be02f99f5b9d7cf4364b9d589782e2b9518
Ling-Xiao Yang <ling-xiao.yang@mail.mcgill.ca>
2015-07-28 11:50:03 -0400

    Test: project deletion test case for issue #378

---
+++ rodan/test/models/test_project.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+from django.db.models import ProtectedError
+from model_mommy import mommy
+from rodan.test.helpers import RodanTestTearDownMixin, RodanTestSetUpMixin
+
+
+class ProjectTestCase(RodanTestTearDownMixin, TestCase, RodanTestSetUpMixin):
+    def setUp(self):
+        self.setUp_rodan()
+
+    def test_delete_project_with_workflowrun(self):
+        prj = mommy.make('rodan.Project')
+        wfrun = mommy.make('rodan.WorkflowRun', project=prj)
+        rj = mommy.make('rodan.RunJob', workflow_run=wfrun)
+        resi = mommy.make('rodan.Resource', project=prj)
+        reso = mommy.make('rodan.Resource', project=prj)
+        i = mommy.make('rodan.Input', run_job=rj, resource=resi)
+        o = mommy.make('rodan.Output', run_job=rj, resource=reso)
+        reso.origin = o
+        reso.save()
+
+        wfrun2 = mommy.make('rodan.WorkflowRun', project=prj)
+        rj2 = mommy.make('rodan.RunJob', workflow_run=wfrun2)
+        reso2 = mommy.make('rodan.Resource', project=prj)
+        i2 = mommy.make('rodan.Input', run_job=rj, resource=reso)
+        o2 = mommy.make('rodan.Output', run_job=rj, resource=reso2)
+        reso2.origin = o
+        reso2.save()
+
+        try:
+            prj.delete()
+        except ProtectedError as e:
+            self.fail("ProtectedError when deleting project: {0}".format(e))
