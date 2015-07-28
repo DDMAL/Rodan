301aaa68695fb7854ae94db3bcdbf3741a8027d6
Yihong Luo <szyhluo@hotmail.com>
2015-07-28 16:38:43 -0400

    Refactor: Removed receiver for publish Redis messages since it's included in the update_database daemon

--- rodan/models/__init__.py
+++ rodan/models/__init__.py
@@ -14,25 +14,3 @@ from rodan.models.resultspackage import ResultsPackage
 from rodan.models.resource import Resource
 from rodan.models.resourcetype import ResourceType
 from rodan.models.connection import Connection
-
-from django.db.models.signals import post_save, pre_delete
-from django.dispatch import receiver
-from ws4redis.publisher import RedisPublisher
-from ws4redis.redis_store import RedisMessage
-import json
-import psycopg2
-import psycopg2.extensions
-
-@receiver(post_save)
-def notify_socket_subscribers(sender, instance, created, **kwargs):
-    publisher = RedisPublisher(facility='rodan', broadcast=True)
-    status = "created" if created else "updated"
-    uuid = "{0}".format(instance.uuid) if hasattr(instance, "uuid") else ""
-    data = {
-        'status': status,
-        'model': instance.__class__.__name__,
-        'uuid': uuid
-    }
-    message = RedisMessage(json.dumps(data))
-    publisher.publish_message(message)
-
