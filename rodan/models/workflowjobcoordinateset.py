ce18ce81b7b666c6fb914d67ea78fc2b4211cc86
Ling-Xiao Yang <ling-xiao.yang@mail.mcgill.ca>
2015-08-05 10:32:04 -0400

    Fixed: WorkflowJobCoordinateSet model -- use JSON field containing coordinate and appearance data

--- rodan/models/workflowjobcoordinateset.py
+++ rodan/models/workflowjobcoordinateset.py
@@ -1,5 +1,6 @@
 from django.db import models
 from uuidfield import UUIDField
+from jsonfield import JSONField
 
 
 class WorkflowJobCoordinateSet(models.Model):
@@ -11,16 +12,14 @@ class WorkflowJobCoordinateSet(models.Model):
 
     - `uuid`
     - `workflow_job` -- a reference to the `WorkflowJob`.
-    - `x` -- `x` coordinate of the center position (% of the canvas size).
-    - `y` -- `y` coordinate of the center position (% of the canvas size).
+    - `data` -- a JSON field including: `x` -- coordinate of the center position (% of the canvas size); `y` -- `y` coordinate of the center position (% of the canvas size); `width` -- relative width regarding the canvas size; `height` -- relative height regarding the canvas size; `color` (optional) -- CSS color code.
     - `user_agent` -- (optional) the name of user agent.
     - `created`
     - `updated`
     """
     uuid = UUIDField(primary_key=True, auto=True)
     workflow_job = models.ForeignKey('rodan.WorkflowJob', related_name="workflow_job_coordinate_sets", on_delete=models.CASCADE)
-    x = models.PositiveSmallIntegerField()
-    y = models.PositiveSmallIntegerField()
+    data = JSONField(default={}, blank=True, null=True)
     user_agent = models.CharField(max_length=255, blank=True, null=True)
 
     created = models.DateTimeField(auto_now_add=True)
