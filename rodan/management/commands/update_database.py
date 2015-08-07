b0dad3b93a956ad1dcbb3b6fa720a80ebc11f470
Yihong Luo <szyhluo@hotmail.com>
2015-08-07 17:02:56 -0400

    New: Push Redis messages after database update

--- rodan/management/commands/update_database.py
+++ rodan/management/commands/update_database.py
@@ -9,6 +9,7 @@ from ws4redis.redis_store import RedisMessage
 import json
 import datetime
 import os
+import time
 
 class Command(BaseCommand):
     help = 'Running the command update_database will work as a daemon running in the backend and any update in the database will trigger a notification that will be broadcasted as a Redis message.'
@@ -16,32 +17,6 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         setattr(settings, "_update_database", True)
 
-        def notify_socket_subscribers(notify):
-            ''' Convert the notify message into a Redis message and broadcast to all subscribers '''
-
-            publisher = RedisPublisher(facility='rodan', broadcast=True)
-            info = notify.split('/')
-            status = info[0]
-            model = info[1]
-            model = model.replace('rodan_', '')
-            uuid = info[2]
-            uuid = uuid.replace('-', '')
-
-            data = {
-                'status': status,
-                'model': model,
-                'uuid': uuid
-            }
-            message = RedisMessage(json.dumps(data))
-            publisher.publish_message(message) 
-
-        def handle_notification(notify):
-            ''' Send the notify message to Redis message queue, and print the notification with the time '''
-
-            notify_socket_subscribers(notify.payload) 
-            print "Got NOTIFY:", notify.pid, notify.channel, notify.payload
-            print datetime.datetime.now().time()
-        
         conn = psycopg2.connect(database="postgres", host=os.environ.get('HOSTNAME'), user=os.environ.get('USER'), password="")
         conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
 
@@ -50,11 +25,41 @@ class Command(BaseCommand):
         curs.execute('LISTEN "test";')
         print "Waiting for notifications on channel 'test'"
 
+        print_notify = '''
+            CREATE OR REPLACE FUNCTION print_notify(notify text) RETURNS void AS $$
+                import datetime
+                import os
+                import sys
+                import json
+                import redis
+
+                info = notify.split('/')
+                status = info[0]
+                model = info[1].replace('rodan_','')
+                uuid = info[2].replace('-','')
+
+                data = {
+                    'status': status,
+                    'model': model,
+                    'uuid': uuid
+                }
+
+                r = redis.StrictRedis(host='localhost', port=6379, db=0)
+                r.publish('rodan:broadcast:rodan', json.dumps(data))
+
+                print "Got NOTIFY:", notify
+                print datetime.datetime.now().time()
+
+            $$ LANGUAGE plpythonu;
+            '''
+
+
         # Create trigger that sends information about the status, the table name, and the uuid of the modified element
         trigger = '''
             CREATE OR REPLACE FUNCTION object_notify() RETURNS trigger AS $$
             DECLARE
                 status text;
+                notify text;
             BEGIN
                 IF (TG_OP = 'INSERT') THEN
                     status = 'created';
@@ -63,7 +68,9 @@ class Command(BaseCommand):
                 ELSIF (TG_OP = 'DELETE') THEN
                     status = 'deleted';
                 END IF;
-                PERFORM pg_notify('test', status || '/' || CAST(TG_TABLE_NAME AS text) || '/' || CAST(NEW.uuid AS text));
+                notify = status || '/' || CAST(TG_TABLE_NAME AS text) || '/' || CAST(NEW.uuid AS text);
+                -- PERFORM pg_notify('test', notify);
+                PERFORM print_notify(notify);
                 RETURN NEW;
             END;
             $$ LANGUAGE plpgsql;
@@ -88,11 +95,6 @@ class Command(BaseCommand):
             SELECT name();
             '''
 
+        curs.execute(print_notify)
         curs.execute(trigger)
         curs.execute(create_trigger)
-
-        while True:
-            conn.poll()
-            while conn.notifies:
-                notify = conn.notifies.pop()
-                handle_notification(notify)
