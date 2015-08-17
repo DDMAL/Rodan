be188380ea4c320bfd0a3d726f794be030d82670
Yihong Luo <szyhluo@hotmail.com>
2015-08-17 16:10:39 -0400

    New: Stress test script with flag options

--- rodan/test/test_load.sh
+++ rodan/test/test_load.sh
@@ -1,34 +1,36 @@
 #!/bin/bash
 
 dt=`date '+%Y-%m-%d_%H:%M:%S'`
+amount=10000
+number=1
+server='localhost:8000'
 
 if [ ! -d 'test_log' ]; then
     mkdir test_log
 fi
 
-echo 'Input the amount of connections you want to test'
-read amount
-
-echo 'Input the number of messages per connection'
-read num
-
-echo 'Please enter the server you would like to test on'
-options=("rodan.simssa.ca" "rodan-dev.simssa.ca" "localhost")
-select server in "${options[@]}";
-do
-    case $server in
-        "rodan.simssa.ca")
-            thor --amount $amount --messages $num ws://$server/ws/ > ./test_log/$dt.txt
-            break
+while getopts 'ha:n:s' flag; do
+    case "${flag}" in
+        h)
+            echo "Options:"
+            echo "-a            Number of connections to test on, if unspecified, 10000 connections will be tested"
+            echo "-n            Number of messages to be sent per connection"
+            echo "-s            Name of the server, eg. rodan.simssa.ca. if unspecified, server will be localhost"
+            echo "-h            Show help instructions"
+            exit 0
             ;;
-        "rodan-dev.simssa.ca")
-            thor --amount $amount --messages $num ws://$server/ws/ > ./test_log/$dt.txt
-            break
+        a)  amount=${OPTARG} ;;
+        n)  number=${OPTARG} ;;
+        s)  server=${OPTARG} ;;
+        *)
+            echo Invalid argument ${flag}
+            exit 0
             ;;
-        "localhost")
-            thor --amount $amount --messages $num ws://$server:8000/ws/ > ./test_log/$dt.txt
-            break
-            ;;
-        *) echo Invalid option;;
     esac
 done
+
+echo "Number of connections tested: $amount" >> ./test_log/$dt.txt
+echo "Number of messages sent per connection: $number" >>  ./test_log/$dt.txt
+thor --amount $amount --messages $number ws://$server/ws/ >> ./test_log/$dt.txt
+echo "Test successfully executed"
+exit 0
