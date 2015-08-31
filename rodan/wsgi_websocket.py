1901ba8229ac27d1500c7a8c3395e30cc79852a9
Ling-Xiao Yang <ling-xiao.yang@mail.mcgill.ca>
2015-08-31 19:31:45 +0000

    WIP: fix websocket conf

--- rodan/wsgi_websocket.py
+++ rodan/wsgi_websocket.py
@@ -1,8 +1,8 @@
-mport os
+import os
 import gevent.socket
 import redis.connection
 redis.connection.socket = gevent.socket
-os.environ.update(DJANGO_SETTINGS_MODULE='my_app.settings')
+os.environ.update(DJANGO_SETTINGS_MODULE='rodan.settings')
 from ws4redis.uwsgi_runserver import uWSGIWebsocketServer
 application = uWSGIWebsocketServer()
 
