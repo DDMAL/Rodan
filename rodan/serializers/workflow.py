6113590d6da0cbcfa4ea3452fad7e34813570331
Ling-Xiao Yang <ling-xiao.yang@mail.mcgill.ca>
2015-10-27 10:28:04 -0400

    Fixed: #403. Add description field to workflow list

--- rodan/serializers/workflow.py
+++ rodan/serializers/workflow.py
@@ -69,6 +69,7 @@ class WorkflowListSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('url',
                   "uuid",
                   'project',
+                  "description",
                   'creator',
                   'uuid',
                   'name',
