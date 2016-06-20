267f9bd30b0b88c52ef78ddf09a99005285de9a5
Nicky <negareh.mirfallahliarestani@mail.mcgill.ca>
2016-06-20 12:03:33 -0400

    put all migrations into 1 migration file

--- rodan/migrations/0007_auto_20160620_1153.py
+++ rodan/migrations/0007_auto_20160620_1153.py
@@ -7,12 +7,20 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('rodan', '0008_merge'),
+        ('rodan', '0006_auto_20160607_1454'),
     ]
 
     operations = [
         migrations.RemoveField(
             model_name='resource',
+            name='compat_resource_file',
+        ),
+        migrations.RemoveField(
+            model_name='resource',
             name='has_thumb',
+        ),
+        migrations.RemoveField(
+            model_name='resourcelist',
+            name='resource_type',
         ),
     ]
