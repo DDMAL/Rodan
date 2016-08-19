26b6e78974ee61b85d8a25b0fc4cd6da660c823e
Nicky <negareh.mirfallahliarestani@mail.mcgill.ca>
2016-08-19 15:24:47 -0400

    Fix:using OneToOneField instead of Unique ForeignKey

--- rodan/models/tempauthtoken.py
+++ rodan/models/tempauthtoken.py
@@ -22,7 +22,7 @@ class Tempauthtoken(models.Model):
 
     uuid = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     expiry = models.DateTimeField(null=True, db_index=True)
-    user = models.ForeignKey(User, related_name="temp_authtoken", on_delete=models.CASCADE, unique=True)
+    user = models.OneToOneField(User, related_name="temp_authtoken", on_delete=models.CASCADE)
 
     def __unicode__(self):
         return u"<TempAuthtoken {0} {1}>".format(str(self.uuid), self.user_id)
