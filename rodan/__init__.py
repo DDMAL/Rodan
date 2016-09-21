5563f0737c8ce7cf8f1317bb6fdef7007063981d
Ryan Bannon <ryan.bannon@gmail.com>
2016-09-21 13:37:21 -0400

    VERSION: v1.1.0

--- rodan/__init__.py
+++ rodan/__init__.py
@@ -14,7 +14,7 @@ __title__ = "Rodan"
 
 # Module version following PEP 396
 # Get version: import rodan; rodan.__version__
-__version__ = "1.0.1-alpha"
+__version__ = "1.1.0"
 
 __copyright__ = "Copyright 2011-2015 Distributed Digital Music Archives & Libraries Lab"
 
