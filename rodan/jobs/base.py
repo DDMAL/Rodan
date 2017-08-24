458cd0309dac55d5855b951a63023dcb6d8986cc
Matan Gover <matangover@gmail.com>
2017-08-24 17:14:34 -0400

    Add support for RODAN_NON_INTERACTIVE in load.py.

--- rodan/jobs/base.py
+++ rodan/jobs/base.py
@@ -678,6 +678,7 @@ def TemporaryDirectory():
 
 def confirm(prompt, default=True):
     if os.environ.get("RODAN_NON_INTERACTIVE") == "true":
+        print "Using default of '%s' for prompt '%s'" % (default, prompt)
         return default
     else:
         return raw_input(prompt).lower() == 'y'    
