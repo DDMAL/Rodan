828fb1dc7ebb8ec67b10435a30635a26bd4cc65e
deepio <alex.daigle2@gmail.com>
2019-10-22 13:15:33 -0400

    Update resource.py

--- rodan/serializers/resource.py
+++ rodan/serializers/resource.py
@@ -12,7 +12,7 @@ class ResourceSerializer(serializers.HyperlinkedModelSerializer):
     small_thumb = AbsoluteURLField(source="small_thumb_url", read_only=True)
     medium_thumb = AbsoluteURLField(source="medium_thumb_url", read_only=True)
     large_thumb = AbsoluteURLField(source="large_thumb_url", read_only=True)
-    viewer_url = AbsoluteURLField(read_only=True, source="viewer_relurl")
+    viewer_url = AbsoluteURLField(source="viewer_relurl", read_only=True)
 
     class Meta:
         model = Resource
