fc59249ee44c2f82b80e134c983b6d0d58b4c4d5
deepio <alex.daigle2@gmail.com>
2020-02-25 12:38:00 -0500

    Added: Test files to filetype ident

--- rodan/jobs/resource_identification.py
+++ rodan/jobs/resource_identification.py
@@ -20,6 +20,8 @@ def define_png(filename, mime=None):
     png_mimetypes = {
         "1-bit grayscale": "image/onebit+png",
         "16-bit grayscale": "image/grey16+png",
+        # image/rgb+png
+        # image/rgba+png
     }
 
     try:
@@ -41,69 +43,61 @@ def define_xml(filename, mime=None):
         data = f.readlines()
 
     xml_mimetypes = {
-        """<gamera-database version="2.0">\n""": "application/gamera+xml",
-        """<!DOCTYPE feature_vector_file [\n""": "application/ace+xml",
+        """<gamera-database version="2.0">""": "application/gamera+xml",
+        """<!DOCTYPE feature_vector_file [""": "application/ace+xml",
     }
 
     try:
-        return xml_mimetypes[data[1]]
+        return xml_mimetypes[data[1].strip()]
     except KeyError:
         # is MusicXML
-        if re.search(r"-//Recordare//DTD MusicXML", data[1]) is not None:
-            # [TODO] application/vnd.recordare.musicxml(+xml)
-            return "application/x-musicxml+xml"
+        # [TODO] application/vnd.recordare.musicxml(+xml)
+        if re.search(re.compile(r"""-//Recordare//DTD MusicXML"""), data[1]) is not None:
+            return "application/x-muscxml+xml"
 
-        if re.search(r"<mei xml", data[1]) is not None:
+        if re.search(r"""<mei xml""", data[1]) is not None:
             return "application/mei+xml"
-
-    except IndexError:
-        if (
-            re.search(r"^\[\[", data[0]) is not None
-            and re.search(r"\]\]$", data[0]) is not None
-        ):
-            return "application/gamera-polygons+txt"
 
     return "application/octet-stream", data[1]
 
 
 def define_text(filename, mime=None):
     with open(filename) as f:
-        data = f.readlines()
+        data = f.readline().strip()
 
-    # This is the top line of how these csv files look like.
-    # Do not cut these lines short.
     text_mimetypes = {
-        "Indexer,interval.IntervalIndexer,interval.IntervalIndexer,interval.IntervalIndexer,interval.IntervalIndexer,interval.IntervalIndexer,interval.IntervalIndexer\n": "application/x-vis_vertical_pandas_series+csv",  # noqa
-        "Indexer,interval.HorizontalIntervalIndexer,interval.HorizontalIntervalIndexer,interval.HorizontalIntervalIndexer,interval.HorizontalIntervalIndexer,interval.HorizontalIntervalIndexer\n": "application/x-vis_horizontal_pandas_series+csv",  # noqa
-        "Indexer,offset.FilterByOffsetIndexer,offset.FilterByOffsetIndexer,offset.FilterByOffsetIndexer\n": "application/x-vis_noterest_pandas_series+csv",  # noqa
-        "Indexer,ngram.NGramIndexer\n": "application/x-vis_ngram_pandas_dataframe+csv",
-        "@relation Converted_from_ACE_XML\n": "application/arff",
-        "Duration,Acoustic_Guitar_Fraction,Amount_of_Arpeggiation,Average_Melodic_Interval,Average_Note_Duration,Average_Note_To_Note_Dynamics_Change,Average_Number_of_Independent_Voices,Average_Range_of_Glissandos,Average_Time_Between_Attacks,Average_Time_Between_Attacks_For_Each_Voice,Average_Variability_of_Time_Between_Attacks_For_Each_Voice,Brass_Fraction,Changes_of_Meter,Chromatic_Motion,Combined_Strength_of_Two_Strongest_Rhythmic_Pulses,Compound_Or_Simple_Meter,Direction_of_Motion,Distance_Between_Most_Common_Melodic_Intervals,Dominant_Spread,Duration_of_Melodic_Arcs,Electric_Guitar_Fraction,Electric_Instrument_Fraction,Glissando_Prevalence,Harmonicity_of_Two_Strongest_Rhythmic_Pulses,Importance_of_Bass_Register,Importance_of_High_Register,Importance_of_Loudest_Voice,Importance_of_Middle_Register,Initial_Tempo,Interval_Between_Strongest_Pitch_Classes,Interval_Between_Strongest_Pitches,Maximum_Note_Duration,Maximum_Number_of_Independent_Voices,Melodic_Fifths,Melodic_Intervals_in_Lowest_Line,Melodic_Octaves,Melodic_Thirds,Melodic_Tritones,Minimum_Note_Duration,Most_Common_Melodic_Interval,Most_Common_Melodic_Interval_Prevalence,Most_Common_Pitch_Class,Most_Common_Pitch_Class_Prevalence,Most_Common_Pitch,Most_Common_Pitch_Prevalence,Note_Density,Number_of_Common_Melodic_Intervals,Number_of_Common_Pitches,Number_of_Moderate_Pulses,Number_of_Pitched_Instruments,Number_of_Relatively_Strong_Pulses,Number_of_Strong_Pulses,Number_of_Unpitched_Instruments,Orchestral_Strings_Fraction,Overall_Dynamic_Range,Percussion_Prevalence,Pitch_Class_Variety,Pitch_Variety,Polyrhythms,Primary_Register,Quality,Quintuple_Meter,Range,Range_of_Highest_Line,Relative_Note_Density_of_Highest_Line,Relative_Range_of_Loudest_Voice,Relative_Strength_of_Most_Common_Intervals,Relative_Strength_of_Top_Pitch_Classes,Relative_Strength_of_Top_Pitches,Repeated_Notes,Rhythmic_Looseness,Rhythmic_Variability,Saxophone_Fraction,Second_Strongest_Rhythmic_Pulse,Size_of_Melodic_Arcs,Staccato_Incidence,Stepwise_Motion,Strength_of_Second_Strongest_Rhythmic_Pulse,Strength_of_Strongest_Rhythmic_Pulse,Strength_Ratio_of_Two_Strongest_Rhythmic_Pulses,String_Ensemble_Fraction,String_Keyboard_Fraction,Strong_Tonal_Centres,Strongest_Rhythmic_Pulse,Triple_Meter,Variability_of_Note_Duration,Variability_of_Note_Prevalence_of_Pitched_Instruments,Variability_of_Note_Prevalence_of_Unpitched_Instruments,Variability_of_Number_of_Independent_Voices,Variability_of_Time_Between_Attacks,Variation_of_Dynamics,Variation_of_Dynamics_In_Each_Voice,Vibrato_Prevalence,Violin_Fraction,Voice_Equality_-_Dynamics,Voice_Equality_-_Melodic_Leaps,Voice_Equality_-_Note_Duration,Voice_Equality_-_Number_of_Notes,Voice_Equality_-_Range,Voice_Separation,Woodwinds_Fraction,\n": "application/arff+csv",  # noqa
-        ",Basso seguente,Figured bass\n": "application/x-vis_figuredbass_pandas_series+csv",
-        "<features_to_extract>\n": "application/jsc+txt",
+        "Indexer,interval.IntervalIndexer,interval.IntervalIndexer,interval.IntervalIndexer,interval.IntervalIndexer,interval.IntervalIndexer,interval.IntervalIndexer": "application/x-vis_vertical_pandas_series+csv",  # noqa
+        "Indexer,interval.HorizontalIntervalIndexer,interval.HorizontalIntervalIndexer,interval.HorizontalIntervalIndexer,interval.HorizontalIntervalIndexer,interval.HorizontalIntervalIndexer": "application/x-vis_horizontal_pandas_series+csv",  # noqa
+        "Indexer,offset.FilterByOffsetIndexer,offset.FilterByOffsetIndexer,offset.FilterByOffsetIndexer": "application/x-vis_noterest_pandas_series+csv",  # noqa
+        "Indexer,ngram.NGramIndexer": "application/x-vis_ngram_pandas_dataframe+csv",
+        "@relation Converted_from_ACE_XML": "application/arff",
+        "Duration,Acoustic_Guitar_Fraction,Amount_of_Arpeggiation,Average_Melodic_Interval,Average_Note_Duration,Average_Note_To_Note_Dynamics_Change,Average_Number_of_Independent_Voices,Average_Range_of_Glissandos,Average_Time_Between_Attacks,Average_Time_Between_Attacks_For_Each_Voice,Average_Variability_of_Time_Between_Attacks_For_Each_Voice,Brass_Fraction,Changes_of_Meter,Chromatic_Motion,Combined_Strength_of_Two_Strongest_Rhythmic_Pulses,Compound_Or_Simple_Meter,Direction_of_Motion,Distance_Between_Most_Common_Melodic_Intervals,Dominant_Spread,Duration_of_Melodic_Arcs,Electric_Guitar_Fraction,Electric_Instrument_Fraction,Glissando_Prevalence,Harmonicity_of_Two_Strongest_Rhythmic_Pulses,Importance_of_Bass_Register,Importance_of_High_Register,Importance_of_Loudest_Voice,Importance_of_Middle_Register,Initial_Tempo,Interval_Between_Strongest_Pitch_Classes,Interval_Between_Strongest_Pitches,Maximum_Note_Duration,Maximum_Number_of_Independent_Voices,Melodic_Fifths,Melodic_Intervals_in_Lowest_Line,Melodic_Octaves,Melodic_Thirds,Melodic_Tritones,Minimum_Note_Duration,Most_Common_Melodic_Interval,Most_Common_Melodic_Interval_Prevalence,Most_Common_Pitch_Class,Most_Common_Pitch_Class_Prevalence,Most_Common_Pitch,Most_Common_Pitch_Prevalence,Note_Density,Number_of_Common_Melodic_Intervals,Number_of_Common_Pitches,Number_of_Moderate_Pulses,Number_of_Pitched_Instruments,Number_of_Relatively_Strong_Pulses,Number_of_Strong_Pulses,Number_of_Unpitched_Instruments,Orchestral_Strings_Fraction,Overall_Dynamic_Range,Percussion_Prevalence,Pitch_Class_Variety,Pitch_Variety,Polyrhythms,Primary_Register,Quality,Quintuple_Meter,Range,Range_of_Highest_Line,Relative_Note_Density_of_Highest_Line,Relative_Range_of_Loudest_Voice,Relative_Strength_of_Most_Common_Intervals,Relative_Strength_of_Top_Pitch_Classes,Relative_Strength_of_Top_Pitches,Repeated_Notes,Rhythmic_Looseness,Rhythmic_Variability,Saxophone_Fraction,Second_Strongest_Rhythmic_Pulse,Size_of_Melodic_Arcs,Staccato_Incidence,Stepwise_Motion,Strength_of_Second_Strongest_Rhythmic_Pulse,Strength_of_Strongest_Rhythmic_Pulse,Strength_Ratio_of_Two_Strongest_Rhythmic_Pulses,String_Ensemble_Fraction,String_Keyboard_Fraction,Strong_Tonal_Centres,Strongest_Rhythmic_Pulse,Triple_Meter,Variability_of_Note_Duration,Variability_of_Note_Prevalence_of_Pitched_Instruments,Variability_of_Note_Prevalence_of_Unpitched_Instruments,Variability_of_Number_of_Independent_Voices,Variability_of_Time_Between_Attacks,Variation_of_Dynamics,Variation_of_Dynamics_In_Each_Voice,Vibrato_Prevalence,Violin_Fraction,Voice_Equality_-_Dynamics,Voice_Equality_-_Melodic_Leaps,Voice_Equality_-_Note_Duration,Voice_Equality_-_Number_of_Notes,Voice_Equality_-_Range,Voice_Separation,Woodwinds_Fraction,": "application/arff+csv",  # noqa
+        ",Basso seguente,Figured bass": "application/x-vis_figuredbass_pandas_series+csv",
+        "<features_to_extract>": "application/jsc+txt",
     }
 
     try:
-        return text_mimetypes[data[0]]
+        return text_mimetypes[data]
     except KeyError:
 
-        # Files that start with [[ and end with ]]
-        if (
-            re.search(r"^\[\[", data[0]) is not None
-            and re.search(r"\]\]$", data[0]) is not None
-        ):
+        if re.search(r"^\[\[", data) is not None and re.search(r"\]\]$", data) is not None:
             return "application/gamera-polygons+txt"
 
-        try:
-            commas_line1 = data[0].count(",")
-            commas_line2 = data[1].count(",")
+        # For detecting regular CSV's, we need more than the first line
+        # Fallback, also if the newline character messed the json identification in libmagic.
+        with open(filename) as f:
+            data = f.readlines()
 
-            if commas_line1 > 0 and commas_line1 == commas_line2:
-                return "text/csv"
-        except IndexError:
-            pass
+        if re.search(r"^\{", data[0]) is not None and re.search(r"^\}$", data[-1]) is not None:
+            return "application/json"
 
-    return "text/plain"
+        commas_line1 = data[0].count(",")
+        commas_line2 = data[1].count(",")
+
+        if commas_line1 > 0 and commas_line1 == commas_line2:
+            return "text/csv"
+
+    return "text/plain",
 
 
 def define_stream(filename, mime=None):
@@ -113,7 +107,6 @@ def define_stream(filename, mime=None):
     if data[0:2] == b"\x80\x02":
         # [TODO] Change to application/x-ocropus+pyrnn
         return "application/ocropus+pyrnn"
-
     return "application/octet-stream"
 
 
@@ -136,5 +129,5 @@ def fileparse(filename):
     try:
         return mimetype_translation[magic_mime](filename, text_description)
     except KeyError:
-        return "application/octet-stream", text_description, magic_mime
+        return "application/octet-stream"
     return "application/octet-stream"
