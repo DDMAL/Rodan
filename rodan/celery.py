319940de69e643a31b9068af1023baa041363672
Kemal Kongar <47402993+kemalkongar@users.noreply.github.com>
2021-06-01 09:55:30 -0400

    revert

--- rodan/celery.py
+++ rodan/celery.py
@@ -21,7 +21,6 @@ from rodan.jobs.core import (  # noqa
     redo_runjob_tree,
     retry_workflowrun,
     send_email,
-    HelloWorld,
 )
 from rodan.jobs.master_task import master_task  # noqa
 
@@ -35,5 +34,5 @@ app.tasks.register(redo_runjob_tree)
 app.tasks.register(retry_workflowrun)
 app.tasks.register(send_email)
 app.tasks.register(master_task)
-app.tasks.register(HelloWorld)
+#app.tasks.register(HelloWorld)
 #app.tasks.register(test_task)
--- rodan/jobs/core.py
+++ rodan/jobs/core.py
@@ -40,9 +40,6 @@ from rodan.constants import task_status
 from rodan.jobs.base import TemporaryDirectory
 from rodan.jobs.diva_generate_json import GenerateJson
 from rodan.jobs.resource_identification import fileparse
-from rodan.jobs.base import RodanTask
-
-
 # from rodan.celery import app
 
 
@@ -1034,36 +1031,6 @@ def create_archive(resource_uuids):
 
     temporary_storage.seek(0)
     return temporary_storage
-
-class HelloWorld(RodanTask):
-    name = 'Hello World'
-    author = 'Ryan Bannon'
-    description = 'Output string "Hello World"'
-    settings = {}
-    enabled = True
-    category = "Test"
-    interactive = False
-
-    input_port_types = (
-        {'name': 'Text input', 'minimum': 0, 'maximum': 1, 'resource_types': ['text/plain']},
-    )
-    output_port_types = (
-        {'name': 'Text output', 'minimum': 1, 'maximum': 1, 'resource_types': ['text/plain']},
-    )
-
-    def run_my_task(self, inputs, settings, outputs):
-        outfile_path = outputs['Text output'][0]['resource_path']
-        outfile = open(outfile_path, "w")
-        if 'Text input' in inputs:
-            infile_path = inputs['Text input'][0]['resource_path']
-            infile = open(infile_path, "r")
-            outfile.write(("Hello World {0}").format(infile.read()))
-            infile.close()
-        else:
-            outfile.write("Hello World")
-        outfile.close()
-        return True
-
 
 
 # app.tasks.register(create_resource())
