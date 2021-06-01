e018439c914bab2f73598bb216844cbff207bf97
Kemal Kongar <47402993+kemalkongar@users.noreply.github.com>
2021-06-01 10:53:55 -0400

    testing celery import -- rodan import works

--- rodan/celery.py
+++ rodan/celery.py
@@ -25,6 +25,8 @@ from rodan.jobs.core import (  # noqa
 )
 from rodan.jobs.master_task import master_task  # noqa
 
+from rodan.jobs.test_rodan.test_rodan import test_rodan_task
+
 # Core Rodan Tasks
 app.tasks.register(create_resource())
 app.tasks.register(create_workflowrun())
@@ -36,5 +38,6 @@ app.tasks.register(retry_workflowrun)
 app.tasks.register(send_email)
 app.tasks.register(master_task)
 app.tasks.register(test_work)
+app.tasks.register(test_rodan_task)
 #app.tasks.register(HelloWorld)
 #app.tasks.register(test_task)
