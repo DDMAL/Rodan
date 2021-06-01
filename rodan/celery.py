c678fec7625add38193eb139a25b63118bccb96c
Kemal Kongar <47402993+kemalkongar@users.noreply.github.com>
2021-06-01 09:51:30 -0400

    added helloworld to celery.py

--- rodan/celery.py
+++ rodan/celery.py
@@ -21,11 +21,9 @@ from rodan.jobs.core import (  # noqa
     redo_runjob_tree,
     retry_workflowrun,
     send_email,
+    HelloWorld,
 )
 from rodan.jobs.master_task import master_task  # noqa
-
-#from rodan.jobs.helloworld.helloworld import HelloWorld
-
 
 # Core Rodan Tasks
 app.tasks.register(create_resource())
@@ -37,5 +35,5 @@ app.tasks.register(redo_runjob_tree)
 app.tasks.register(retry_workflowrun)
 app.tasks.register(send_email)
 app.tasks.register(master_task)
-#app.tasks.register(HelloWorld)
+app.tasks.register(HelloWorld)
 #app.tasks.register(test_task)
