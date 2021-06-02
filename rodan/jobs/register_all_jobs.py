8b18e4fb9970ad369c731a64ed4d7f05d80f0bbd
GabbyHalpin <gabriellehalpin@icloud.com>
2021-06-02 11:29:00 -0400

    commented out all python 2 jobs

--- rodan/jobs/register_all_jobs.py
+++ rodan/jobs/register_all_jobs.py
@@ -9,31 +9,33 @@ try:
     # from rodan.jobs.gamera_rodan import
     # from rodan.jobs.helloworld import
     # from rodan.jobs.heuristic-pitch-finding import MiyaoStaffinding
-    from rodan.jobs.JSOMR2MEI.base import JSOMR2MEI
+    f  # rom rodan.jobs.JSOMR2MEI.base import JSOMR2MEI
 
     # from rodan.jobs.jSymbolic-Rodan import extract_features
-    from rodan.jobs.MEI_encoding.MEI_encoding import MEI_encoding
+    # from rodan.jobs.MEI_encoding.MEI_encoding import MEI_encoding
 
     # from rodan.jobs.neon-wrapper import Neon
     # from rodan.jobs.pixel_wrapper import PixelInteractive
 
     # from rodan.jobs.vis-rodan import
     # from rodan.jobs.biollante-rodan import BiollanteRodan
-    from rodan.jobs.MEI_resizing.mei_resizing import MEI_Resize
-except(ImportError):
-    raise ImportError("Problem importing jobs, make sure imports are configured correctly")
+    # from rodan.jobs.MEI_resizing.mei_resizing import MEI_Resize
+except (ImportError):
+    raise ImportError(
+        "Problem importing jobs, make sure imports are configured correctly"
+    )
 
 
 def run_register_jobs():
     # Python2 jobs
     app.register_task(InteractiveClassifier())
-    app.register_task(JSOMR2MEI())
+    # app.register_task(JSOMR2MEI())
     # app.register_task(extract_features())
     # app.register_task(MiyaoStaffinding())
-    app.register_task(MEI_encoding())
+    # app.register_task(MEI_encoding())
     # app.register_task(Neon())
     # app.register_task(PixelInteractive())
-    app.register_task(MEI_Resize())
+    # app.register_task(MEI_Resize())
     # app.register_task(DiagonalNeumeSlicing())
     # app.register_task(BiollanteRodan())
 
