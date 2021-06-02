87436fe0b49a9273182b0d834eda697d17247b54
Kemal Kongar <47402993+kemalkongar@users.noreply.github.com>
2021-06-02 14:34:37 -0400

    i just DID that

--- rodan/jobs/register_all_jobs.py
+++ rodan/jobs/register_all_jobs.py
@@ -88,43 +88,43 @@ def run_register_jobs():
     except ImportError as error:
         print(error.__class__.__name__ + ": " + error.message)
 
-    # TODO: handle "-" imports
+    # TODO: handle "_" imports
     # TODO: complete the python 3 jobs
-    # from rodan.jobs.hpc-fast-trainer.hpc-fast-trainer import HPCFastTrainer
+    # from rodan.jobs.hpc_fast_trainer.hpc_fast_trainer import HPCFastTrainer
 
-    # from rodan.jobs.pil-rodan.red_filtering import RedFilter
-    # from rodan.jobs.pil-rodan.resize import resize
-    # from rodan.jobs.pil-rodan.to_jpeg2000 import to_jpeg2000
-    # from rodan.jobs.pil-rodan.to_png import to_png
-    # from rodan.jobs.pil-rodan.to_tiff import to_tiff
+    # from rodan.jobs.pil_rodan.red_filtering import RedFilter
+    # from rodan.jobs.pil_rodan.resize import resize
+    # from rodan.jobs.pil_rodan.to_jpeg2000 import to_jpeg2000
+    # from rodan.jobs.pil_rodan.to_png import to_png
+    # from rodan.jobs.pil_rodan.to_tiff import to_tiff
 
     # TODO: GPU jobs
-    # from rodan.jobs.Calvo-classifier import
+    # from rodan.jobs.Calvo_classifier import
     # from rodan.jobs.text_alignment import
 
-    # from rodan.jobs.diagonal-neume-slicing import DiagonalNeumeSlicing
-    # from rodan.jobs.diagonal-neume-slicing import DirtyLayerRepair
+    # from rodan.jobs.diagonal_neume_slicing import DiagonalNeumeSlicing
+    # from rodan.jobs.diagonal_neume_slicing import DirtyLayerRepair
     # from rodan.jobs.helloworld.helloworld import HelloWorld
     # from rodan.jobs.helloworld.helloworld import HelloWorldMultiPort
-    # from rodan.jobs.heuristic-pitch-finding import MiyaoStaffinding
-    # from rodan.jobs.heuristic-pitch-finding import HeuristicPitchFinding
+    # from rodan.jobs.heuristic_pitch_finding import MiyaoStaffinding
+    # from rodan.jobs.heuristic_pitch_finding import HeuristicPitchFinding
 
-    # from rodan.jobs.vis-rodan.wrappers.indexers.cadence_indexer import VRCadenceIndexer
-    # from rodan.jobs.vis-rodan.wrappers.indexers.dissonance_indexer import VRDissonanceIndexer
-    # from rodan.jobs.vis-rodan.wrappers.indexers.duration_indexer import VRDurationIndexer
-    # from rodan.jobs.vis-rodan.wrappers.indexers.fermata_indexer import VRFermataIndexer
-    # from rodan.jobs.vis-rodan.wrappers.indexers.figuredbass_indexer import VRFiguredBassIndexer
-    # from rodan.jobs.vis-rodan.wrappers.indexers.horizontal_interval_indexer import VRHorizontalIntervalIndexer
-    # from rodan.jobs.vis-rodan.wrappers.indexers.measure_indexer import VRMeasureIndexer
-    # from rodan.jobs.vis-rodan.wrappers.indexers.ngram_indexer import VRNGramIntervalIndexer
-    # from rodan.jobs.vis-rodan.wrappers.indexers.notebeatstrength_indexer import VRNoteBeatStrengthIndexer
-    # from rodan.jobs.vis-rodan.wrappers.indexers.noterest_indexer import VRNoteRestIndexer
-    # from rodan.jobs.vis-rodan.wrappers.indexers.offset_indexer import VROffsetIndexer
-    # from rodan.jobs.vis-rodan.wrappers.indexers.vertical_interval_indexer import VRVerticalIntervalIndexer
+    # from rodan.jobs.vis_rodan.wrappers.indexers.cadence_indexer import VRCadenceIndexer
+    # from rodan.jobs.vis_rodan.wrappers.indexers.dissonance_indexer import VRDissonanceIndexer
+    # from rodan.jobs.vis_rodan.wrappers.indexers.duration_indexer import VRDurationIndexer
+    # from rodan.jobs.vis_rodan.wrappers.indexers.fermata_indexer import VRFermataIndexer
+    # from rodan.jobs.vis_rodan.wrappers.indexers.figuredbass_indexer import VRFiguredBassIndexer
+    # from rodan.jobs.vis_rodan.wrappers.indexers.horizontal_interval_indexer import VRHorizontalIntervalIndexer
+    # from rodan.jobs.vis_rodan.wrappers.indexers.measure_indexer import VRMeasureIndexer
+    # from rodan.jobs.vis_rodan.wrappers.indexers.ngram_indexer import VRNGramIntervalIndexer
+    # from rodan.jobs.vis_rodan.wrappers.indexers.notebeatstrength_indexer import VRNoteBeatStrengthIndexer
+    # from rodan.jobs.vis_rodan.wrappers.indexers.noterest_indexer import VRNoteRestIndexer
+    # from rodan.jobs.vis_rodan.wrappers.indexers.offset_indexer import VROffsetIndexer
+    # from rodan.jobs.vis_rodan.wrappers.indexers.vertical_interval_indexer import VRVerticalIntervalIndexer
 
-    # from rodan.jobs.biollante-rodan import BiollanteRodan
-    # from rodan.jobs.jSymbolic-Rodan import extract_features
-    # from rodan.jobs.neon-wrapper import Neon
+    # from rodan.jobs.biollante_rodan import BiollanteRodan
+    # from rodan.jobs.jSymbolic_Rodan import extract_features
+    # from rodan.jobs.neon_wrapper import Neon
 
 
 if __name__ == "__main__":
