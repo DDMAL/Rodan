2fb0035bfb1f1e2c9af10b70e2388d92fbcc62e1
raviraina <ravi.raina@brentwood.ca>
2021-06-02 13:51:30 -0400

    better exception handling

--- rodan/jobs/register_all_jobs.py
+++ rodan/jobs/register_all_jobs.py
@@ -1,74 +1,75 @@
 from rodan.celery import app
 
-
+# hacky but works
 def run_register_jobs():
     try:
         from rodan.jobs.interactive_classifier.wrapper import InteractiveClassifier
 
         app.register_task(InteractiveClassifier())
-    except:
-        raise ImportError
+    except ImportError as error:
+        print(error.__class__.__name__ + ": " + error.message)
 
     try:
         from rodan.jobs.resource_distributor import ResourceDistributor
 
         app.register_task(ResourceDistributor())
-    except:
-        raise ImportError
+    except ImportError as error:
+        print(error.__class__.__name__ + ": " + error.message)
 
     try:
         from rodan.jobs.helloworld.helloworld import HelloWorld3
 
         app.register_task(HelloWorld3())
-    except:
-        raise ImportError
+    except ImportError as error:
+        print(error.__class__.__name__ + ": " + error.message)
 
     try:
         from rodan.jobs.labeler import Labeler
 
         app.register_task(Labeler())
-    except:
-        raise ImportError
+    except ImportError as error:
+        print(error.__class__.__name__ + ": " + error.message)
 
     try:
         from rodan.jobs.JSOMR2MEI.base import JSOMR2MEI
 
         app.register_task(JSOMR2MEI())
-    except:
-        raise ImportError
+    except ImportError as error:
+        print(error.__class__.__name__ + ": " + error.message)
 
     try:
-        # from rodan.jobs.jSymbolic-Rodan import extract_features
         from rodan.jobs.MEI_encoding.MEI_encoding import MEI_encoding
 
         app.register_task(MEI_encoding())
-    except:
-        raise ImportError
+    except ImportError as error:
+        print(error.__class__.__name__ + ": " + error.message)
 
     try:
-        # from rodan.jobs.neon-wrapper import Neon
         from rodan.jobs.pixel_wrapper.wrapper import PixelInteractive
 
         app.register_task(PixelInteractive())
-    except:
-        raise ImportError
+    except ImportError as error:
+        print(error.__class__.__name__ + ": " + error.message)
 
     try:
         from rodan.jobs.MEI_resizing.mei_resize import MEI_Resize
 
         app.register_task(MEI_Resize())
-    except:
-        raise ImportError
+    except ImportError as error:
+        print(error.__class__.__name__ + ": " + error.message)
 
     try:
         from rodan.jobs.gamera_rodan.wrappers.classification import ClassificationTask
-
         app.register_task(ClassificationTask())
-        from rodan.jobs.gamera_rodan.wrappers.masking import GameraMaskLogicalAnd
+    except ImportError as error:
+        print(error.__class__.__name__ + ": " + error.message)
 
+    try:
+        from rodan.jobs.gamera_rodan.wrappers.masking import GameraMaskLogicalAnd
         app.register_task(GameraMaskLogicalAnd())
-    except:
-        raise ImportError
+    except ImportError as error:
+        print(error.__class__.__name__ + ": " + error.message)
+
 
     # TODO: handle "-" imports
     # TODO: complete the python 3 jobs
@@ -84,6 +85,9 @@ def run_register_jobs():
     # from rodan.jobs.heuristic-pitch-finding import MiyaoStaffinding
     # from rodan.jobs.vis-rodan import
     # from rodan.jobs.biollante-rodan import BiollanteRodan
+
+    # from rodan.jobs.jSymbolic-Rodan import extract_features
+    # from rodan.jobs.neon-wrapper import Neon
 
 
 if __name__ == "__main__":
