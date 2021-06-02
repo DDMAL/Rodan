9cc6fe0f14f20fda35d17f5a36b4d64fb5dd2497
raviraina <ravi.raina@brentwood.ca>
2021-06-02 11:23:46 -0400

    error check

--- rodan/jobs/register_all_jobs.py
+++ rodan/jobs/register_all_jobs.py
@@ -1,24 +1,27 @@
-from rodan.jobs.interactive_classifier.wrapper import InteractiveClassifier
-from rodan.jobs.resource_distributor import ResourceDistributor
-from rodan.jobs.helloworld.helloworld import HelloWorld3
-from rodan.jobs.labeler import Labeler
-from rodan.celery import app
+try:
+    from rodan.jobs.interactive_classifier.wrapper import InteractiveClassifier
+    from rodan.jobs.resource_distributor import ResourceDistributor
+    from rodan.jobs.helloworld.helloworld import HelloWorld3
+    from rodan.jobs.labeler import Labeler
+    from rodan.celery import app
 
-# from rodan.jobs.diagonal-neume-slicing import DiagonalNeumeSlicing
-# from rodan.jobs.gamera_rodan import
-# from rodan.jobs.helloworld import
-# from rodan.jobs.heuristic-pitch-finding import MiyaoStaffinding
-from rodan.jobs.JSOMR2MEI.base import JSOMR2MEI
+    # from rodan.jobs.diagonal-neume-slicing import DiagonalNeumeSlicing
+    # from rodan.jobs.gamera_rodan import
+    # from rodan.jobs.helloworld import
+    # from rodan.jobs.heuristic-pitch-finding import MiyaoStaffinding
+    from rodan.jobs.JSOMR2MEI.base import JSOMR2MEI
 
-# from rodan.jobs.jSymbolic-Rodan import extract_features
-from rodan.jobs.MEI_encoding.MEI_encoding import MEI_encoding
+    # from rodan.jobs.jSymbolic-Rodan import extract_features
+    from rodan.jobs.MEI_encoding.MEI_encoding import MEI_encoding
 
-# from rodan.jobs.neon-wrapper import Neon
-# from rodan.jobs.pixel_wrapper import PixelInteractive
+    # from rodan.jobs.neon-wrapper import Neon
+    # from rodan.jobs.pixel_wrapper import PixelInteractive
 
-# from rodan.jobs.vis-rodan import
-# from rodan.jobs.biollante-rodan import BiollanteRodan
-from rodan.jobs.MEI_resizing.mei_resizing import MEI_Resize
+    # from rodan.jobs.vis-rodan import
+    # from rodan.jobs.biollante-rodan import BiollanteRodan
+    from rodan.jobs.MEI_resizing.mei_resizing import MEI_Resize
+except(ImportError):
+    raise ImportError("Problem importing jobs, make sure imports are configured correctly")
 
 
 def run_register_jobs():
