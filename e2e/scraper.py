17666fa07aba4499ca9e0b97d3c2fbb4b2b5379a
Paul Buser <buser.paul@gmail.com>
2022-06-02 11:10:41 -0400

    more

--- e2e/scraper.py
+++ e2e/scraper.py
@@ -1,10 +1,21 @@
 # Originally from https://github.com/jsoma/selenium-github-actions
 # Under the MIT License.
 
+import json
+
+from os import environ
+from time import sleep
+from urllib.parse import urljoin
+
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+PROTOCOL = "https"
+SITE = "rodan2.simssa.ca"
 
 chrome_options = Options()
 options = [
@@ -14,13 +25,41 @@ options = [
     "--ignore-certificate-errors",
     "--disable-extensions",
     "--no-sandbox",
-    "--disable-dev-shm-usage"
+    "--disable-dev-shm-usage",
 ]
-
 for option in options:
     chrome_options.add_argument(option)
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+driver = webdriver.Chrome(
+    service=Service(ChromeDriverManager().install()), options=chrome_options
+)
 
-driver.get('http://nytimes.com')
-print(driver.title)
+driver.implicitly_wait(5)
+
+# driver.get(SITE)
+
+username = environ["RODAN_USERNAME"]
+password = environ["RODAN_PASSWORD"]
+
+# username_field = driver.find_element(By.ID, "text-login_username")
+# password_field = driver.find_element(By.ID, "text-login_password")
+# login_button = driver.find_element(By.ID, "button-login")
+
+# username_field.send_keys(username)
+# password_field.send_keys(password)
+# login_button.click()
+
+# while not driver.get_cookies():
+#     sleep(1)
+
+# new_project_button = driver.find_element(By.ID, "button-new_project")
+
+# new_project_button.click()
+
+# projects_table = driver.find_element(By.XPATH, '//*[@id="table-projects"]')
+
+# print(projects_table.get_attribute("innerHTML"))
+full_url = f'{PROTOCOL}://{username}:{password}@{SITE}'
+projects_url = urljoin(full_url, "api/projects/?format=json")
+projects_json = driver.get(projects_url)
+print(driver.page_source)
