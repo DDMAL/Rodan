ab74e7a58de85117890ee97e6d822c8f05e0ef0c
sabrina0822 <90227881+sabrina0822@users.noreply.github.com>
2022-06-06 11:12:06 -0400

    MEI_ENCODING to python3

--- rodan-main/code/rodan/settings.py
+++ rodan-main/code/rodan/settings.py
@@ -143,7 +143,6 @@ RODAN_PYTHON2_JOBS = [
     "rodan.jobs.helloworld",
     "rodan.jobs.heuristic_pitch_finding",
     "rodan.jobs.interactive_classifier",
-    "rodan.jobs.MEI_encoding",
     "rodan.jobs.neon_wrapper",
     "rodan.jobs.pixel_wrapper",
     "rodan.jobs.biollante_rodan",
@@ -154,6 +153,7 @@ RODAN_PYTHON3_JOBS = [
     "rodan.jobs.helloworld",
     "rodan.jobs.hpc_fast_trainer",
     "rodan.jobs.pil_rodan",
+    "rodan.jobs.MEI_encoding",
     "rodan.jobs.mei2vol_wrapper"
 ]
 RODAN_GPU_JOBS = [
