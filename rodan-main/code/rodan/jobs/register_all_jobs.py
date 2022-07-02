a7488b84ff0c0a343321b5cfbf2e1b0db0219116
softcat477 <softcat477@gmail.com>
2022-07-01 22:01:50 -0400

    Add calvo back. to see if calvo classifier release GPU memory

--- rodan-main/code/rodan/jobs/register_all_jobs.py
+++ rodan-main/code/rodan/jobs/register_all_jobs.py
@@ -493,23 +493,23 @@ def register_gpu():
         import_name = "Calvo Trainer"
         print(import_name + " failed to import with the following error:", exception.__class__.__name__)
 
-    # try:
-    #     from rodan.jobs.Calvo_classifier.fast_calvo_classifier import (
-    #         FastCalvoClassifier,
-    #     )
+    try:
+        from rodan.jobs.Calvo_classifier.fast_calvo_classifier import (
+            FastCalvoClassifier,
+        )
 
-    #     app.register_task(FastCalvoClassifier())
-    # except Exception as exception:
-    #     import_name = "Fast Calvo Classifier"
-    #     print(import_name + " failed to import with the following error:", exception.__class__.__name__)
+        app.register_task(FastCalvoClassifier())
+    except Exception as exception:
+        import_name = "Fast Calvo Classifier"
+        print(import_name + " failed to import with the following error:", exception.__class__.__name__)
 
-    # try:
-    #     from rodan.jobs.Calvo_classifier.fast_calvo_trainer import FastCalvoTrainer
+    try:
+        from rodan.jobs.Calvo_classifier.fast_calvo_trainer import FastCalvoTrainer
 
-    #     app.register_task(FastCalvoTrainer())
-    # except Exception as exception:
-    #     import_name = "Fast Calvo Trainer"
-    #     print(import_name + " failed to import with the following error:", exception.__class__.__name__)
+        app.register_task(FastCalvoTrainer())
+    except Exception as exception:
+        import_name = "Fast Calvo Trainer"
+        print(import_name + " failed to import with the following error:", exception.__class__.__name__)
 
     # Register Text Alignment
     try:
