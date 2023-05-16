7e66ba5244c165221edee21b900718edb562c825
Anthony Tan <anthony.tan2@mail.mcgill.ca>
2023-05-16 16:18:12 -0400

    reorganize structure of test file

--- rodan-main/code/rodan/jobs/text_alignment/test.py
+++ rodan-main/code/rodan/jobs/text_alignment/test.py
@@ -1,17 +1,20 @@
 from skimage import io
+import json
 from . import align_to_ocr as align
 
-path_to_transcript="./example_data/transcript.txt"
-path_to_image="./example_data/image.png"
+folder = "./example_data/healthy/"
+
+path_to_transcript=folder + "transcript"
+path_to_image=folder+ "image.png"
 
 transcript = align.read_file(path_to_transcript)
-raw_image = io.imread(path_to_image
+raw_image = io.imread(path_to_image)
 model_name = "gothic_salzinnes_2021"
 
 result = align.process(raw_image, transcript, model_name)
 
 syl_boxes, _, lines_peak_locs, _ = result
-align.draw_results_on_page(raw_image, syl_boxes, lines_peak_locs,"result.png")
-outfile_path = "./example_data/output.json"
+align.draw_results_on_page(raw_image, syl_boxes, lines_peak_locs,folder+"result.png")
+outfile_path = folder+"result.json"
 with open(outfile_path, 'w') as file:
 json.dump(align.to_JSON_dict(syl_boxes, lines_peak_locs), file)
