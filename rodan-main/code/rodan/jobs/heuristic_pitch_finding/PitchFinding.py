8519feed350d59e70140da5d502a9ff4f3982c49
Anthony Tan <anthony.tan2@mail.mcgill.ca>
2023-06-22 16:06:21 -0400

    fix issue where id types flips to str instead of bytes

--- rodan-main/code/rodan/jobs/heuristic_pitch_finding/PitchFinding.py
+++ rodan-main/code/rodan/jobs/heuristic_pitch_finding/PitchFinding.py
@@ -159,8 +159,14 @@ class PitchFinder(object):
     # otherwise it will grab the left most part of the glyph with the width of the average punctum size * 0.8
     def get_subimage(self,glyph,extend_by_cols,extend_by_rows):
         # neumes where the bottom left head needs to be targeted
+        
+        # for some strange reason the type randomly will switch from bytes to str
+        id = glyph.get_main_id()
+        if type(id) is bytes:
+            id = id.decode()
+
         bottom_lefts = ["neume.podatus2b","neume.podatus3","neume.podatus4","neume.podatus5","neume.scandicus22b"]
-        if(glyph.get_main_id().decode() in bottom_lefts):
+        if(id in bottom_lefts):
             # gets bottom left head
             temp_glyph = glyph.subimage((glyph.offset_x + 0.0 * extend_by_cols, glyph.offset_y + glyph.nrows -1 - extend_by_rows),
                                     ((glyph.offset_x + 1.0 * extend_by_cols - 1), (glyph.offset_y + glyph.nrows -1)))
@@ -169,7 +175,7 @@ class PitchFinder(object):
             self.debug.addBox(glyph.offset_x + 0.0 * extend_by_cols, glyph.offset_y + glyph.nrows -1 - extend_by_rows,
                                 glyph.offset_x + 1.0 * extend_by_cols - 1, glyph.offset_y + glyph.nrows -1)
             return temp_glyph,y_add
-        elif(glyph.get_main_id().decode() == "neume.virga"):
+        elif(id == "neume.virga"):
             # gets the note head
             temp_glyph = glyph.subimage((glyph.offset_x + 0.0 *extend_by_cols, glyph.offset_y),
                                     ((glyph.offset_x + 1.0 * extend_by_cols - 1), (glyph.offset_y + 1.0 * extend_by_rows - 1)))
