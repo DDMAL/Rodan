f721dcbdee52a693fb8f72eede18b6c273a9990a
Van Pham <64571718+malajvan@users.noreply.github.com>
2023-06-23 12:41:42 -0400

    Update containers_healthcheck.sh

--- containers_healthcheck.sh
+++ containers_healthcheck.sh
@@ -3,9 +3,21 @@
 
 #Check whether volumes are properly mounted on containers 
 #for staging:
-# sudo docker inspect --format '{{json .Spec.TaskTemplate.ContainerSpec.Mounts}}' rodan_rodan-main 
-#for local
-docker inspect --format "{{ .Mounts }}" rodan-rodan-main-1 #
+ #get all mounts information for containers 
+for CONTAINER in $(sudo docker ps -aq)
+do 
+    if [[ $(sudo docker inspect --format " {{.Name}}" $CONTAINER) == *"rabbit"* ]];
+    then 
+    fi
+    mounts=$(sudo docker inspect --format  "{{json .Mounts }}" $CONTAINER )
+    echo $mounts 
+done
+# sudo docker inspect --format "{{.Name}}:      {{json .Mounts }}" $(sudo docker ps -aq) | column -t -s ' '
 
+# #Check whether ports are exposed in each containers
 
-#Check whether ports are exposed in each containers
+check_mounts(){
+    
+}
+
+# sudo docker inspect ls --format='{{json .Spec.TaskTemplate.ContainerSpec.Mounts}}' $(sudo docker ps -aq)
