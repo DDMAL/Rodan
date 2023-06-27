e211e91de7daa8ce712943f69cd1b650d7b7d4b5
Van Pham <64571718+malajvan@users.noreply.github.com>
2023-06-27 12:03:03 -0400

    check volume name

--- containers_healthcheck.sh
+++ containers_healthcheck.sh
@@ -4,20 +4,35 @@
 #Check whether volumes are properly mounted on containers 
 #for staging:
  #get all mounts information for containers 
-for CONTAINER in $(sudo docker ps -aq)
+for CONTAINER in $(sudo docker ps -aq);
 do 
-    if [[ $(sudo docker inspect --format " {{.Name}}" $CONTAINER) == *"rabbit"* ]];
-    then 
+    name=$(sudo docker inspect --format " {{.Name}}" $CONTAINER)
+    if [[ "$name" == *"rabbit"* ||  "$name" == *"redis"* || "$name" == *"postgres"* ]]; then
+        continue 
     fi
-    mounts=$(sudo docker inspect --format  "{{json .Mounts }}" $CONTAINER )
-    echo $mounts 
+    mount_list=$(sudo docker inspect --format  " {{json .Mounts }}" $CONTAINER )
+
+    IFS='{}[]' read -r -a a <<< $mount_list
+    array=( $a ) #separate mounts and reconstruct an array
+    binds=()
+    echo $name
+    for mount in "${array[@]}";
+    do
+        if [[ "$mount" == "," ]]; then 
+            continue
+        fi
+        # binds+=("$m")
+        IFS=',' read -r -a b <<< $mount
+        for e in $b
+        do 
+            if [[ "$e" == *"Name"* ]]; then
+                if [[ "$e" == "\"Name\":\"rodan_resources\"" ]]; then
+                    echo good
+                fi
+            fi
+
+        done
+    done
+    # echo "${binds[@]}" #now each bind is a json string 
+
 done
-# sudo docker inspect --format "{{.Name}}:      {{json .Mounts }}" $(sudo docker ps -aq) | column -t -s ' '
-
-# #Check whether ports are exposed in each containers
-
-check_mounts(){
-    
-}
-
-# sudo docker inspect ls --format='{{json .Spec.TaskTemplate.ContainerSpec.Mounts}}' $(sudo docker ps -aq)
