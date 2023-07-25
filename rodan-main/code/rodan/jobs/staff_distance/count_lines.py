610ccf2ac61353c4140ed34a040e27a78da28d39
Anthony Tan <anthony.tan2@mail.mcgill.ca>
2023-07-25 11:56:28 -0400

    change preprocessing/thresholding to be perfect

--- rodan-main/code/rodan/jobs/staff_distance/count_lines.py
+++ rodan-main/code/rodan/jobs/staff_distance/count_lines.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 from skimage import io
 from skimage.color import rgb2gray, rgba2rgb
 from skimage.util import img_as_float32, img_as_ubyte
-from skimage.filters import gaussian, threshold_otsu
+from skimage.filters import gaussian, threshold_yen, threshold_local
 from skimage.morphology import binary_opening, binary_closing
 from skimage.transform import rescale, rotate
 from skimage.segmentation import flood_fill
@@ -55,11 +55,9 @@ def find_rotation_angle(img, coarse_bound=4, fine_bound=0.25, rescale_amt=0.25):
         return best_angle
     
     angles_to_try = np.linspace(-coarse_bound, coarse_bound, num_trials)
-    print("finding coarse angle")
     coarse_angle = project_angles(img_resized, angles_to_try)
 
     angles_to_try = np.linspace(-fine_bound + coarse_angle, fine_bound + coarse_angle, int(num_trials/2))
-    print("finding fine angle")
     fine_angle = project_angles(img_resized, angles_to_try)
 
     return fine_angle
@@ -71,9 +69,9 @@ def find_rotation_angle(img, coarse_bound=4, fine_bound=0.25, rescale_amt=0.25):
 #     # determined experimentally
 
 #     x1 = 4872* 6496
-#     y1 = 2
+#     y1 = 4
 #     x2 = 9322*13438
-#     y2 = 6
+#     y2 = 7
 
 #     slope = (y2-y1)/(x2-x1)
 #     intercept = y1 - slope * x1
@@ -84,7 +82,7 @@ def find_rotation_angle(img, coarse_bound=4, fine_bound=0.25, rescale_amt=0.25):
 def get_kernel_size(img):
     height = img.shape[0]
 
-    # (x1,y1) and (x2,y2) (x3,xy) are mapping from pixels from kernel size that were
+    # (x1,y1) and (x2,y2) (x3,y3) are mappings from pixels from kernel size that were
     # determined experimentally
     # ideal kernel size is linear in the number of pixels, or quadratic in side length
     # two options: map pixels to kernel size with a linear function
@@ -92,13 +90,15 @@ def get_kernel_size(img):
     # the latter is preferable because for multi column folios, the width can be arbitrarily large
     # but the ideal kernel size will still be quadratic in the image height
 
+    # it's possible this is overkill and a linear function would be fine
+
     # solve for the interpolating quadratic function
     x1 = 6496
-    y1 = 2
+    y1 = 4
     x2 = 9854
-    y2 = 4
+    y2 = 5
     x3 = 13438
-    y3 = 6
+    y3 = 7
 
     matrix = np.array([[x1 **2, x1, 1],
                        [x2 **2, x2, 1],
@@ -110,15 +110,16 @@ def get_kernel_size(img):
     coefficients = np.matmul(inverse, vector)
     # the kernel size
     size = coefficients[0] * (height ** 2) + coefficients[1] * height + coefficients[2]
+    print(size)
     return max(1, round(size))
 
-def preprocess_image(input_image):
+def preprocess_image(input_image, contrast=0.25, brightness=0):
 
     # ensure that all points which are transparent have RGB values of 255 (will become white when
     # converted to non-transparent grayscale.)
 
     fill_holes = get_kernel_size(input_image)
-
+    # contrasted = cv2.addWeighted( input_image, contrast, input_image, 0, brightness)
     input_image = img_as_float32(input_image)
     if len(input_image.shape) == 3 and input_image.shape[2] == 4:
         input_image = rgba2rgb(input_image)
@@ -126,25 +127,24 @@ def preprocess_image(input_image):
 
     # get the otsu threshold after running a flood fill on the corners, so that those huge clumps of
     # dark pixels don't mess up the statistics too much (we only care about text!)
-    thresh = threshold_otsu(fill_corners(gray_img, fill_value=255, thresh=5, tol=1, fill_below_thresh=True))
-
+    thresh = threshold_yen(fill_corners(gray_img, fill_value=255, thresh=5, tol=1, fill_below_thresh=True))
     # now, fill corners of binarized images with black (value 0)
     img_blur = img_as_ubyte(img_as_ubyte(gray_img) < thresh)
-
     # now, fill corners of binarized images with black (value 0)
     img_blur_bin = fill_corners(img_blur, fill_value=0, thresh=1, tol=1, fill_below_thresh=False)
 
     # run smoothing on the blurred-binarized image so we get blobs of text in neat lines
-    kernel = np.ones((fill_holes, fill_holes), np.uint8)
-    img_cleaned = binary_opening(binary_closing(img_blur_bin, kernel), kernel)
-
+    
     # find rotation angle of cleaned, smoothed image. use that to correct the rotation of the unsmoothed image
-    print("finding rotation angle")
-    angle = find_rotation_angle(img_cleaned)
-    print("angle: ", angle)
-    img_cleaned_rot = rotate(img_cleaned, angle, order=0, mode='edge') > 0
+    angle = find_rotation_angle(img_blur_bin)
+    img_rot = rotate(img_blur_bin, angle, order=0, mode='edge') > 0
 
-    return img_cleaned_rot
+    kernel1 = np.ones((fill_holes, fill_holes*4), np.uint8)
+    kernel2 = np.ones((fill_holes, fill_holes), np.uint8)
+    img_cleaned_rot = binary_opening(img_rot, kernel1)
+    img_cleaned_rot = binary_closing(img_cleaned_rot, kernel2)
+
+    return img_cleaned_rot != 0
 
 
 
@@ -163,17 +163,4 @@ def calculate_via_slices(img):
                     distances[distance] += 1
                 else:
                     distances[distance] = 1
-    # distances[1] = 0
-    print(sorted(distances, key=distances.get, reverse=True)[:5])
     return max(distances, key=distances.get)
-
-
-if __name__ == "__main__":
-    img = get_image('Test_Data/5r.jpg')
-    print("preprocessing image")
-    processed = preprocess_image(img)
-    # save binarized image
-    plt.imsave('Test_Data/debug.jpg', np.array(processed), cmap='gray')
-    print("calculating distance")
-    distance = calculate_via_slices(processed)
-    print("distance: ", distance)
