80fb533cff17f0b83e8bf1c48e4c253e31ca3f7f
Anthony Tan <anthony.tan2@mail.mcgill.ca>
2023-07-27 13:48:34 -0400

    add code for generating augmented text alignment datasets

---
+++ rodan-main/code/rodan/jobs/text_alignment/training_new_models/generate_dataset.py
@@ -0,0 +1,134 @@
+import os
+import numpy as np
+from imgaug import augmenters as iaa
+import cv2
+from math import floor
+from random import shuffle
+pwd = os.getcwd() +"/"
+
+#fixes old nump verion issue
+np.bool = np.bool_
+
+dirs = ["salzinnes","stgallen"]
+
+images = []
+labels = []
+
+def augment_images(img, num):
+    name = str(num)
+    
+    for j in range(5):
+        file_name = name + "-" + str(j+1)
+        #img_aug = aug_some.augment_image(img)
+        img_aug = (aug_all.augment_image(img))
+        #write img_aig to file
+        cv2.imwrite(file_name+".png",img_aug)
+        text_file = open(file_name+".gt.txt", "w")
+        text_file.write(labels[i-1])
+
+for dir in dirs:
+    currdir = pwd+dir
+    os.chdir(currdir)
+    subdirs = os.listdir()
+    for subdir in subdirs:
+        datum_dir = currdir + "/" + subdir
+        if os.path.isdir(datum_dir):
+            os.chdir(datum_dir)
+            files = os.listdir()
+            for file in files:
+                if file.endswith("png"):
+                    try:
+                        text_file = open(file[:-7]+"gt.txt", "r")
+                        labels.append(text_file.read())
+                        img = cv2.imread(file)
+                        images.append(img)
+                    except Exception as e:
+                        print(e)
+                    #cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+                    
+                    
+
+#create augmenter for resclaing   
+
+contrast = iaa.GammaContrast((0.8,1.3))
+brightness = iaa.Multiply((0.8,1.3))
+blur = iaa.GaussianBlur(sigma=(0,1.5))
+noise = iaa.AdditiveGaussianNoise(scale=(0,0.2*255))
+hueAndSat = iaa.AddToHueAndSaturation((-200,100))
+pad = iaa.Pad(px= ((0,30),(0,10),(0,30),(0,2000)), pad_cval=255)
+#translateY = iaa.Affine(translate_percent={"y":(-0.2,0.2)},cval=255)
+
+#aug_some = iaa.SomeOf((0,None),[pad,contrast,brightness,blur,noise,hueAndSat])
+
+
+
+# rescale = iaa.Resize((0.5,1.3))
+scale = iaa.Affine(scale=(0.8, 1.3))
+cutout = iaa.Cutout(nb_iterations=(105, 120), size=(0.05,0.1), squared=True,fill_mode="constant", cval=255)
+dropout = iaa.ReplaceElementwise(0.1, 255)
+aug_all = iaa.Sequential([pad,contrast,brightness,blur,noise,scale,hueAndSat,dropout,cutout])
+
+outupt_dir = pwd + "train_aug"
+if(not os.path.isdir(outupt_dir)):
+    os.mkdir(outupt_dir)
+
+os.chdir(outupt_dir)
+i = 1
+
+temp = list(zip(images,labels))
+shuffle(temp)
+
+images, labels = zip(*temp)
+images, labels = list(images), list(labels)
+
+index= floor(len(images)*0.8)
+
+for img in images[:index]:
+    # new_dir = outupt_dir + "/" + str(i)
+    # if(not os.path.isdir(new_dir)):
+    #     os.mkdir(new_dir)
+    # os.chdir(new_dir)
+    #write label to txt file
+    augment_images(img,i)
+    i+=1
+
+os.chdir(pwd)
+outupt_dir = pwd + "val_aug"
+if(not os.path.isdir(outupt_dir)):
+    os.mkdir(outupt_dir)
+
+os.chdir(outupt_dir)
+
+for img in images[index:]:
+    # new_dir = outupt_dir + "/" + str(i)
+    # if(not os.path.isdir(new_dir)):
+    #     os.mkdir(new_dir)
+    # os.chdir(new_dir)
+    #write label to txt file
+    augment_images(img,i)
+    i+=1
+
+os.chdir(pwd)
+outupt_dir = pwd + "train_same"
+if(not os.path.isdir(outupt_dir)):
+    os.mkdir(outupt_dir)
+
+os.chdir(outupt_dir)
+i = 1
+for img in images[:index]:
+    cv2.imwrite(str(i)+".png",img)
+    text_file = open(str(i)+".gt.txt", "w")
+    text_file.write(labels[i-1])
+    i+=1
+
+os.chdir(pwd)
+outupt_dir = pwd + "val_same"
+if(not os.path.isdir(outupt_dir)):
+    os.mkdir(outupt_dir)
+
+os.chdir(outupt_dir)
+for img in images[index:]:
+    cv2.imwrite(str(i)+".png",img)
+    text_file = open(str(i)+".gt.txt", "w")
+    text_file.write(labels[i-1])
+    i+=1
